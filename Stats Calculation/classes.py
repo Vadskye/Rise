7829c8e6cd87153f64abb6efa19eefb9a288b519
Kevin Johnson <vadskye@gmail.com>
2013-12-17 16:40:17 -0800

    Establish main/classes divide
    The primary character object is for the character's final calculated
    attributes. The classes object will be for progressions and
    modifications. Essentially, the class should be a template which can be
    quickly swapped independently of the character's other statistics
    (attributes, magic items, etc.)
    
    
    Former-commit-id: d908b9c5fb2b5d7cc395f74eea26c7c71fe4ba04

--- Stats Calculation/classes.py
+++ Stats Calculation/classes.py
@@ -3,31 +3,30 @@ AVERAGE = 'average'
 POOR = 'poor'
 
 class barbarian:
-    base_attack_bonus_prog = GOOD
-    fortitude_prog = GOOD
-    reflex_prog = AVERAGE
-    will_prog = POOR
+    base_attack_bonus_progression = GOOD
+    save_progressions = {'Fortitude':GOOD, 'Reflex':AVERAGE, 'Will':POOR}
+    level = 0
 
     def __init__(self, level):
-        pass
+        self.level = level
 
+    def calculate_base_attack_bonus(self):
+        if self.base_attack_bonus_progression == GOOD:
+            return self.level;
+        elif self.base_attack_bonus_progression == AVERAGE:
+            return self.level*3/4;
+        elif self.base_attack_bonus_progression == POOR:
+            return self.level/2;
+        else:
+            return False
 
-def calc_bab(level, progression):
-    if progression == GOOD:
-        return level;
-    elif progression == AVERAGE:
-        return level*3/4;
-    elif progression == POOR:
-        return level/2;
-    else:
-        return False
-
-def calc_save(level, progression):
-    if progression == GOOD:
-        return level+2;
-    elif progression == AVERAGE:
-        return level*3/4+1;
-    elif progression == POOR:
-        return level/2;
-    else:
-        return False
+    def calc_save(self, save_type):
+        save_progression = self.save_progressions[save_type]
+        if save_progression == GOOD:
+            return level+2;
+        elif save_progression == AVERAGE:
+            return level*3/4+1;
+        elif save_progression == POOR:
+            return level/2;
+        else:
+            return False
