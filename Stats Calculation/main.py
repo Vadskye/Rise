66cbcdd6c320f475b8ca6c75d2c7f582387e9f85
Kevin Johnson <vadskye@gmail.com>
2014-02-08 19:38:53 -0800

    Create one character per level

--- Stats Calculation/main.py
+++ Stats Calculation/main.py
@@ -1,15 +1,28 @@
 import argparse
 from character import character
+import util
 
 def initialize_argument_parser():
     parser = argparse.ArgumentParser(description='Calculate combat statistics for Rise characters')
     parser.add_argument('-c', '--character', dest='character', 
             help='the character file to load', default='brb-heavy')
+    parser.add_argument('-l', '--level', dest='level',
+            help='the level of the character', default=None)
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
     args = initialize_argument_parser()
     print 'character:', args['character']
-    character_file = 'data/'+args['character']+'.txt'
-    barbarian = character(character_file)
-    print barbarian
+    character_file_name = 'data/'+args['character']+'.txt'
+    #If a specific level is given, show that level
+    raw_stats = util.parse_stats_from_file(character_file_name)
+    if args['level']:
+        barbarian = character(raw_stats, int(level))
+        print barbarian
+    #Otherwise, show all levels
+    else:
+        for i in xrange(1,5):
+            barbarian = character(raw_stats, i)
+            print barbarian
+            
+        
