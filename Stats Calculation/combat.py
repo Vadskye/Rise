9da0145d6b9300a99c20b2460f8dd740728003e6
Kevin Johnson <vadskye@gmail.com>
2014-02-09 12:47:23 -0800

    consolidate attack_count calculations
    Former-commit-id: 2e0816d99680a2c1915bd4d2ca909216548bc532

--- Stats Calculation/combat.py
+++ Stats Calculation/combat.py
@@ -3,14 +3,20 @@ def attack_hits(attack_bonus, ac):
     probability = (21+attack_bonus-ac)/20.0
     return min(0.95, max(0.05, probability))
 
+#return number of attacks this base attack bonus grants
+def attack_count(base_attack_bonus):
+    return 1 + (base_attack_bonus-1)/5
+
 #return estimated number of hits
 def full_attack_hits(primary_attack_bonus, ac, base_attack_bonus):
     total_hits=0
-    attack_count=0
-    while 5*attack_count<base_attack_bonus:
-        total_hits+=attack_hits(primary_attack_bonus-attack_count*5, ac)
-        attack_count+=1
+    for i in xrange(attack_count(base_attack_bonus)):
+        total_hits+=attack_hits(primary_attack_bonus-i*5, ac)
     return total_hits
+
+#return average probability that each attack will hit 
+def avg_hit_probability(primary_attack_bonus, ac, base_attack_bonus):
+    return full_attack_hits(primary_attack_bonus, ac, base_attack_bonus)/attack_count(base_attack_bonus)
 
 def attack_damage_dealt(attack_bonus, ac, avg_damage):
     return attack_hits(attack_bonus, ac)*avg_damage
