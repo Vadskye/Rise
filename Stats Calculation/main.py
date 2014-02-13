d8df6541511c86dc6f9aede0e7b9cfb51f27a3a7
Kevin Johnson <vadskye@gmail.com>
2014-02-12 18:45:06 -0800

    count rounds of standard combat
    Former-commit-id: d79b271ac78f30563b4299bfc15cafffcb2fdf92

--- Stats Calculation/main.py
+++ Stats Calculation/main.py
@@ -1,6 +1,7 @@
 import argparse
 from creature import Creature
 import util
+import combat
 
 def initialize_argument_parser():
     parser = argparse.ArgumentParser(description='Calculate combat statistics for Rise characters')
@@ -16,15 +17,28 @@ def compare_ac_to_reflex(creature):
                     creature.saves['reflex'].total(),
                     util.mstr(creature.armor_class.get_normal() - (10 + creature.saves['reflex'].total())))
 
+def rounds_survived(attacker, defender):
+    return combat.rounds_survived(attacker.attack_bonus.total(),
+                    defender.armor_class.get_normal(), attacker.base_attack_bonus,
+                    attacker.attack_damage.total(), defender.hp)
+
+def rounds_survived_generic(creature, i):
+    return combat.rounds_survived(creature.attack_bonus.total(),
+                    generic_ac_calc[i], creature.base_attack_bonus,
+                    creature.attack_damage.total(), generic_hp[i])
+    
+
 if __name__ == "__main__":
     args = initialize_argument_parser()
     #If a specific level is given, show that level
 
     #this is the generic AC we assume for attack calculations
     #note that we assume overwhelm 2 since overwhelm penalties are so common 
-    generic_ac_calc = range(15,35)
+    generic_ac_calc = [i+14 for i in range(1,21)]
     #so actual character AC should be two higher
-    generic_ac_real = range(17,37)
+    generic_ac_real = [i+16 for i in range(1,21)]
+
+    generic_hp = [i*7 for i in range(1,21)]
 
     if args['level']:
         creature = Creature.from_creature_name(args['creature'],
@@ -45,4 +59,7 @@ if __name__ == "__main__":
             npc = Creature.from_creature_name('npc', i+1)
             rogue = Creature.from_creature_name('rogue-single', i+1)
             #print i+1, cleric.dpr(generic_ac_calc[i]), 'vs', npc.dpr(generic_ac_calc[i])
-            print i+1, compare_ac_to_reflex(barbarian)
+            #print i+1, compare_ac_to_reflex(barbarian)
+            #print rounds_survived(npc, fighter)
+            #print i+1, fighter.hp, (i+1)*5.5/2, fighter.hp/((i+1)*5.5/2)
+            print rounds_survived_generic(npc, i)
