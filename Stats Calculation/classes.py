2e8d7d7893a4fbb4fe25ffe897eae5800f529646
Kevin Johnson <vadskye@gmail.com>
2014-02-28 19:32:26 -0800

    classes no longer directly calculate base bonuses
    Former-commit-id: 10ed7f39d75b57d6835ec0516d67101b2330b3e0

--- Stats Calculation/classes.py
+++ Stats Calculation/classes.py
@@ -1,8 +1,5 @@
 import util
-
-GOOD = 'good'
-AVERAGE = 'average'
-POOR = 'poor'
+from util import GOOD, AVERAGE, POOR
 
 class GenericClass:
     
@@ -21,27 +18,6 @@ class GenericClass:
         self.set_attack_damage()
         self.set_armor_class()
         self.set_saves()
-
-    def calculate_base_attack_bonus(self):
-        if self.base_attack_bonus_progression == GOOD:
-            return self.level;
-        elif self.base_attack_bonus_progression == AVERAGE:
-            return self.level*3/4;
-        elif self.base_attack_bonus_progression == POOR:
-            return self.level/2;
-        else:
-            return False
-
-    def calc_save(self, save_type):
-        save_progression = self.save_progressions[save_type]
-        if save_progression == GOOD:
-            return self.level+2;
-        elif save_progression == AVERAGE:
-            return self.level*3/4+1;
-        elif save_progression == POOR:
-            return self.level/2;
-        else:
-            return False 
 
     #override with specific classes
     def set_attack_bonus(self):
