f94949c8b922cc60186e80784c0459a8bf5479cf
Kevin Johnson <vadskye@gmail.com>
2014-03-01 20:28:12 -0800

    Run battles repeatedly to get more reliable statistics
    Former-commit-id: 33916e257e3d92efca7bd2b099316b290e00828e

--- Stats Calculation/combat.py
+++ Stats Calculation/combat.py
@@ -1,4 +1,5 @@
 import dice
+import copy
 
 d20 = dice.dx(20)
 
@@ -8,10 +9,25 @@ class Battle(object):
         self.first_creature = first_creature
         self.second_creature = second_creature
 
+    def iterated_battles(self, battle_count):
+        first_creature_wins = 0
+        second_creature_wins = 0
+        round_count_total = 0
+        for i in xrange(battle_count):
+            victor, round_count = self.determine_victor()
+            if victor.name == self.first_creature.name:
+                first_creature_wins+=1
+            elif victor.name == self.second_creature.name:
+                second_creature_wins+=1
+            else:
+                return False
+            round_count_total+=round_count
+        return first_creature_wins/float(battle_count), second_creature_wins/float(battle_count), round_count_total/float(battle_count)
+
     def determine_victor(self):
         round_count = 0
-        first_creature = self.first_creature
-        second_creature = self.second_creature
+        first_creature = copy.copy(self.first_creature)
+        second_creature = copy.copy(self.second_creature)
         while first_creature.is_alive and second_creature.is_alive:
             first_creature.new_round()
             second_creature.new_round()
