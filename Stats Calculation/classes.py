a876b618b68ba72ec585ca0405dac8b76fafb173
Kevin Johnson <vadskye@gmail.com>
2014-03-01 16:33:57 -0800

    Classes now directly modify Creature object

--- Stats Calculation/classes.py
+++ Stats Calculation/classes.py
@@ -5,113 +5,94 @@ class CharacterClass:
     
     def __init__(self, level):
         self.level = level
-        self.attack_bonus=dict()
-        self.attack_damage=dict()
-        self.armor_class=dict()
-        for title in util.ac_modifier_titles:
-            self.armor_class[title]=dict()
-        self.saves=dict()
-        for title in util.save_titles:
-            self.saves[title]=dict()
 
-        self.set_attack_bonus()
-        self.set_attack_damage()
-        self.set_armor_class()
-        self.set_saves()
+    def apply_progressions(self, base_creature):
+        base_creature.attack_bonus.set_progression(self.bab_progression)
+        base_creature.saves.set_progressions_dict(self.save_progressions)
+        base_creature.hit_value = self.hit_value
 
-    #override with specific classes
-    def set_attack_bonus(self):
-        pass
-    def set_attack_damage(self):
-        pass
-    def set_armor_class(self):
-        pass
-    def set_saves(self):
+    #Inherited classes overwrite
+    def apply_modifications(self, base_creature):
         pass
 
 class Average(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
-    save_progressions = {'fortitude': AVERAGE, 'reflex': AVERAGE, 'will': AVERAGE}
-    hit_value = 5
-
+        bab_progression = AVERAGE
+        save_progressions = {'fortitude': AVERAGE, 'reflex': AVERAGE, 'will': AVERAGE}
+        hit_value = 5
+    
 class Barbarian(CharacterClass):
 
-    base_attack_bonus_progression = GOOD
+    bab_progression = GOOD
     save_progressions = {'fortitude':GOOD, 'reflex':AVERAGE, 'will':POOR}
     hit_value = 7
 
-    def set_attack_damage(self):
-        self.attack_damage['competence']=std_scale(self.level)
-
-    def set_armor_class(self):
-        self.armor_class['misc']['inherent']=-2
-
-    def set_saves(self):
-        self.saves['fortitude']['competence']=std_scale(self.level)
-        self.saves['will']['competence']=std_scale(self.level)
+    def apply_modifications(self, base_creature):
+        base_creature.attack_damage.add_competence(std_scale(self.level))
+        base_creature.armor_class.misc.add_inherent(-2)
+        base_creature.saves.fortitude.add_competence(std_scale(self.level))
+        base_creature.saves.will.add_competence(std_scale(self.level))
 
 class Bard(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
+    bab_progression = AVERAGE
     save_progressions = {'fortitude':AVERAGE, 'reflex':AVERAGE, 'will':AVERAGE}
     hit_value = 6
 
 class Cleric(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
+    bab_progression = AVERAGE
     save_progressions = {'fortitude':AVERAGE, 'reflex':POOR, 'will':GOOD}
     hit_value = 5
 
 class Druid(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
+    bab_progression = AVERAGE
     save_progressions = {'fortitude':GOOD, 'reflex':POOR, 'will':AVERAGE}
     hit_value = 5
 
 class Fighter(CharacterClass):
-    base_attack_bonus_progression = GOOD
+    bab_progression = GOOD
     save_progressions = {'fortitude':GOOD, 'reflex':POOR, 'will':AVERAGE}
     hit_value = 6
 
-    def set_armor_class(self):
-        self.armor_class['dodge']['competence']=(self.level+5)/6
+    def apply_modifications(self, base_creature):
+        base_creature.armor_class.dodge.add_competence((self.level+5)/6)
 
-    def set_attack_bonus(self):
         #weapon focus + weapon disciplines
         ab=1+(self.level+3)/6
         if self.level>=8:
             ab+=1
-        self.attack_bonus['competence']=ab
+        base_creature.attack_bonus.add_competence(ab)
 
 class Monk(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
+    bab_progression = AVERAGE
     save_progressions = {'fortitude':AVERAGE, 'reflex':GOOD, 'will':GOOD}
     hit_value = 5
 
 class Paladin(CharacterClass):
-    base_attack_bonus_progression = GOOD
+    bab_progression = GOOD
     save_progressions = {'fortitude':GOOD, 'reflex':POOR, 'will':GOOD}
     hit_value = 6
 
 class Ranger(CharacterClass):
-    base_attack_bonus_progression = GOOD
+    bab_progression = GOOD
     save_progressions = {'fortitude':GOOD, 'reflex':AVERAGE, 'will':AVERAGE}
     hit_value = 6
 
 class Rogue(CharacterClass):
-    base_attack_bonus_progression = AVERAGE
+    bab_progression = AVERAGE
     save_progressions = {'fortitude':POOR, 'reflex':GOOD, 'will':POOR}
     hit_value = 5
 
 class Sorcerer(CharacterClass):
-    base_attack_bonus_progression = POOR
+    bab_progression = POOR
     save_progressions = {'fortitude':POOR, 'reflex':POOR, 'will':GOOD}
     hit_value = 4
 
 class Wizard(CharacterClass):
-    base_attack_bonus_progression = POOR
+    bab_progression = POOR
     save_progressions = {'fortitude':POOR, 'reflex':POOR, 'will':GOOD}
     hit_value = 4
 
 class Warrior(CharacterClass):
-    base_attack_bonus_progression = GOOD
+    bab_progression = GOOD
     save_progressions = {'fortitude':GOOD, 'reflex':POOR, 'will':POOR}
     hit_value = 6
 
