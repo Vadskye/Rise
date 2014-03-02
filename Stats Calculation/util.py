265cfb95d8bd37a7801643b02887a9b10b61a695
Kevin Johnson <vadskye@gmail.com>
2014-03-01 17:38:34 -0800

    Fix bugs with DR class
    Former-commit-id: 043840f2ea9e0ad475fe7ae5102242a514b40888

--- Stats Calculation/util.py
+++ Stats Calculation/util.py
@@ -177,26 +177,26 @@ class ArmorClass:
 class DamageReduction(object):
     def __init__(self, value = None, type_resisted = None, type_vulnerable=None):
         self.base_reduction = value
-        self.remaining_reduction = base_reduction
+        self.remaining_reduction = self.base_reduction
         self.type_resisted = type_resisted
         self.type_vulnerable = type_vulnerable
 
     def reduce_damage(self, damage, damage_types):
         #vulnerable typing shuts off DR
-        if type_vulnerable in damage_types:
-            remaining_reduction = 0
+        if self.type_vulnerable in damage_types:
+            self.remaining_reduction = 0
             return damage
         #apply DR if it is relevant
-        if type_resisted in damage_types:
-            damage_resisted = min(damage, remaining_reduction)
-            remaining_reduction -= damage_resisted
+        if self.type_resisted in damage_types:
+            damage_resisted = min(damage, self.remaining_reduction)
+            self.remaining_reduction -= damage_resisted
             return damage - damage_resisted
         #Just return the damage
         return damage
 
     #Damage reduction refreshes at the beginning of each round
     def refresh(self):
-        self.remaining_reduction = base_reduction
+        self.remaining_reduction = self.base_reduction
 
 def ifloor(num):
     return int(math.floor(num))
