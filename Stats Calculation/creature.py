b73e987fbac3413e9986415d293e457aa6582366
Kevin Johnson <vadskye@gmail.com>
2014-03-01 20:47:40 -0800

    remove print statement
    Former-commit-id: 88d83469e5c38fc655f23e355d7ce5ec42266da7

--- Stats Calculation/creature.py
+++ Stats Calculation/creature.py
@@ -103,7 +103,6 @@ class Creature(object):
 
     def _calculate_derived_statistics(self):
         self.attack_damage.add_die(self.weapon.damage_die)
-        print self.armor.encumbrance
         self.armor_class.armor.add_inherent(self.armor.ac_bonus)
         if self.armor.encumbrance=='medium' or self.armor.encumbrance=='heavy':
             self.armor_class.dodge.add_inherent(util.ifloor(
