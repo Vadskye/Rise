7c44c6c92091c97238c79a4363002804daa70cc3
Kevin Johnson <vadskye@gmail.com>
2014-03-03 23:42:19 -0800

    weapon generation now in classmethod
    Former-commit-id: 8862be642953ba146669effa2fa9a220ae281726

--- Stats Calculation/equipment.py
+++ Stats Calculation/equipment.py
@@ -7,6 +7,17 @@ class Weapon:
         self.weapon_type = weapon_type
         self.damage_die = dice.Dice.from_string(damage_die_name)
 
+    @classmethod
+    def from_weapon_name(cls, weapon_name):
+        return {
+                'heavy_melee': cls('heavy', 'melee', 'd10m2', 2),
+                'medium_melee': cls('medium', 'melee', 'd8m1', 1),
+                'light_melee': cls('light', 'melee', 'd6', 0),
+                'projectile': cls('heavy', 'projectile', 'd8m1', 1),
+                'claws': cls('medium', 'melee', 'd8m1', 0),
+                'none': None
+                }[weapon_name]
+
 class Armor:
     def __init__(self, ac_bonus, encumbrance, armor_type):
         self.ac_bonus = ac_bonus
@@ -15,14 +26,7 @@ class Armor:
 
 class EquipmentSet:
     def __init__(self, weapon_name, armor_name, shield_name):
-        self.weapon = {
-                'heavy_melee': Weapon('heavy', 'melee', 'd10m2', 2),
-                'medium_melee': Weapon('medium', 'melee', 'd8m1', 1),
-                'light_melee': Weapon('light', 'melee', 'd6', 0),
-                'projectile': Weapon('heavy', 'projectile', 'd8m1', 1),
-                'claws': Weapon('medium', 'melee', 'd8m1', 0),
-                'none': None
-                }[weapon_name]
+        self.weapon = Weapon.from_weapon_name(weapon_name)
         self.armor = {
                 'heavy': Armor(8, 'heavy', 'body'),
                 'medium': Armor(6, 'medium', 'body'),
