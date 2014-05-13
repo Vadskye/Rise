d7ff96916fa063d83b037e9eb43807e02d0c5aae
Kevin Johnson <vadskye@gmail.com>
2014-05-13 12:55:20 -0700

    use more generic "ability" instead of "feat"
    Former-commit-id: 098b11f7495ca16cd74accb2c58dc59622e67c5d

--- Stats Calculation/abilities.py
+++ Stats Calculation/abilities.py
@@ -21,10 +21,11 @@ def rage(level, creature):
     creature.saves.will.add_competence(util.std_scale(level))
     creature.current_hit_points += level*util.std_scale(level)
 
-class Feat(object):
+class Ability(object):
+    tags = set()
 
     def has_tag(self, tag):
-        return tag in self.tags
+        return tag.lower() in self.tags
 
     def meets_prerequisites(self, creature):
         return True
@@ -32,8 +33,9 @@ class Feat(object):
     def apply_benefit(self, creature):
         pass
 
-class OverwhelmingForce(Feat):
-    tags = ['Combat', 'Power']
+
+class OverwhelmingForce(Ability):
+    tags = set(('feat', 'combat', 'power'))
     def meets_prerequisites(self, creature):
         return creature.attributes.strength.get_total() >=5 and creature.attack_bonus.base_attack_bonus >=8 and creature.weapon.encumbrance == 'heavy'
 
@@ -42,8 +44,8 @@ class OverwhelmingForce(Feat):
             creature.weapon_damage.add_inherent(
                     (creature.attributes.strength.get_total()+1)/2)
 
-class TwoWeaponFighting(Feat):
-    tags = ['Combat', 'Finesse']
+class TwoWeaponFighting(Ability):
+    tags = set(('feat', 'combat', 'finesse'))
     def meets_prerequisites(self, creature):
         return creature.attributes.dexterity.get_total() >= 3
 
@@ -51,8 +53,8 @@ class TwoWeaponFighting(Feat):
         if creature.offhand_weapon:
             creature.attack_bonus.add_competence(2)
 
-class TwoWeaponDefense(Feat):
-    tags = ['Combat', 'Defense', 'Finesse']
+class TwoWeaponDefense(Ability):
+    tags = set(('feat', 'combat', 'defense', 'finesse'))
     def meets_prerequisites(self, creature):
         #can't currently implement feat prerequisites
         return creature.attributes.dexterity.get_total() >= 3
@@ -61,16 +63,16 @@ class TwoWeaponDefense(Feat):
         if creature.offhand_weapon:
             creature.armor_class.shield.add_competence(2)
 
-class CombatExpertise(Feat):
-    tags = ['Combat', 'Defense', 'Style']
+class CombatExpertise(Ability):
+    tags = set(('feat', 'combat', 'defense', 'style'))
     def meets_prerequisites(self, creature):
         return creature.attributes.intelligence.get_total() >= 3
     def apply_benefit(self, creature):
         creature.attack_bonus.add_circumstance(-util.bab_scale(creature.level))
         creature.armor_class.dodge.add_circumstance(util.bab_scale(creature.level))
 
-class PowerAttack (Feat):
-    tags = ['Combat', 'Power', 'Style']
+class PowerAttack (Ability):
+    tags = set(('feat', 'combat', 'power', 'style'))
     def meets_prerequisites(self, creature):
         return creature.attributes.strength.get_total() >= 3
     def apply_benefit(self, creature):
@@ -84,8 +86,8 @@ class PowerAttack (Feat):
             creature.offhand_weapon_damage.add_circumstance(damage_bonus/2)
 
 #Doesn't check to make sure creature uses ranged weapon
-class DeadlyAim (Feat):
-    tags = ['Combat', 'Precision', 'Style']
+class DeadlyAim (Ability):
+    tags = set(('feat', 'combat', 'precision', 'style'))
     def meets_prerequisites(self, creature):
         return creature.attributes.dexterity.get_total() >= 3
     def apply_benefit(self, creature):
