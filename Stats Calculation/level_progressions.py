4902dd3ea7a07338bf682d1b133f3066b0e8bb3e
Kevin Johnson <vadskye@gmail.com>
2014-09-12 17:39:18 -0700

    stats: return new class each time to allow modifications

--- Stats Calculation/level_progressions.py
+++ Stats Calculation/level_progressions.py
@@ -23,113 +23,116 @@ class LevelProgression(object):
         setattr(self, progression_name, util.improved_progression(
             getattr(self, progression_name)))
 
-classes = dict()
+def get_class(class_name):
+    if class_name == BARBARIAN:
+        def barbarian_modifications(base_creature):
+            base_creature.add_ability(abilities.rage)
+            base_creature.add_ability(abilities.barbarian_damage_reduction)
+            if base_creature.meta[LEVEL]>=2:
+                base_creature.add_ability(abilities['danger sense'])
+            if base_creature.meta[LEVEL]>=7:
+                abilities.larger_than_life(base_creature)
+            if base_creature.meta[LEVEL]>=17:
+                abilities.larger_than_belief(base_creature)
+        return LevelProgression(BARBARIAN, GOOD, GOOD,
+                AVERAGE, POOR, 7, NONE, barbarian_modifications)
 
-def barbarian_modifications(base_creature):
-    base_creature.add_ability(abilities.rage)
-    base_creature.add_ability(abilities.barbarian_damage_reduction)
-    if base_creature.meta[LEVEL]>=2:
-        base_creature.add_ability(abilities['danger sense'])
-    if base_creature.meta[LEVEL]>=7:
-        abilities.larger_than_life(base_creature)
-    if base_creature.meta[LEVEL]>=17:
-        abilities.larger_than_belief(base_creature)
+    elif class_name == CLERIC:
+        def cleric_modifications(base_creature):
+            base_creature.attack_mode='damage spell'
+        return LevelProgression(CLERIC, AVERAGE, AVERAGE, POOR, GOOD,
+                5, NONE, cleric_modifications)
 
-classes[BARBARIAN] = LevelProgression(BARBARIAN, GOOD, GOOD,
-        AVERAGE, POOR, 7, NONE, barbarian_modifications)
+    elif class_name == DRUID:
+        return LevelProgression(DRUID, AVERAGE, GOOD, POOR, AVERAGE,
+                NONE, 5)
 
-def cleric_modifications(base_creature):
-    base_creature.attack_mode='damage spell'
+    elif class_name == FIGHTER:
+        def fighter_modifications(base_creature):
+            #armor discipline
+            armor_discipline_count = (base_creature.meta[LEVEL]+5)/6
+            base_creature.defenses[AC].dodge.add_competence(
+                    armor_discipline_count)
+            for i in xrange(1, armor_discipline_count):
+                base_creature.items[ARMOR].encumbrance = util.lower_encumbrance(
+                        base_creature.items[ARMOR].encumbrance)
 
-classes[CLERIC] = LevelProgression(CLERIC, AVERAGE, AVERAGE, POOR, GOOD,
-        5, NONE, cleric_modifications)
+            #weapon discipline
+            ab = 0
+            ab += 1 if base_creature.meta[LEVEL]>=3 else 0
+            ab += 1 if base_creature.meta[LEVEL]>=9 else 0
+            base_creature.attacks[ATTACK_BONUS].add_competence(ab)
 
+            if base_creature.meta[LEVEL]>=15:
+                pass
+                #add critical changes
 
-classes[DRUID] = LevelProgression(DRUID, AVERAGE, GOOD, POOR, AVERAGE,
-        NONE, 5)
+        return LevelProgression(FIGHTER, GOOD, GOOD, POOR, AVERAGE,
+                6, NONE, fighter_modifications)
 
+    elif class_name == AVERAGE:
+        return LevelProgression(AVERAGE, AVERAGE, AVERAGE, AVERAGE,
+                AVERAGE, 5, NONE)
 
-def fighter_modifications(base_creature):
-    #armor discipline
-    armor_discipline_count = (base_creature.meta[LEVEL]+5)/6
-    base_creature.defenses[AC].dodge.add_competence(
-            armor_discipline_count)
-    for i in xrange(1, armor_discipline_count):
-        base_creature.items[ARMOR].encumbrance = util.lower_encumbrance(
-                base_creature.items[ARMOR].encumbrance)
+    elif class_name == MONK:
+        def monk_modifications(base_creature):
+            #wisdom is used often, so make it quick to access
+            wisdom = base_creature.attributes[WIS].get_total()
 
-    #weapon discipline
-    ab = 0
-    ab += 1 if base_creature.meta[LEVEL]>=3 else 0
-    ab += 1 if base_creature.meta[LEVEL]>=9 else 0
-    base_creature.attacks[ATTACK_BONUS].add_competence(ab)
+            #enlightened defense
+            if base_creature.items[ARMOR] is None:
+                base_creature.defenses[AC].misc.add_inherent(wisdom)
+            else:
+                base_creature.printverb('Monk is wearing armor? %s' %
+                        base_creature.items[ARMOR])
 
-    if base_creature.meta[LEVEL]>=15:
-        pass
-        #add critical changes
+            #unarmed strike
+            if base_creature.items[WEAPON_PRIMARY] is None:
+                unarmed_weapon = equipment.Weapon.from_weapon_name('unarmed')
+                #make the weapon deal monk damage
+                for i in xrange(2):
+                    unarmed_weapon.damage_die.increase_size(increase_min=True)
+                base_creature.items[WEAPON_PRIMARY] = unarmed_weapon
+                base_creature.attacks[WEAPON_DAMAGE_PRIMARY].add_die(
+                        unarmed_weapon)
 
-classes[FIGHTER] = LevelProgression(FIGHTER, GOOD, GOOD, POOR, AVERAGE,
-        6, NONE, fighter_modifications)
+            #wholeness of body
 
+            #improved ki strike
+            if base_creature.meta[LEVEL]>=10:
+                base_creature.attacks[DAMAGE][WEAPON_PRIMARY].add_inherent(wisdom/2)
 
-classes[AVERAGE] = LevelProgression(AVERAGE, AVERAGE, AVERAGE, AVERAGE,
-        AVERAGE, 5, NONE)
+        return LevelProgression(MONK, GOOD, AVERAGE, AVERAGE, AVERAGE,
+                5, NONE)
 
-def monk_modifications(base_creature):
-    #wisdom is used often, so make it quick to access
-    wisdom = base_creature.attributes[WIS].get_total()
+    elif class_name == PALADIN:
+        return LevelProgression(PALADIN, GOOD, GOOD, POOR, GOOD,
+                6, NONE)
 
-    #enlightened defense
-    if base_creature.items[ARMOR] is None:
-        base_creature.defenses[AC].misc.add_inherent(wisdom)
+    elif class_name == ROGUE:
+        def rogue_modifications(base_creature):
+            base_creature.add_ability('danger sense')
+
+        return LevelProgression(ROGUE, AVERAGE, POOR, GOOD, AVERAGE,
+                5, NONE, rogue_modifications)
+
+    elif class_name == SPELLWARPED:
+        return LevelProgression(SPELLWARPED, AVERAGE, AVERAGE,
+                AVERAGE, AVERAGE, 5, NONE)
+
+    elif class_name == SORCERER:
+        return LevelProgression(SORCERER, POOR, POOR, POOR, GOOD, 4, NONE)
+
+    elif class_name == WARRIOR:
+        return LevelProgression(WARRIOR, GOOD, GOOD, POOR, POOR, 6, NONE)
+
+    elif class_name == WIZARD:
+        return LevelProgression(WIZARD, POOR, POOR, POOR, GOOD, 4, NONE)
+
     else:
-        base_creature.printverb('Monk is wearing armor? %s' %
-                base_creature.items[ARMOR])
+        raise Exception("Can't recognize class name %s" % class_name)
 
-    #unarmed strike
-    if base_creature.items[WEAPON_PRIMARY] is None:
-        unarmed_weapon = equipment.Weapon.from_weapon_name('unarmed')
-        #make the weapon deal monk damage
-        for i in xrange(2):
-            unarmed_weapon.damage_die.increase_size(increase_min=True)
-        base_creature.items[WEAPON_PRIMARY] = unarmed_weapon
-        base_creature.attacks[WEAPON_DAMAGE_PRIMARY].add_die(
-                unarmed_weapon)
-
-    #wholeness of body
-
-    #improved ki strike
-    if base_creature.meta[LEVEL]>=10:
-        base_creature.attacks[DAMAGE][WEAPON_PRIMARY].add_inherent(wisdom/2)
-
-classes[MONK] = LevelProgression(MONK, GOOD, AVERAGE, AVERAGE, AVERAGE,
-        5, NONE)
-
-
-classes[PALADIN] = LevelProgression(PALADIN, GOOD, GOOD, POOR, GOOD,
-        6, NONE)
-
-
-def rogue_modifications(base_creature):
-    base_creature.add_ability('danger sense')
-
-classes[ROGUE] = LevelProgression(ROGUE, AVERAGE, POOR, GOOD, AVERAGE,
-        5, NONE, rogue_modifications)
-
-
-classes[SPELLWARPED] = LevelProgression(SPELLWARPED, AVERAGE, AVERAGE,
-        AVERAGE, AVERAGE, 5, NONE)
-
-
-classes[SORCERER] = LevelProgression(SORCERER, POOR, POOR, POOR, GOOD, 4, NONE)
-
-classes[WARRIOR] = LevelProgression(WARRIOR, GOOD, GOOD, POOR, POOR, 6, NONE)
-
-classes[WIZARD] = LevelProgression(WIZARD, POOR, POOR, POOR, GOOD, 4, NONE)
-
-monster_types = dict()
-
-def get_monster_level_progression(creature_type):
+def get_monster(creature_type):
     if creature_type == ABERRATION:
         def aberration_modifications(base_creature):
             base_creature.add_ability('darkvision', by_name=True)
