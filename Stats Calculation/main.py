02c01b64bc1ec6389b569876eae71622ccf2aaf2
Kevin Johnson <vadskye@gmail.com>
2015-02-08 18:06:50 -0800

    include round count in battle results
    Former-commit-id: 0802d050495213248743d0ca7d6f329bec047d39

--- Stats Calculation/main.py
+++ Stats Calculation/main.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
         if args['level'] == 'all':
             for i in xrange(len(allies)):
                 battle_results, hit_chances = generate_battle_results(allies[i], enemies[i])
-                print "Level %s \t%.3g\t%.3g" % (i+1, battle_results[0], battle_results[1])
+                print "Level %s \t%.3f\t%.3f\t%.3f" % (i+1, battle_results[0], battle_results[1], battle_results[2])
         else:
             battle_results, hit_chances = generate_battle_results(allies, enemies)
             print "Battle results:", battle_results
