aa159f9e23e4b1b0ebbe50b9981a44f720ce6282
Kevin Johnson <vadskye@gmail.com>
2015-09-11 21:35:56 -0700

    character sheet uses flexbox

--- character_sheet/sheet.py
+++ character_sheet/sheet.py
@@ -1,7 +1,15 @@
 from cgi_simple import *
 
-ATTRIBUTES = 'strength dexterity constitution intelligence perception willpower'.split()
+ATTRIBUTES = 'Strength Dexterity Constitution Intelligence Perception Willpower'.split()
 DEFENSES = 'Armor Maneuver Fortitude Reflex Mental'.split()
+ATTRIBUTE_SKILLS = {
+    'strength':  'Climb Jump Sprint Swim'.split(),
+    'dexterity': ['Balance', 'Escape Artist', 'Ride', 'Sleight of Hand', 'Tumble', 'Stealth'],
+    'constitution': [],
+    'intelligence': ['Craft', 'Devices', 'Disguise', 'Knowledge', 'Linguistics'],
+    'perception': ['Awareness', 'Heal', 'Sense Motive', 'Spellcraft', 'Survival'],
+    'willpower': [],
+}
 
 def main(fh):
     w('<!DOCTYPE html>')
@@ -9,135 +17,100 @@ def main(fh):
 
     html = '\n'.join([
         boring_stuff(),
-        div({'class': 'sheet-4colrow'}, [
-            col(attributes_and_skills()),
-            col(defenses()),
-            col([core_statistics(), conditional_effects()], 2),
+        flex_row([
+            attributes_and_skills(),
+            flex_col([
+                defenses(),
+                core_statistics(),
+            ]),
+            conditional_effects(),
+            attacks(),
         ]),
-        attacks(),
     ])
 
     w(debug_html_wrapper(html))
 
 def boring_stuff():
     return div({'id': 'boring-stuff'}, [
-        row(3, [
+        flex_row([
             labeled_text_input('Character name', 'character-name'),
             labeled_text_input('Player name', 'player-name'),
             labeled_text_input('Class and level', 'class-and-level'),
         ]),
-        row(4, [
+        flex_row([
             labeled_text_input('Race and background', 'race-and-background'),
-            labeled_text_input('Concept', 'concept'),
             labeled_text_input('Alignment and deity', 'alignment-and-deity'),
             labeled_text_input('Appearance', 'appearance'),
+            labeled_text_input('Concept', 'concept'),
         ]),
     ])
 
 def attributes_and_skills():
-    return div({'id': 'attributes-and-skills'}, [
-        attribute_section(attribute) for attribute in ATTRIBUTES
+    return flex_col({'id': 'attributes-and-skills'}, [
+        div(div({'class': 'section-header'}, 'Attributes and Skills')),
+        ''.join([attribute_section(attribute) for attribute in ATTRIBUTES]),
     ])
 
 def attribute_section(attribute):
-    return {
-        'strength': strength_section,
-        'dexterity': dexterity_section,
-        'constitution': constitution_section,
-        'intelligence': intelligence_section,
-        'perception': perception_section,
-        'willpower': willpower_section,
-    }[attribute]()
-
-def strength_section():
-    return div({'id': 'strength-section', 'class': 'attribute-section'},
-               [attribute_header('Strength')]
-               + [skill_box(skill) for skill in 'Climb Jump Sprint Swim'.split()]
-    )
-
-def dexterity_section():
-    return div({'id': 'dexterity-section', 'class': 'attribute-section'},
-               [attribute_header('Dexterity')]
-               + [skill_box(skill) for skill in ['Balance', 'Escape Artist', 'Ride', 'Sleight of Hand', 'Tumble', 'Stealth']]
-    )
-
-def constitution_section():
-    return div({'id': 'constitution-section', 'class': 'attribute-section'},
-               [attribute_header('Constitution')],
-    )
-
-def intelligence_section():
-    return div({'id': 'intelligence-section', 'class': 'attribute-section'},
-               [attribute_header('Intelligence')]
-               + [skill_box(skill) for skill in ['Craft', 'Devices', 'Disguise', 'Knowledge', 'Linguistics']]
-    )
-
-def perception_section():
-    return div({'id': 'perception-section', 'class': 'attribute-section'},
-               [attribute_header('Perception')]
-               + [skill_box(skill) for skill in ['Awareness', 'Heal', 'Sense Motive', 'Spellcraft', 'Survival']]
-    )
-
-def willpower_section():
-    return div({'id': 'willpower-section', 'class': 'attribute-section'},
-               [attribute_header('Willpower')]
-    )
-
-def attribute_header(name):
-    return div({'class': 'attribute-header labeled_number_input'}, [
-        span({'class': 'attribute-name number-label'}, name),
-        number_input({
-            'name': name,
-            'class': 'number-input boxed-input',
-        }),
-    ])
-
-def defense_header(name):
-    return div({'class': 'defense-header labeled_number_input'}, [
-        span({'class': 'defense-name number-label'}, name),
-        number_input({
-            'name': name,
-            'class': 'number-input boxed-input',
-        }),
+    return flex_col({'class': 'attribute-section'}, [
+        labeled_number_input(attribute, attribute.lower(), {'class': 'attribute'}),
+        ''.join([skill_box(skill) for skill in ATTRIBUTE_SKILLS[attribute.lower()]])
     ])
 
 def skill_box(name):
-    return div({'class': 'skill-box'}, [
+    return flex_row({'class': 'skill-box'}, [
         span(
             {'class': 'number-label'},
             name
         ),
         number_input({
             'name': name.lower(),
-            'class': 'number-input boxed-input',
         }),
     ])
 
 def core_statistics():
-    return ''
+    return flex_col({'id': 'core-statistics'}, [
+        div(div({'class': 'section-header'}, 'Core Statistics')),
+        hit_points(),
+    ])
 
 def defenses():
-    return div({'id': 'defenses'}, [
-        defense_header(defense) for defense in DEFENSES
+    return flex_col({'id': 'defenses'}, [
+        div(div({'class': 'section-header'}, 'Defenses')),
+        "".join([labeled_defense_input(defense) for defense in DEFENSES]),
     ])
+
+def labeled_defense_input(defense_name):
+    return labeled_number_input(defense_name, defense_name.lower(), {'class': 'defense'})
+
+def hit_points():
+    return labeled_number_input('Hit Points', 'hit-points', {'class': 'defense'})
 
 def conditional_effects():
     return ''
 
 def attacks():
-    return ''
+    return flex_col({'id': 'attacks'}, [
+        div(div({'class': 'section-header'}, 'Attacks')),
+        "".join([attack(i) for i in range(4)]),
+    ])
 
-def row(column_count, contents):
-    return div(
-        {'class': 'sheet-{0}colrow'.format(column_count)},
-        [col(stuff) for stuff in contents]
-    )
+def attack(attack_number = None):
+    return flex_row({'class': 'single-attack'}, [
+        labeled_text_input('Attack', 'attack{0}-name'.format(attack_number), {'class': 'attack-name'}),
+        underlabeled_number_input('Bonus', 'attack{0}-bonus'.format(attack_number), {'class': 'attack-bonus'}),
+        labeled_text_input('Damage/Effect', 'attack{0}-effect'.format(attack_number), {'class': 'attack-effect'}),
+    ])
 
-def col(contents, column_count = None):
-    return div(
-        {'class': 'my-{0}col'.format(column_count or '')},
-        contents
-    )
+def flex_row(attributes = None, contents = None):
+    attributes, contents = ensure_valid_attributes_and_contents(attributes, contents)
+    attributes['class'] = 'flex-row ' + attributes.get('class', '')
+    return div(attributes, contents)
+
+def flex_col(attributes = None, contents = None):
+    attributes, contents = ensure_valid_attributes_and_contents(attributes, contents)
+    attributes['class'] = 'flex-col ' + attributes.get('class', '')
+    return div(attributes, contents)
 
 def rise_title():
     return h1(
@@ -145,28 +118,48 @@ def rise_title():
         'Rise'
     )
 
-def labeled_text_input(label_name, input_name):
-    return div({'class': 'labeled-text-input'}, [
+def labeled_text_input(label_name, input_name, attributes = None):
+    attributes = attributes or dict()
+    attributes['class'] = 'labeled-text-input ' + attributes.get('class', '')
+    return span(attributes, [
         text_input({
             'name': input_name,
-            'class': 'text-input underlined-input',
         }),
         span(
-            {'class': 'text-label'},
+            {'class': 'under-label'},
             label_name
         ),
     ])
 
-def labeled_number_input(label_name, input_name):
-    return div({'class': 'labeled-number-input'}, [
+def labeled_number_input(label_name, input_name, attributes = None):
+    attributes = attributes or dict()
+    attributes['class'] = 'labeled-number-input ' + attributes.get('class', '')
+
+    return flex_row(attributes, [
         span(
             {'class': 'number-label'},
             label_name
         ),
         number_input({
             'name': input_name,
-            'class': 'number-input boxed-input',
         }),
+    ])
+
+def underlabeled_number_input(label_name, input_name, attributes = None):
+    attributes = attributes or dict()
+    if attributes.has_key('class'):
+        attributes['class'] += ' underlabeled-number-input'
+    else:
+        attributes['class'] = 'underlabeled-number-input'
+
+    return div(attributes, [
+        number_input({
+            'name': input_name,
+        }),
+        span(
+            {'class': 'under-label'},
+            label_name
+        ),
     ])
 
 def labeled_dual_input(label_name, text_input_name, number_input_name):
@@ -174,7 +167,6 @@ def labeled_dual_input(label_name, text_input_name, number_input_name):
         labeled_text_input(label_name, text_input_name),
         number_input({
             'name': number_input_name,
-            'class': 'number-input boxed-input',
         }),
     ])
 
@@ -186,13 +178,13 @@ def debug_stylesheets():
         #    'rel': 'stylesheet',
         #    'href': 'roll20.css',
         #}),
+        #link({
+            #'rel': 'stylesheet',
+            #'href': 'roll20_editor.css',
+        #}),
         link({
             'rel': 'stylesheet',
-            'href': 'roll20_editor.css',
-        }),
-        link({
-            'rel': 'stylesheet',
-            'href': 'sheet.css',
+            'href': 'paper_sheet.css',
         }),
     ])
 
