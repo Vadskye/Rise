bddef5bfc4e63e891163fe99336c04e8bfce4e6a
Kevin Johnson <vadskye@gmail.com>
2015-09-22 21:57:21 -0700

    more missing skills
    Former-commit-id: c251576374a4016949249826117b29a6717cd271

--- character_sheet/sheet_data.py
+++ character_sheet/sheet_data.py
@@ -2,9 +2,9 @@ ATTRIBUTES = 'Strength Dexterity Constitution Intelligence Perception Willpower'
 DEFENSES = 'Armor Maneuver Fortitude Reflex Mental'.split()
 ATTRIBUTE_SKILLS = {
     'strength':  'Climb Jump Sprint Swim'.split(),
-    'dexterity': ['Balance', 'Escape Artist', 'Ride', 'Sleight of Hand', 'Tumble', 'Stealth'],
+    'dexterity': ['Balance', 'Escape Artist', 'Ride', 'Sleight of Hand', 'Stealth', 'Tumble'],
     'constitution': [],
-    'intelligence': ['Craft', 'Devices', 'Disguise', 'Knowledge', 'Linguistics'],
+    'intelligence': ['Craft', 'Devices', 'Disguise', 'Knowledge', 'Knowledge', 'Linguistics'],
     'perception': ['Awareness', 'Creature Handling', 'Heal', 'Sense Motive', 'Spellcraft', 'Survival'],
     'willpower': [],
 }
