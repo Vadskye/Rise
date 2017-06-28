e183219b477b639faae07c0d6810d15c1b53e440
Kevin Johnson <vadskye@gmail.com>
2017-06-28 07:33:52 +0000

    add domain spell generation
    Former-commit-id: 8a5537da026af98dee3dc68fc2452be0919a2680

--- core_book/gen_spell_lists.py
+++ core_book/gen_spell_lists.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import click
-from gen_spell_descriptions import generate_spells, latexify, SPELL_SOURCES
+from gen_spell_descriptions import DOMAINS, generate_spells, latexify, SPELL_SOURCES
 
 def spells_by_source(spells):
     spells_by_source = {source: [] for source in SPELL_SOURCES}
@@ -12,13 +12,22 @@ def spells_by_source(spells):
     return spells_by_source
 
 
+def spells_by_domain(spells):
+    by_domain = {}
+    for spell in spells:
+        for domain in DOMAINS:
+            if domain in spell.lists:
+                if (domain in by_domain):
+                    print(f"Domain {domain} has too many spells")
+                by_domain[domain] = spell
+    return by_domain
+
 def latex_for_source(source, spells):
     spell_headers = []
     for spell in sorted(spells, key=lambda s: s.name):
         spell_headers.append(f"\\spellhead<{spell.name}> {spell.short_description}")
 
     spell_latex = "\n".join(spell_headers)
-
     return f"""
         \\small
         \\section<{source} Magic>\\label<{source} Magic>
@@ -29,14 +38,34 @@ def latex_for_source(source, spells):
     """
 
 
+def latex_for_domains(spells):
+    by_domain = spells_by_domain(spells)
+    domain_spell_headers = []
+    for domain in sorted(DOMAINS):
+        spell = by_domain.get(domain, None)
+        if spell is None:
+            domain_spell_headers.append(f"\\item {domain} -- \\spellhead*<missing> \\tdash")
+        else:
+            domain_spell_headers.append(f"\\item {domain} -- \\spellhead*<{spell.name}> {spell.short_description}")
+    domain_spell_latex = "\n".join(domain_spell_headers)
+    return f"""
+        \\subsection<Domain Spells>\\label<Domain Spells>
+        \\begin<spelllist>
+            {domain_spell_latex}
+        \\end<spelllist>
+    """
+
+
 def generate_spell_lists(spells):
     by_source = spells_by_source(spells)
 
     return latexify(
-        "\n\n".join(
-            [latex_for_source(source, by_source[source])
-             for source in SPELL_SOURCES]
-        )
+        "\n\n".join([
+            latex_for_source('Arcane', by_source['Arcane']),
+            latex_for_source('Divine', by_source['Divine']),
+            latex_for_domains(spells),
+            latex_for_source('Nature', by_source['Nature']),
+        ])
     )
 
 
