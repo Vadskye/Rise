9b66fbaa4e8d0971079e47698f9e9afe7d99a2e3
Kevin Johnson <vadskye@gmail.com>
2017-11-08 07:16:24 +0000

    redesign monster gen to be more functional

--- python/rise/latex_generation/monsters.py
+++ python/rise/latex_generation/monsters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import click
-from rise.latex.monster import Monster
+from rise.latex.monster import get_latex_from_creature
 from rise.statistics.character_class import CharacterClass
 from rise.statistics.creature import Creature
 from rise.statistics.race import Race
@@ -9,42 +9,46 @@ from rise.statistics.weapon import Weapon
 from rise.latex.util import latexify
 
 def generate_monsters():
-    creatures = []
-    creatures.append(Creature(
-        character_class=CharacterClass('behemoth'),
-        level=3,
-        name='Bear',
-        name_suffix='Black',
-        natural_armor=4,
-        race=Race('animal'),
-        starting_attributes=[3, 1, 2, -7, 1, 0],
-        weapons=[Weapon('bite'), Weapon('claw')],
-    ))
-
-    creatures.append(Creature(
-        character_class=CharacterClass('behemoth'),
-        level=6,
-        name='Bear',
-        natural_armor=4,
-        name_suffix='Brown',
-        race=Race('animal'),
-        starting_attributes=[3, 1, 2, -7, 1, 0],
-        weapons=[Weapon('bite'), Weapon('claw')],
-    ))
-
-    creatures.append(Creature(
-        character_class=CharacterClass('adept'),
-        level=12,
-        name='Aboleth',
-        natural_armor=6,
-        race=Race('aberration'),
-        starting_attributes=[2, 0, 3, 2, 1, 3],
-        weapons=[Weapon('tentacle')],
-    ))
-
     monsters = []
-    for creature in creatures:
-        monsters.append(Monster.from_creature(creature))
+    monsters.append(get_latex_from_creature(
+        Creature(
+            character_class=CharacterClass('behemoth'),
+            level=3,
+            name='Bear',
+            name_suffix='Black',
+            natural_armor=4,
+            race=Race('animal'),
+            starting_attributes=[3, 1, 2, -7, 1, 0],
+            weapons=[Weapon('bite'), Weapon('claw')],
+            immunities=['staggered'],
+        ),
+    ))
+
+    monsters.append(get_latex_from_creature(
+        Creature(
+            character_class=CharacterClass('behemoth'),
+            level=6,
+            name='Bear',
+            natural_armor=4,
+            name_suffix='Brown',
+            race=Race('animal'),
+            starting_attributes=[3, 1, 2, -7, 1, 0],
+            weapons=[Weapon('bite'), Weapon('claw')],
+            immunities=['staggered'],
+        ),
+    ))
+
+    monsters.append(get_latex_from_creature(
+        Creature(
+            character_class=CharacterClass('adept'),
+            level=12,
+            name='Aboleth',
+            natural_armor=6,
+            race=Race('aberration'),
+            starting_attributes=[2, 0, 3, 2, 1, 3],
+            weapons=[Weapon('tentacle')],
+        ),
+    ))
 
     return monsters
 
