f7dbcd05a5ccdf49964f0e7c3dc05d5fa1f0492c
Kevin Johnson <vadskye@gmail.com>
2017-12-06 17:35:49 +0000

    simplify spell description format
    Former-commit-id: 8457615aba8a1d777855cfb1f6fc188564f027bc

--- python/rise/latex/targeting.py
+++ python/rise/latex/targeting.py
@@ -34,20 +34,31 @@ class Targeting(object):
         self.time = time
         self.unrestricted_range = unrestricted_range
 
+        if self.rng and not (self.area or self.target or self.targets):
+            raise Exception('Range must be paired with something')
+
     def area_text(self):
         """Return the text corresponding to this spell's area, if any."""
         if self.area is not None:
             prefix = Targeting.area_prefix[self.area_type]
-            return f"{prefix}<{self.area}>"
+            return f"{prefix}<{self.area}{self.range_text()}>"
         else:
             return ""
 
+    def range_text(self):
+        unrestricted_text = f" (Unrestricted)" if self.unrestricted_range else ""
+        return (
+            f" within {rng_mapping.get(self.rng, self.rng)} range{unrestricted_text}"
+            if self.rng else ""
+        )
+
     def target_text(self):
         """Return the text corresponding to this spell's targets, if any."""
+
         if self.target:
-            return f"\\spelltgt<{self.target}>"
+            return f"\\spelltgt<{self.target}{self.range_text()}>"
         elif self.targets:
-            return f"\\spelltgts<{self.targets}>"
+            return f"\\spelltgts<{self.targets}{self.range_text()}>"
         else:
             return ""
 
@@ -55,36 +66,13 @@ class Targeting(object):
         action_point_text = f'\\parhead*<Action Points> {self.action_points}' if self.action_points else ""
         time_text = f'\\spelltime<{self.time}>' if self.time else ""
         special_text = f'\\spellspecial {self.special}' if self.special else ""
-        if self.rng:
-            col2 = "\\spellrng<{rng}{unrestricted}>".format(
-                rng=rng_mapping.get(self.rng, self.rng),
-                unrestricted=" (Unrestricted)" if self.unrestricted_range else "",
-            )
-            col1 = self.area_text()
-            included_target_text = False
-            if not col1:
-                col1 = self.target_text()
-                included_target_text = True
-            if col1:
-                twocol_text = f"\\spelltwocol<{col1}><{col2}>"
-            else:
-                twocol_text = col2
 
-            # if we included the target_text above, don't include it again
-            return f"""
-                \\begin<spelltargetinginfo>
-                    {special_text}
-                    {twocol_text}
-                    {self.target_text() if not included_target_text else ""}
-                    {time_text}
-                    {action_point_text}
-                \\end<spelltargetinginfo>
-            """
-        else:
-            return f"""
-                \\begin<spelltargetinginfo>
-                    {special_text}
-                    {self.area_text()}
-                    {self.target_text()}
-                \\end<spelltargetinginfo>
-            """
+        return f"""
+            \\begin<spelltargetinginfo>
+                {special_text}
+                {self.target_text()}
+                {self.area_text()}
+                {time_text}
+                {action_point_text}
+            \\end<spelltargetinginfo>
+        """
