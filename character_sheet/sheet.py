ec5a65328c294b9512dae047d4007291c852904c
Kevin Johnson <vadskye@gmail.com>
2018-04-23 20:36:47 -0700

    support arguments on command line
    Former-commit-id: 345d99fe1d421e71ea0a17606de8864a17cd6380

--- character_sheet/sheet.py
+++ character_sheet/sheet.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 
+import click
 import cgi_simple as cgi
 import first_page
 import second_page
 import third_page
-import ability_cards
+import ability_cards as generate_ability_cards
 from subprocess import call
 import sys
-
-DESTINATION = 'paper'
 
 try:
     if sys.argv[1] == 'pretty':
         cgi.is_pretty = True
 except IndexError:
     pass
-
-if DESTINATION == 'roll20':
-    cgi.input_name_prefix = 'attr_'
 
 def compile_less():
     call(['lessc', 'ability_cards.less', 'ability_cards.css'])
@@ -28,36 +24,44 @@ def compile_less():
     call(['lessc', 'paper_sheet.less', 'paper_sheet.css'])
     call(['lessc', 'roll20.less', 'roll20.css'])
 
-def main():
+
+@click.command()
+@click.option('-a', '--ability-cards/--no-ability-cards', default=False)
+@click.option('-d', '--destination', default='paper')
+def main(ability_cards, destination):
     compile_less()
+
+    if destination == 'roll20':
+        cgi.input_name_prefix = 'attr_'
 
     with open('first_page.html', 'w') as fh:
         fh.write(''.join([
-            debug_stylesheets('first_page'),
-            debug_html_wrapper(first_page.create_page()),
+            debug_stylesheets('first_page', destination),
+            debug_html_wrapper(first_page.create_page(), destination),
         ]) + '\n')
 
     with open('second_page.html', 'w') as fh:
         fh.write(''.join([
-            debug_stylesheets('second_page'),
-            debug_html_wrapper(second_page.create_page()),
+            debug_stylesheets('second_page', destination),
+            debug_html_wrapper(second_page.create_page(), destination),
         ]) + '\n')
 
     with open('third_page.html', 'w') as fh:
         fh.write(''.join([
-            debug_stylesheets('third_page'),
-            debug_html_wrapper(third_page.create_page()),
+            debug_stylesheets('third_page', destination),
+            debug_html_wrapper(third_page.create_page(), destination),
         ]) + '\n')
 
-    with open('ability_cards.html', 'w') as fh:
-        fh.write(''.join([
-            debug_stylesheets('ability_cards'),
-            debug_html_wrapper(ability_cards.create_page()),
-        ]) + '\n')
+    if ability_cards:
+        with open('ability_cards.html', 'w') as fh:
+            fh.write(''.join([
+                debug_stylesheets('ability_cards', destination),
+                debug_html_wrapper(generate_ability_cards.create_page(), destination),
+            ]) + '\n')
 
-def debug_stylesheets(page_name):
+def debug_stylesheets(page_name, destination):
 
-    if DESTINATION == 'paper':
+    if destination == 'paper':
         return '<!DOCTYPE html>' + cgi.head([
             cgi.link({
                 'rel': 'stylesheet',
@@ -69,16 +73,16 @@ def debug_stylesheets(page_name):
             }),
             cgi.link({
                 'rel': 'stylesheet',
-                'href': DESTINATION + '_sheet.css',
+                'href': destination + '_sheet.css',
             }),
         ])
-    elif DESTINATION == 'roll20':
+    elif destination == 'roll20':
         return ""
     else:
-        raise Exception("Unknown destination '" + DESTINATION + "'")
+        raise Exception("Unknown destination '" + destination + "'")
 
-def debug_html_wrapper(html):
-    if DESTINATION == 'paper':
+def debug_html_wrapper(html, destination):
+    if destination == 'paper':
         return cgi.div(
             {
                 'class': 'dialog characterdialog ui-dialog ui-dialog-content ui-widget-content',
@@ -88,10 +92,10 @@ def debug_html_wrapper(html):
                 html
             )
         )
-    elif DESTINATION == 'roll20':
+    elif destination == 'roll20':
         return html
     else:
-        raise Exception("Unknown destination '" + DESTINATION + "'")
+        raise Exception("Unknown destination '" + destination + "'")
 
 if __name__ == "__main__":
     main()
