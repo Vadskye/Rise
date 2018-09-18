8f25e490d5a4e09078395df5ee3940c86874fbe2
Kevin Johnson <vadskye@gmail.com>
2018-09-18 01:36:43 +0000

    improve testing infrastructure
    Former-commit-id: 42fdafa17408838991629a38859555f91137c10d

--- python/rise/simulator/run_combat.py
+++ python/rise/simulator/run_combat.py
@@ -11,28 +11,40 @@ def run_combat(blue, red):
         object: Unknown
     """
 
-    blue_defenses = get_defenses(blue)
-    red_defenses = get_defenses(red)
-
-    blue_attack = find_best_attack(blue.attacks, red_defenses)
-    red_attack = find_best_attack(red.attacks, blue_defenses)
-
-    blue_average_damage = get_average_damage(blue_attack, red_defenses)
-    red_average_damage = get_average_damage(red_attack, blue_defenses)
-
-    blue_survival_time = blue.hit_points / red_average_damage
-    red_survival_time = red.hit_points / blue_average_damage
+    blue_survival_time = get_survival_time(red, blue)
+    red_survival_time = get_survival_time(blue, red)
 
     winner = 'Blue' if blue_survival_time > red_survival_time else None
     winner = 'Red' if red_survival_time > blue_survival_time else winner
     winner = winner or 'Tie'
 
     return {
-        'blue_survival_time': round(blue_survival_time, 2),
-        'red_survival_time': round(red_survival_time, 2),
+        'blue_time_to_kill': round(red_survival_time, 2),
+        'red_time_to_kill': round(blue_survival_time, 2),
         'round_count': ceil(min(blue_survival_time, red_survival_time)),
         'winner': winner,
     }
+
+
+def get_survival_time(attacker, target):
+    defenses = get_defenses(target)
+    best_attack = find_best_attack(attacker.attacks, defenses, allow_action_points=True)
+    average_damage = get_average_damage(best_attack, defenses, target.damage_reduction)
+    if average_damage == 0:
+        return 100000
+    survival_time = target.hit_points / average_damage
+
+    # If we're using an AP spender and the combat lasts too long, we have to
+    # switch our attack.
+    if best_attack.action_point and survival_time > attacker.recovery_action_points:
+        hp_after_ap = target.hit_points - average_damage * attacker.recovery_action_points
+        best_attack = find_best_attack(attacker.attacks, get_defenses(target), allow_action_points=False)
+        average_damage = get_average_damage(best_attack, defenses, target.damage_reduction)
+        if average_damage == 0:
+            return 50000
+        survival_time = attacker.recovery_action_points + hp_after_ap / average_damage
+
+    return survival_time
 
 
 def get_defenses(creature):
@@ -45,11 +57,14 @@ def get_defenses(creature):
     }
 
 
-def find_best_attack(attacks, target):
+def find_best_attack(attacks, target, allow_action_points):
     """Given a set of attacks, return the optimal attack to attack with"""
     best_average_damage = 0
     best_attack = None
     for attack in attacks:
+        if attack.action_point and not allow_action_points:
+            continue
+
         average_damage = get_average_damage(attack, target)
         if average_damage > best_average_damage:
             best_average_damage = average_damage
@@ -59,11 +74,33 @@ def find_best_attack(attacks, target):
     return best_attack
 
 
-def get_average_damage(attack, defenses):
+# Damage reduction is not always required; it's irrelevant when calculating
+# which attack is the best attack, for example.
+def get_average_damage(attack, defenses, damage_reduction=0):
     """Given a attack and a set of defenses, return the average damage for that attack"""
-    return attack.damage.average() * hit_chance(attack.accuracy, defenses[attack.defense])
+    # For convenience, ignore the possibility of double crits.
+    # We subtract damage reduction directly from attack damage instead of
+    # applying it to the result of this calculation. This slightly overstates
+    # the value of damage reduction, since it ignores that some DR is wasted on
+    # low-damage attacks.
+    damage_on_hit = attack.damage.average() - damage_reduction
+    return max(0, (
+        damage_on_hit * hit_chance(attack.accuracy, defenses[attack.defense])
+        # Ignore damage reduction when calculating extra damage from a crit
+        + attack.damage.average() * crit_chance(attack.accuracy, defenses[attack.defense])
+    ))
 
 
 def hit_chance(accuracy, defense):
     """Given an accuracy against a defense, return the chance to hit (range 0-1)"""
-    return min(1, max(0, (accuracy - defense + 11) / 10))
+    accuracy = min(1, max(0, (accuracy - defense + 11) / 10))
+    explosions = 1
+    while accuracy == 0:
+        # Take into account explosion
+        accuracy = min(1, max(0, (accuracy + 10 * explosions - defense + 11) / (10 * (10 ** explosions))))
+        explosions += 1
+    return accuracy
+
+
+def crit_chance(accuracy, defense):
+    return hit_chance(accuracy, defense + 10)
