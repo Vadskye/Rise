4328d7c81a9c7cfe13e6fc73f201ca88d4447c97
Kevin Johnson <vadskye@gmail.com>
2018-11-21 23:24:31 +0000

    make simulator modifier parsing more robust
    Former-commit-id: 23fd230223cd2de60c9181c07c1aa6a6731ae8d9

--- python/rise/simulator/test_combat.py
+++ python/rise/simulator/test_combat.py
@@ -1,42 +1,50 @@
 #!/usr/bin/env python3
 
 import click
+from copy import copy
 import re
 from rise.simulator.run_combat import run_combat
-from rise.statistics.sample_creatures import get_sample_creatures
+from rise.statistics.sample_creatures import get_sample_creatures, parse_creature
 
 def format_combat_results(results):
     return f"{results['blue_time_to_kill']} vs {results['red_time_to_kill']} rounds, total {results['round_count']}."
 
+
+def output_combat(sample_info, blue_text, red_text):
+    blue = parse_creature(sample_info, blue_text)
+    red = parse_creature(sample_info, red_text)
+
+    combat_results = run_combat(blue, red)
+    print(f"'{blue_text}' vs '{red_text}': {combat_results['winner']}"
+          + f"\n    {format_combat_results(combat_results)}")
+
 @click.command()
 @click.option('-b', '--blue-grep', default=None)
 @click.option('-g', '--grep', default=None)
-@click.option('-m', '--mix-levels/--no-mix-levels', default=False)
+@click.option('-l', '--levels', default=None)
+@click.option('-m', '--modifiers', default=None)
 @click.option('-r', '--red-grep', default=None)
-def main(blue_grep, grep, mix_levels, red_grep):
-    test_samples = get_sample_creatures()['tests']
+def main(blue_grep, grep, levels, modifiers, red_grep):
+    sample_info = get_sample_creatures()
 
-    sample_keys = sorted(test_samples.keys())
-    if grep:
-        sample_keys = list(filter(lambda key: re.search(grep, key), sample_keys))
+    blue_text = (blue_grep or grep) + (f" {modifiers}" if modifiers else "")
+    red_text = (red_grep or grep) + (f" {modifiers}" if modifiers else "")
 
-    blue_keys = sample_keys
-    if blue_grep:
-        blue_keys = list(filter(lambda key: re.search(blue_grep, key), blue_keys))
-
-    red_keys = sample_keys
-    if red_grep:
-        red_keys = list(filter(lambda key: re.search(red_grep, key), red_keys))
-
-    for blue_key in blue_keys:
-        blue = test_samples[blue_key]
-        for red_key in red_keys:
-            red = test_samples[red_key]
-            # We normally want to avoid having fights across different levels.
-            if blue.level == red.level or mix_levels:
-                combat_results = run_combat(blue, red)
-                print(f"'{blue_key}' vs '{red_key}': {combat_results['winner']}"
-                      + f"\n    {format_combat_results(combat_results)}")
+    if levels:
+        if levels == 'all':
+            levels = ' '.join(str(n) for n in range(1, 21))
+        if levels == 'odd':
+            levels = ' '.join(str(n) for n in range(1, 21, 2))
+        if levels == 'even':
+            levels = ' '.join(str(n) for n in range(2, 21, 2))
+        if levels == 'std':
+            levels = ' '.join(str(n) for n in range(1, 21, 5))
+        for level in sorted(int(n) for n in levels.split(' ')):
+            leveled_blue_text = blue_text + f" l{level}"
+            leveled_red_text = red_text + f" l{level}"
+            output_combat(sample_info, leveled_blue_text, leveled_red_text)
+    else:
+        output_combat(sample_info, blue_text, red_text)
 
 
 if __name__ == "__main__":
