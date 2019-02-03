8af448b02a76f74b5ed22b933163072ad8842a70
Kevin Johnson <vadskye@gmail.com>
2019-02-03 00:15:09 +0000

    add potions
    Former-commit-id: a34a80eae7aa9ff7b74cb01712d2b6335aab19d7

--- python/rise/latex_generation/gen_all.py
+++ python/rise/latex_generation/gen_all.py
@@ -5,6 +5,7 @@ import rise.latex_generation.gen_implements as gen_implements
 import rise.latex_generation.gen_maneuvers as gen_maneuvers
 import rise.latex_generation.gen_spell_descriptions as gen_spell_descriptions
 import rise.latex_generation.gen_spell_lists as gen_spell_lists
+import rise.latex_generation.gen_tools as gen_tools
 import rise.latex_generation.gen_weapons as gen_weapons
 import rise.latex_generation.monsters as monsters
 
@@ -14,6 +15,7 @@ def main():
     gen_maneuvers.write_to_file()
     gen_spell_descriptions.write_to_file()
     gen_spell_lists.write_to_file()
+    gen_tools.write_to_file()
     gen_weapons.write_to_file()
     monsters.write_to_file()
 
