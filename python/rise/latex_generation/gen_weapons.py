526702a64059467dc696853ddbe4932eb68bb4a6
Kevin Johnson <vadskye@gmail.com>
2019-05-11 18:34:26 +0000

    remove AP costs from weapons
    Former-commit-id: 1967b5caebf15a9896359b07de608f2c01321f18

--- python/rise/latex_generation/gen_weapons.py
+++ python/rise/latex_generation/gen_weapons.py
@@ -257,8 +257,8 @@ def generate_weapons():
         material_type='Weapon',
         description="""
             You can throw this weapon as if it was designed to be thrown.
-            In addition, as a standard action, you can spend an \\glossterm<action point> to throw this weapon in a spinning arc.
-            When you do, make a thrown \\glossterm<strike> against up to three targets within two range increments.
+            In addition, as a standard action, you can throw this weapon in a spinning arc.
+            When you do, make a thrown \\glossterm<strike> against up to three targets within two \\glossterm<range increments>.
             After attacking the last target, the weapon flies to your hand.
         """,
         short_description="Can be thrown to strike multiple foes",
@@ -319,7 +319,7 @@ def generate_weapons():
         material_type='Weapon',
         tags=['Shaping'],
         description="""
-             As a standard action, you can spend an \\glossterm<action point> to activate this item.
+             As a standard action, you can activate this weapon.
              If you do, it changes shape into a new weapon of your choice from the same weapon group.
         """,
         short_description="Can change into similar weapon",
@@ -331,7 +331,7 @@ def generate_weapons():
         material_type='Weapon',
         tags=['Shaping'],
         description="""
-             As a standard action, you can spend an \\glossterm<action point> to activate this item.
+             As a standard action, you can activate this weapon.
              If you do, it changes shape into a new weapon of your choice that you are proficient with.
              This can only change into existing manufactured weapons, not improvised weapons (see \\pcref<Weapons>).
         """,
@@ -423,7 +423,7 @@ def generate_weapons():
         material_type='Weapon',
         tags=['Shaping'],
         description="""
-            As a \\glossterm<standard action>, you can spend an \\glossterm<action point> to activate this weapon.
+            As a \\glossterm<standard action>, you can activate this weapon.
             If you do, make a \\glossterm<strike> or a \\glossterm<disarm> attack.
             If your disarm succeeds, or if your strike hit an unattended object, this weapon can absorb the struck object.
             The object must be at least one size category smaller than the weapon.
