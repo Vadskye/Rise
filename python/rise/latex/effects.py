486200dd79681eeb1e1f07889b66f0f1206249db
Kevin Johnson <vadskye@gmail.com>
2019-05-28 01:52:15 +0000

    add target style to cantrips

--- python/rise/latex/effects.py
+++ python/rise/latex/effects.py
@@ -3,16 +3,25 @@ from rise.latex.tags import glosstermify, is_valid_tag
 from rise.latex.util import join
 logger = getLogger(__name__)
 
+def targets_are_plural(target_text):
+    target_text = target_text.lower()
+    for keyword in ['any number', 'allies', 'enemies']:
+        if keyword in target_text:
+            return True
+    return False
+
 class Effects(object):
     def __init__(
             self,
             name,
+            targets,
             effect_text,
             tags=None,
     ):
         self.effect_text = effect_text
         self.name = name
         self.tags = tags
+        self.targets = targets
 
         if (self.tags):
             for tag in self.tags:
@@ -27,9 +36,13 @@ class Effects(object):
 
         ability_type = 'attuneability' if 'Attune' in tag_text else 'freeability'
 
+        target_tag = 'targets' if targets_are_plural(self.targets) else 'target'
+        target_text = f"\\{target_tag}<{self.targets}>" if self.targets else ""
+
         return join(
             f"""
                 \\begin<{ability_type}><{self.name}>{f"[{tag_text}]" if tag_text else ""}
+                    {target_text}
                     {self.effect_text.strip()}
                 \\end<{ability_type}>
             """
