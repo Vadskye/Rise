4596e7e4626ea58b5daa6ec6384e07622d9f1150
Kevin Johnson <vadskye@gmail.com>
2019-09-12 15:52:02 +0000

    make weapon inputs safer
    Former-commit-id: d93323111dcd05ec3c2913ecc4a73a33a4716344

--- typescript/src/weapons.ts
+++ typescript/src/weapons.ts
@@ -1,11 +1,17 @@
 import { DamageType } from "@src/data";
 
-export interface WeaponInput {
+interface StandardWeaponInput {
+  name: StandardWeaponName;
+}
+
+interface CustomWeaponInput {
   accuracyBonus?: number;
   damageBonus?: number;
-  damageTypes?: DamageType[];
+  damageTypes: DamageType[];
   name: string;
 }
+
+export type WeaponInput = StandardWeaponInput | CustomWeaponInput;
 
 export type Weapon = Required<WeaponInput>;
 
@@ -13,13 +19,24 @@ export function parseWeaponInput(input: WeaponInput): Weapon {
   return {
     accuracyBonus: 0,
     damageBonus: 0,
-    damageTypes: [],
-    ...standardWeapons[input.name],
+    ...(isStandardWeaponName(input.name) && standardWeapons[input.name]),
     ...input,
   };
 }
 
-const standardWeapons: Record<string, Omit<WeaponInput, "name">> = {
+// This is somewhat cumbersome to write, but it ensure that we specify a damage type for any
+// intentionally custom weapons. For the foreseeable future, it's likely that any "custom" weapon is
+// actually just a missing standard weapon, so it's worth the duplication.
+type StandardWeaponName = "bite" | "greatsword" | "longsword" | "short sword";
+
+function isStandardWeaponName(name: StandardWeaponName | string): name is StandardWeaponName {
+  return Boolean(standardWeapons[name as StandardWeaponName]);
+}
+
+const standardWeapons: Record<StandardWeaponName, Omit<WeaponInput, "name">> = {
+  "bite": {
+    damageTypes: ["bludgeoning", "slashing"],
+  },
   "greatsword": {
     damageBonus: 1,
     damageTypes: ["slashing"],
