23c4559c111a8d450e68341bcc40829c12e9da76
Kevin Johnson <vadskye@gmail.com>
2019-09-12 15:45:18 +0000

    add infra to support weapons
    Former-commit-id: 4a2457efc0953531aa6f8177da604a4a3d0daf30

--- typescript/src/weapons.ts
+++ typescript/src/weapons.ts
@@ -1,7 +1,34 @@
-// TODO: add subtypes
-export type DamageType = "physical" | "energy" | null;
+import { DamageType } from "@src/data";
 
-export interface Weapon {
-  damageTypes: DamageType[];
+export interface WeaponInput {
+  accuracyBonus?: number;
+  damageBonus?: number;
+  damageTypes?: DamageType[];
   name: string;
 }
+
+export type Weapon = Required<WeaponInput>;
+
+export function parseWeaponInput(input: WeaponInput): Weapon {
+  return {
+    accuracyBonus: 0,
+    damageBonus: 0,
+    damageTypes: [],
+    ...standardWeapons[input.name],
+    ...input,
+  };
+}
+
+const standardWeapons: Record<string, Omit<WeaponInput, "name">> = {
+  "greatsword": {
+    damageBonus: 1,
+    damageTypes: ["slashing"],
+  },
+  "longsword": {
+    damageTypes: ["slashing"],
+  },
+  "short sword": {
+    damageBonus: -1,
+    damageTypes: ["slashing"],
+  },
+};
