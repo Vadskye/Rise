75dd0fd841674753898ea0ac94d7f5e1fcdfc7a3
Kevin Johnson <vadskye@gmail.com>
2019-09-22 20:35:06 +0000

    add support for strike calculations
    Former-commit-id: a001f5404f9921725baa82cf9e9f87f6fc7d36aa

---
+++ typescript/src/calculate/calculate_strike.ts
@@ -0,0 +1,9 @@
+import { parseAttack } from "@src/attacks";
+import { MonsterBase } from "@src/monsters";
+import { StandardWeaponName, Weapon } from "@src/weapons";
+import { calculateAttack, CalculatedAttack } from "./calculate_attack";
+
+export function calculateStrike(monster: MonsterBase, weapon: Weapon): CalculatedAttack {
+  const attack = parseAttack({ name: weapon.name, weaponName: weapon.name as StandardWeaponName });
+  return calculateAttack(attack, monster);
+}
