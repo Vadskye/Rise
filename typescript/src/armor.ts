2393fd59b56cba99b92d535c50e225031c382e21
Kevin Johnson <vadskye@gmail.com>
2019-09-30 19:49:02 +0000

    add infra to support arbitrary attacks

--- typescript/src/armor.ts
+++ typescript/src/armor.ts
@@ -1,5 +1,87 @@
-export interface Armor {
-  armorDefenseBonus: number;
-  name: string;
-  resistanceBonus: number;
+import { DefenseType, defenseTypes, ResistanceType, resistanceTypes } from "@src/data";
+import { fromPairs } from "@src/util/from_pairs";
+
+interface StandardArmorInput {
+  name: StandardArmorName;
 }
+
+interface CustomArmorInput {
+  defenseBonuses: Partial<Record<DefenseType, number>>;
+  resistanceBonuses: Partial<Record<ResistanceType, number>>;
+  name: string;
+}
+
+export type ArmorInput = StandardArmorInput | CustomArmorInput;
+
+export interface Armor {
+  defenseBonuses: Record<DefenseType, number>;
+  name: string;
+  resistanceBonuses: Record<ResistanceType, number>;
+}
+
+export function parseArmorInput(input: ArmorInput): Armor {
+  const armorDefenseBonuses: Record<DefenseType, number> = Object.assign(
+    {},
+    fromPairs(defenseTypes.map((d) => [d, 0])),
+    isStandardArmorInput(input) ? standardArmors[input.name].defenseBonuses : input.defenseBonuses,
+  );
+  const armorResistanceBonuses: Record<ResistanceType, number> = Object.assign(
+    {},
+    fromPairs(resistanceTypes.map((r) => [r, 0])),
+    isStandardArmorInput(input)
+      ? standardArmors[input.name].resistanceBonuses
+      : input.resistanceBonuses,
+  );
+  return {
+    defenseBonuses: armorDefenseBonuses,
+    name: input.name,
+    resistanceBonuses: armorResistanceBonuses,
+  };
+}
+
+// This is somewhat cumbersome to write, but it ensure that we specify a damage type for any
+// intentionally custom armor. For the foreseeable future, it's likely that any "custom" weapon is
+// actually just a missing standard weapon, so it's worth the duplication.
+export type StandardArmorName =
+  | "breastplate"
+  | "full plate"
+  | "heavy shield"
+  | "tough hide"
+  | "scales"
+  | "thick skin"
+  | "fur";
+
+export function isStandardArmorInput(input: ArmorInput): input is StandardArmorInput {
+  return Boolean(standardArmors[(input as StandardArmorInput).name]);
+}
+
+export const standardArmors: Record<StandardArmorName, Omit<CustomArmorInput, "name">> = {
+  "full plate": {
+    defenseBonuses: { armor: 4 },
+    resistanceBonuses: { physical: 6 },
+  },
+  "heavy shield": {
+    defenseBonuses: { armor: 2 },
+    resistanceBonuses: {},
+  },
+  "breastplate": {
+    defenseBonuses: { armor: 3 },
+    resistanceBonuses: { physical: 4 },
+  },
+  "tough hide": {
+    defenseBonuses: { armor: 3 },
+    resistanceBonuses: { physical: 4 },
+  },
+  "fur": {
+    defenseBonuses: { armor: 2 },
+    resistanceBonuses: { physical: 2 },
+  },
+  "scales": {
+    defenseBonuses: { armor: 3 },
+    resistanceBonuses: { physical: 2 },
+  },
+  "thick skin": {
+    defenseBonuses: { armor: 2 },
+    resistanceBonuses: { physical: 2 },
+  },
+};
--- typescript/src/monsters/types/animals.ts
+++ typescript/src/monsters/types/animals.ts
@@ -1,41 +1,53 @@
 import { addType, TypelessMonsterInput } from "./add_type";
 
 const animalInput: TypelessMonsterInput[] = [
-  // Brown bear
   {
+    armorInputs: [{ name: "fur" }],
+    challengeRating: 2,
     level: 3,
     name: "Black bear",
-    startingAttributes: { str: 3, con: 2, int: -8, wil: -1 },
+    startingAttributes: { str: 3, con: 3, int: -8, wil: -1 },
     weaponInput: [{ name: "bite" }],
   },
   {
+    armorInputs: [{ name: "fur" }],
     challengeRating: 2,
     level: 5,
     name: "Brown bear",
     size: "large",
-    startingAttributes: { str: 4, con: 2, int: -8, wil: -1 },
+    startingAttributes: { str: 4, con: 4, int: -8, wil: -1 },
     weaponInput: [{ name: "bite" }],
   },
   {
-    attackInputs: [{ name: "Strike", weaponName: "bite" }],
+    armorInputs: [{ name: "fur" }],
     level: 2,
     name: "Wolf",
     startingAttributes: { str: 1, dex: 2, con: 1, int: -7, wil: -1 },
     weaponInput: [{ name: "bite" }],
   },
   {
+    armorInputs: [{ name: "thick skin" }],
     level: 2,
     name: "Pony",
     startingAttributes: { str: 1, con: 3, int: -7, wil: -1 },
     weaponInput: [{ powerBonus: -2, name: "bite" }],
   },
   {
+    armorInputs: [{ name: "thick skin" }],
     challengeRating: 4,
     level: 9,
     name: "Roc",
     size: "gargantuan",
     startingAttributes: { str: 4, con: 3, int: -7, wil: -1 },
     weaponInput: [{ name: "bite" }],
+  },
+  {
+    armorInputs: [{ name: "scales" }],
+    challengeRating: 2,
+    level: 6,
+    name: "Vampire Eel",
+    size: "large",
+    startingAttributes: { str: 3, dex: 3, con: 2, int: -8, per: 0, wil: -1 },
   },
 ];
 
