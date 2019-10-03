136188a57fe298a86f6ca444ad08bbfc141eb928
Kevin Johnson <vadskye@gmail.com>
2019-10-03 04:29:40 +0000

    update sample maneuvers that affect areas

--- python/rise/latex/maneuver.py
+++ python/rise/latex/maneuver.py
@@ -11,19 +11,24 @@ class Maneuver(object):
             rank_upgrades,
             short_description,
             tags,
+            target=None,
             extra_text=None,
+            rank=None,
     ):
         self.effect_text = effect_text
         self.name = name
         self.lists = lists
+        self.rank = rank or 1
         self.rank_upgrades = rank_upgrades
         self.short_description = short_description
         self.tags = tags
+        self.target = target
 
         self.extra_text = extra_text
 
-        if list(sorted(self.rank_upgrades.keys())) != ['3', '5', '7']:
-            logger.log(WARNING, f"Maneuver {self.name} has invalid rank upgrades {self.rank_upgrades}")
+        lowest_rank_upgrade = int(sorted(self.rank_upgrades.keys())[0])
+        if lowest_rank_upgrade != self.rank + 2:
+            logger.log(WARNING, f"Maneuver {self.name} with rank {self.rank} has invalid rank upgrades {self.rank_upgrades}")
 
         for tag in self.tags:
             if not is_valid_tag(tag):
@@ -33,16 +38,24 @@ class Maneuver(object):
         tag_text = to_latex_tags(self.tags)
         ability_type = 'attuneability' if 'Attune' in tag_text else 'freeability'
 
+        ranks = sorted(self.rank_upgrades.keys())
+        rank_text = (
+            '\\rankline\n' + '\n'.join([f"\\rank<{rank}> {self.rank_upgrades[rank].strip()}" for rank in ranks])
+            if len(ranks) > 0
+            else ''
+        )
+
+        target_tag = 'target' if self.target and self.target.startswith('One') else 'targets'
+        target_text = f"\\{target_tag}<{self.target}>" if self.target else ''
+
         return f"""
             \\lowercase<\\hypertarget<maneuver:{self.name}><>>\\label<maneuver:{self.name}>
             \\hypertarget<maneuver:{self.name}><>
-            \\begin<{ability_type}><{self.name}>{tag_text}
+            \\begin<{ability_type}>[Rank {self.rank}]<{self.name}>{tag_text}
+                {target_text}
                 {self.effect_text.strip()}
 
-                \\rankline
-                \\rank<3> {self.rank_upgrades['3'].strip()}
-                \\rank<5> {self.rank_upgrades['5'].strip()}
-                \\rank<7> {self.rank_upgrades['7'].strip()}
+                {rank_text}
                 \\parhead<Lists> {', '.join(sorted(self.lists))}
             \\end<{ability_type}>
             \\vspace<0.25em>
