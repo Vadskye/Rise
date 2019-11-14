9b6352c682910df2a0728f723a87060fa84af246
Kevin Johnson <vadskye@gmail.com>
2019-11-14 07:05:05 +0000

    add monstrous humanoids to monsters
    Former-commit-id: 52f9aa77137e8d91667eff4e02efffe84f0bb80e

--- typescript/src/monsters/types/types.ts
+++ typescript/src/monsters/types/types.ts
@@ -3,14 +3,22 @@ import { aberrations } from "./aberrations";
 import { animals } from "./animals";
 import { animates } from "./animates";
 import { humanoids } from "./humanoids";
+import { monstrousHumanoids } from "./monstrous_humanoids";
 
-export type MonsterType = "aberration" | "animal" | "animate" | "humanoid";
+export type MonsterType = "aberration" | "animal" | "animate" | "humanoid" | "monstrous humanoid";
 
-export const monsterTypes: MonsterType[] = ["aberration", "animal", "animate", "humanoid"];
+export const monsterTypes: MonsterType[] = [
+  "aberration",
+  "animal",
+  "animate",
+  "humanoid",
+  "monstrous humanoid",
+];
 
 export const monsterInputsByType: Record<MonsterType, MonsterInput[]> = {
-  aberration: aberrations,
-  animal: animals,
-  humanoid: humanoids,
-  animate: animates,
+  "aberration": aberrations,
+  "animal": animals,
+  "animate": animates,
+  "humanoid": humanoids,
+  "monstrous humanoid": monstrousHumanoids,
 };
