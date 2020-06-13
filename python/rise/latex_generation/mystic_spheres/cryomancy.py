9c866fe7c8758e6b7de3a4941ef83e496f2d04c8
Kevin Johnson <vadskye@gmail.com>
2020-06-12 17:48:21 -0700

    add more cyromancy spells
    Former-commit-id: 4841a570e8d7fa5713dccbd2b2cb55e9f23f32ca

--- python/rise/latex_generation/mystic_spheres/cryomancy.py
+++ python/rise/latex_generation/mystic_spheres/cryomancy.py
@@ -44,6 +44,87 @@ cryomancy=MysticSphere(
             \\rank<5> The damage increases to \\glossterm<standard damage> +1d.
             \\rank<7> The damage increases to \\glossterm<standard damage> +2d.
         """, tags=[]),
+        Spell('Frozen Legs', 5, 'One creature within \\rngclose range', """
+            Make an attack vs. Fortitude against the target.
+            \\hit The target is \\glossterm<immobilized> as a \\glossterm<condition>.
+
+            \\rankline
+            \\rank<7> The range increases to \\rngmed.
+        """, tags=[]),
+        Spell('Ice Lance', 3, 'Everything in a \\arealarge, 5 ft.\\ wide line from you', """
+            Make an attack vs. Armor against each target.
+            \\hit Each target takes piercing and cold \\glossterm<standard damage> -1d.
+
+            \\rankline
+            \\rank<5> The damage increases to \\glossterm<standard damage>.
+            \\rank<7> The damage increases to \\glossterm<standard damage> +1d.
+        """, tags=['Manifestation']),
+        Spell('Ice Spike', 1, 'One creature or object within \\rngclose range', """
+            Make an attack vs. Armor against the target.
+            \\hit The target takes piercing and cold \\glossterm<standard damage>.
+
+            \\rankline
+            \\rank<3> The damage increases to \\glossterm<standard damage> +1d.
+            \\rank<5> The damage increases to \\glossterm<standard damage> +2d.
+            \\rank<7> The damage increases to \\glossterm<standard damage> +3d.
+        """, tags=['Manifestation']),
+        Spell('Freeze Poison', 1, 'Yourself or one \\glossterm<ally> within \\rngmed range', """
+            The target takes cold damage equal to your \\glossterm<power>.
+            In addition, it gains an additional success to resist a poison currently affecting it (see \\pcref<Poisons>).
+
+            \\rankline
+            \\rank<3> The number of additional successes increases to two.
+            \\rank<5> The number of additional successes increases to three, which is enough to remove most poisons immediately.
+            \\rank<7> The target can also gain the same number of successes to remove an additional poison affecting it.
+        """, tags=[]),
+        Spell('Brittle Chill', 4, 'One creature or object within \\rngmed range', """
+            Make an attack vs. Fortitude against the target.
+            \\hit The target takes cold \\glossterm<standard damage> -2d.
+            In addition, it becomes \\glossterm<vulnerable> to bludgeoning damage until the end of the round.
+
+            \\rankline
+            \\rank<6> The vulnerability lasts until the end of the next round.
+        """, tags=[]),
+        Spell('Chilled Mind', 3, 'One creature within \\rngmed range', """
+            Make an attack vs. Fortitude against the target.
+            \\hit The target is \\glossterm<stunned> as a \\glossterm<condition>.
+
+            \\rankline
+            \\rank<5> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
+            \\rank<7> The accuracy bonus increases to +2.
+        """, tags=[]),
+        Spell('Skate', 1, 'Yourself or an \\glossterm<ally> within \\rngmed range', """
+            The target can move on top of calm water as if it were land.
+            It treats the water as \\glossterm<difficult terrain>.
+
+            \\rankline
+            \\rank<3> The target can also move on top of rough water.
+            \\rank<5> The target can also move on top of stormy water.
+            \\rank<7> The target no longer treats the water as difficult terrain.
+        """, tags=['Attune (target)']),
+        Spell('Skyskate', 3, 'Yourself', """
+            Whenever you move, you can leave a trail of ice behind you.
+            The ice lasts until the end of the round before disappearing.
+
+            While you are leaving a trail of ice behind you, you can move into thin air by walking on your own ice trail, just as if it was solid ground.
+            If you are still standing on your own ice trail when it disappears at the end of the round, you fall.
+
+            \\rankline
+            \\rank<5> Your ice trail collapses more gradually.  If you are still standing on your own ice trail when it disappears, you can fall up to 50 feet before you start taking \\glossterm<falling damage>.
+            \\rank<7> Your ice trail lasts until the end of the next round after your movement.
+        """, tags=['Attune (self)', 'Manifestation']),
+        Spell('Icy Shell', 1, 'Yourself', """
+            You cover your body with three overlapping layers of ice that crumple when they take damage.
+            The ice does not cover your joints, allowing you to move freely.
+            You are \\glossterm<resistant> to \\glossterm<physical damage> and \\glossterm<fire damage>.
+            Whenever you take physical damage or fire damage, one layer of ice is destroyed.
+            When the last layer of ice is destroyed, this ability provides no further benefit.
+
+            \\rankline
+            \\rank<3> The spell creates five layers of ice.
+            \\rank<5> The spell creates seven layers of ice.
+            \\rank<7> The spell creates ten layers of ice.
+        """, tags=['Attune (self)', 'Manifestation']),
         Spell('Winterwave', 3, 'Everything in a \\arealarge cone from you', """
             Make an attack vs. Fortitude against each target.
             \\hit Each target takes cold \\glossterm<standard damage> -1d.
