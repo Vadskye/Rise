5e62dba5a114bcdbdcc52f8dfc073e3d3c201910
Kevin Johnson <vadskye@gmail.com>
2020-06-13 21:52:40 -0700

    expand verdamancy
    Former-commit-id: 672aaf20836b9a4f35313a1fbb1d7bc81d239580

--- python/rise/latex_generation/mystic_spheres/verdamancy.py
+++ python/rise/latex_generation/mystic_spheres/verdamancy.py
@@ -12,36 +12,143 @@ verdamancy=MysticSphere(
     short_description="Animate and manipulate plants",
     cantrips=[
         Effects('Rapid Growth', 'Small or smaller inanimate plant within \\rngclose range', """
-            The target grows as if a month of time had passed.
+            Choose any number of days up to a week.
+            The target grows as if that many days had passed.
             When this spell ends, the plant returns to its original state.
+
+            \\rankline
+            \\rank<3> You can choose up to a month of time to grow.
+            \\rank<5> You can choose up to three months months of time to grow.
+            \\rank<7> You can choose up to a year of time to grow.
         """, tags=['Sustain (minor)']),
+        Effects('Verdant Conduit', 'Yourself', """
+            You are considered to be a source of unworked earth.
+            This can allow you to cast spells from this sphere that require unworked earth without having other sources of unworked earth nearby.
+
+            \\rankline
+            \\rank<3> All components of Medium or larger solid objects within a \\areasmall \\glossterm<emanation> from you are also considered to be sources of unworked earth.
+            Unlike most spells, this can affect only part of a larger object, and it only affects the part of the object within the radius.
+            \\rank<5> The area of the emanation increases to a \\areamed radius.
+            \\rank<7> The area of the emanation increases to a \\arealarge radius.
+        """, tags=['Attune (self)']),
     ],
     lists=['Nature'],
     spells=[
-        Spell('Entangle', 1, 'One Large or smaller creature within \\rngclose range', """
+        Spell('Entangle', 3, 'One Large or smaller creature within \\rngclose range', """
             You cause plants to grow and trap a foe.
+            The target must be within 10 feet of unworked earth or plants.
             Make an attack vs. Reflex against the target.
-            The target must be within 5 feet of earth or plants.
             You gain a +2 bonus to \\glossterm<accuracy> with this attack if the target is in standing in \\glossterm<undergrowth>.
             \\hit The target is \\glossterm<immobilized> as a \\glossterm<condition>.
-            This condition can be removed if the target or a creature that can reach the target makes a \\glossterm<difficulty rating> 5 Strength check break the target free of the plants.
+            This condition can be removed if the target or a creature that can reach the target makes a \\glossterm<difficulty rating> 10 Strength check to break the target free of the plants.
             The target can make this check as a \\glossterm<move action>, while other creatures can make the check as a standard action.
 
             \\rankline
-            \\rank<3> The \\glossterm<difficulty rating> of the Strength check increases to 10.
             \\rank<5> The condition cannot be removed with a check.
             \\rank<7> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
-        """, tags=[]),
-        Spell('Embedded Growth', 1, 'One creature within \\rngclose range', """
-            You throw a seed that embeds itself in a foe and grows painfully.
+        """, tags=['Manifestation']),
+        Spell('Vine Whip', 1, 'One creature within \\rngmed range', """
+            The target must be within 30 feet of unworked earth or plants.
+            Make an attack vs. Armor against the target.
+            You gain a +2 bonus to \\glossterm<accuracy> with this attack if the target is in standing in \\glossterm<undergrowth>.
+            \\hit The target takes bludgeoning \\glossterm<standard damage> +1d.
+
+            \\rankline
+            \\rank<3> The damage increases to \\glossterm<standard damage> +2d.
+            \\rank<5> The damage increases to \\glossterm<standard damage> +3d.
+            \\rank<7> The damage increases to \\glossterm<standard damage> +4d.
+        """, tags=['Manifestation']),
+        Spell('Strangling Vines', 6, 'One Large or smaller creature within \\rngclose range', """
+            The target must be within 10 feet of unworked earth or plants.
             Make an attack vs. Fortitude against the target.
-            \\hit As a \\glossterm<condition>, the target takes physical \\glossterm<standard damage> during each \\glossterm<action phase> in subsequent rounds.
-            This condition can be removed if the target or a creature that can reach the target makes a \\glossterm<difficulty rating> 5 Medicine check as a standard action to remove the seed (see \\pcref<Treat Condition>).
+            You gain a +2 bonus to \\glossterm<accuracy> with this attack if the target is in standing in \\glossterm<undergrowth>.
+            \\hit The target is \\glossterm<immobilized> by a mass of vines as a \\glossterm<condition>.
+            In addition, it is unable to breathe, and it takes bludgeoning \\glossterm<standard damage> -3d at the end of each round.
+            This condition can be removed if the target or a creature that can reach the target makes a Strength check to break the target free of the vines.
+            The \\glossterm<difficulty rating> of the check is equal to your \\glossterm<power>.
+            The target can make this check as a \\glossterm<move action>, while other creatures can make the check as a standard action.
+
+            \\rankline
+            \\rank<8> The \\glossterm<difficulty rating> to escape the condition increases by 5.
+        """, tags=['Manifestation']),
+        Spell('Vineburst', 1, '\\glossterm<Enemies> and objects in a \\areasmall radius from you', """
+            Make an attack vs. Armor against each target that is within 10 feet of unworked earth or plants..
+            You gain a +2 bonus to \\glossterm<accuracy> with this attack against targets standing in \\glossterm<undergrowth>.
+            \\hit Each target takes bludgeoning \\glossterm<standard damage> -1d.
+
+            \\rankline
+            \\rank<3> The damage increases to \\glossterm<standard damage>.
+            \\rank<5> The damage increases to \\glossterm<standard damage> +1d.
+            \\rank<7> The damage increases to \\glossterm<standard damage> +2d.
+        """, tags=['Manifestation']),
+        Spell('Vine Tentacles', 1, 'Yourself', """
+            You grow vine tentacles from your body.
+            The tentacles grant you a slam \\glossterm<natural weapon> (see \\tref<Natural Weapons>).
+            The natural weapon has the Long \\glossterm<weapon tag> and does not require a \\glossterm<free hand> to use (see \\pcref<Weapon Tags>).
+
+            \\rankline
+            \\rank<3> You gain a +1 \\glossterm<magic bonus> to \\glossterm<accuracy> with attacks using the tentacles.
+            \\rank<5> You gain a +2 \\glossterm<magic bonus> to \\glossterm<power> with attacks using the tentacles.
+            \\rank<7> You gain a +5 bonus to \\glossterm<reach> with attacks using the tentacles.
+        """, tags=['Attune (self)', 'Manifestation']),
+        Spell('Poison -- Nitharit', 1, 'One living creature within \\rngmed range', """
+            Make an attack vs. Fortitude against the target.
+            \\hit The target becomes \\glossterm<poisoned> with nitharit.
+            It becomes \\glossterm<sickened> as long as it is poisoned.
+            At the end of each subsequent round, you repeat this attack, as normal for poisons.
+            A third successful attack causes the target to become \\glossterm<nauseated> as long as it is poisoned.
+            A third failed attack ends the poison.
 
             \\rankline
             \\rank<3> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
             \\rank<5> The accuracy bonus increases to +2.
             \\rank<7> The accuracy bonus increases to +3.
+        """, tags=['Manifestation', 'Sustain (minor)']),
+        Spell('Poison -- Sassone Leaf', 3, 'One living creature within \\rngmed range', """
+            This spell functions like this \\spell<poison -- nitharit> spell, except that the target becomes poisoned with sassone leaf instead (see \\pcref<Poisons>).
+            A creature poisoned by sassone leaf loses a \\glossterm<hit point> from each successful attack, including the first.
+            A third successful attack causes the target to lose an additional hit point and ends the poison.
+            A third failed attack ends the poison.
+
+            \\rankline
+            \\rank<5> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
+            \\rank<7> The accuracy bonus increases to +2.
+        """, tags=[]),
+        Spell('Poison -- Black Lotus', 5, 'One living creature within \\rngmed range', """
+            This spell functions like this \\spell<poison -- nitharit> spell, except that the target becomes poisoned with black lotus extract instead (see \\pcref<Poisons>).
+            A creature poisoned by black lotus extract loses a \\glossterm<hit point> from each successful attack, including the first.
+            A third successful attack causes the target to gain a \\glossterm<vital wound>.
+            A third failed attack ends the poison.
+
+            \\rankline
+            \\rank<7> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
+        """, tags=['Manifestation']),
+        Spell('Herbal Antidote', 1, 'Yourself or an \\glossterm<ally> within \\rngmed range', """
+            The target gains an additional success to resist a poison currently affecting it (see \\pcref<Poisons>).
+
+            \\rankline
+            \\rank<3> The number of additional successes increases to two.
+            \\rank<5> The number of additional successes increases to three.
+            \\rank<7> The number of additional successes increases to four.
+        """, tags=['Sustain (minor)']),
+        Spell('Barkskin', 1, 'Yourself', """
+            You gain a +2 \\glossterm<magic bonus> to \\glossterm<resistances> against \\glossterm<physical> damage.
+
+            You can cast this spell as a \\glossterm<minor action>.
+
+            \\rankline
+            \\rank<3> The bonus increases to +4.
+            \\rank<5> The bonus increases to be equal to half your \\glossterm<power>.
+            \\rank<7> The bonus increases to be equal to your \\glossterm<power>.
+        """, tags=['Attune (target)']),
+        Spell('Embedded Growth', 4, 'One creature within \\rngclose range', """
+            You throw a seed that embeds itself in a foe and grows painfully.
+            Make an attack vs. Fortitude against the target.
+            \\hit As a \\glossterm<condition>, the target takes physical \\glossterm<standard damage> at the end of each round.
+
+            \\rankline
+            \\rank<6> You gain a +1 bonus to \\glossterm<accuracy> with the attack.
+            \\rank<8> The accuracy bonus increases to +2.
         """, tags=[]),
         Spell('Fire Seeds', 3, 'One unattended acorn or similar seed structure you touch', """
             % Does "seed structure" make sense?
@@ -142,12 +249,13 @@ verdamancy=MysticSphere(
         Spell('Verdant Armor', 1, 'Yourself', """
             When you cast this spell, you choose a type of armor you are proficient with that is not normally made from metal.
             Plants grow around your body, functioning like your chosen type of armor for you, except that the \\glossterm<encumbrance penalty> of the armor is reduced by 2.
+            These plants are considered to be normal plants for the purpose of abilities that require plants to be near targets, such as spells from this mystic sphere.
 
             \\rankline
             \\rank<3> You also gain a +1 bonus to Armor defense.
             \\rank<5> The encumbrance penalty reduction is increased to 3.
             \\rank<7> The bonus to Armor defense increases to +2.
-        """, tags=['Attune (self)']),
+        """, tags=['Attune (self)', 'Manifestation']),
     ],
     rituals=[
         Spell('Fertility', 3, None, """
