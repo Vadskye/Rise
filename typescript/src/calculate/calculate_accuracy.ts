f6a744e24ea859ec42b91311484f5f5ada94b4dd
Kevin Johnson <vadskye@gmail.com>
2020-07-26 13:09:08 -0700

    sync roll20 monster level bonus with book
    Former-commit-id: c5124c60651e9690b743e4e0889c45ab3c775a3c

--- typescript/src/calculate/calculate_accuracy.ts
+++ typescript/src/calculate/calculate_accuracy.ts
@@ -1,6 +1,5 @@
 import { MonsterBase } from "@src/monsters";
 import { calculateChallengeRatingBonus } from "./calculate_challenge_rating_bonus";
-import { monsterBonusByLevel } from "./monster_bonus_by_level";
 
 export function calculateAccuracy({
   accuracyBonus,
@@ -9,11 +8,9 @@ export function calculateAccuracy({
   attributes,
 }: Pick<MonsterBase, "accuracyBonus" | "attributes" | "challengeRating" | "level">): number {
   const crBonus = calculateChallengeRatingBonus(challengeRating);
-  const monsterBonus = monsterBonusByLevel(level);
   return (
     Math.max(level, attributes.per ?? 0) +
     accuracyBonus +
-    monsterBonus +
     crBonus +
     Math.min(0, attributes.per ?? 0)
   );
--- typescript/src/calculate/calculate_defenses.ts
+++ typescript/src/calculate/calculate_defenses.ts
@@ -2,7 +2,6 @@ import { defenseBonusesByMonsterType } from "@src/calculate/defense_bonuses_by_m
 import { DefenseType } from "@src/data";
 import { MonsterBase } from "@src/monsters";
 import { calculateChallengeRatingBonus } from "./calculate_challenge_rating_bonus";
-import { monsterBonusByLevel } from "./monster_bonus_by_level";
 
 export function calculateDefenses({
   challengeRating,
@@ -16,35 +15,18 @@ export function calculateDefenses({
 >): Record<DefenseType, number> {
   const crBonus = calculateChallengeRatingBonus(challengeRating);
   const typeBonuses = defenseBonusesByMonsterType(monsterType);
-  const monsterBonus = monsterBonusByLevel(level);
   return {
     armor:
-      level +
-      defenseBonuses.armor +
-      typeBonuses.armor +
-      (startingAttributes.dex ?? 0) +
-      monsterBonus +
-      crBonus,
+      level + defenseBonuses.armor + typeBonuses.armor + (startingAttributes.dex ?? 0) + crBonus,
     fortitude:
       level +
       defenseBonuses.fortitude +
       typeBonuses.fortitude +
       (startingAttributes.con ?? 0) +
-      monsterBonus +
       crBonus,
     reflex:
-      level +
-      defenseBonuses.reflex +
-      typeBonuses.reflex +
-      (startingAttributes.dex ?? 0) +
-      monsterBonus +
-      crBonus,
+      level + defenseBonuses.reflex + typeBonuses.reflex + (startingAttributes.dex ?? 0) + crBonus,
     mental:
-      level +
-      defenseBonuses.mental +
-      typeBonuses.mental +
-      (startingAttributes.wil ?? 0) +
-      monsterBonus +
-      crBonus,
+      level + defenseBonuses.mental + typeBonuses.mental + (startingAttributes.wil ?? 0) + crBonus,
   };
 }
