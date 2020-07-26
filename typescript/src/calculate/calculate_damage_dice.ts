2bf467d725ee80e812e736fb1aeadd3969849e8a
Kevin Johnson <vadskye@gmail.com>
2020-07-26 12:47:46 -0700

    split damage dice calculation into separate function
    Former-commit-id: 35f68ee8cf57731e427cc8466f1d32a153fefb1f

---
+++ typescript/src/calculate/calculate_damage_dice.ts
@@ -0,0 +1,23 @@
+export interface DamageDice {
+  count: number;
+  size: number;
+}
+
+export function calculateDamageDice(power: number): DamageDice {
+  let count = 1;
+  let size = 8;
+  for (let i = 2; i <= power; i += 2) {
+    size += 2;
+    if (size > 10) {
+      if (count < 4) {
+        count *= 2;
+        size = 6;
+      } else {
+        count += 1;
+        size = 10;
+      }
+    }
+  }
+
+  return { count, size };
+}
