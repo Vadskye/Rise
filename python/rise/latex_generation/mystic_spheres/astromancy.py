13a7b676409e11ab1d2d81dbeff2dad750714b3b
Kevin Johnson <vadskye@gmail.com>
2020-08-23 14:27:49 -0700

    clarify controlled flicker
    Former-commit-id: a882786c97ef46926fe46fd34a1df09821b532f0

--- python/rise/latex_generation/mystic_spheres/astromancy.py
+++ python/rise/latex_generation/mystic_spheres/astromancy.py
@@ -172,6 +172,7 @@ astromancy=MysticSphere(
         """, tags=['Attune (self)']),
         Spell('Controlled Flicker', 3, 'Yourself', """
             This spell functions like the \\textit<flicker> spell, except that you can choose at the start of each round to stop flickering for that round.
+            If you do, your attacks do not have a failure chance, and attacks against you also do not have a failure chance.
 
             \\rank<5> The failure chance increases to 30\\%.
             \\rank<7> The failure chance increases to 40\\%.
