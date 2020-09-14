e949573bddeb534f6ef8f7151a188270e34936a5
Kevin Johnson <vadskye@gmail.com>
2020-09-14 11:32:30 -0700

    update implements for new attunement
    Former-commit-id: fdc1a4d65fe47f0742a51061bd72c3041ea54241

--- python/rise/latex_generation/gen_implements.py
+++ python/rise/latex_generation/gen_implements.py
@@ -8,50 +8,59 @@ from rise.latex.util import latexify, longtablify
 def generate_implements():
     implements = []
 
-    implements.append(MagicItem(
-        name="Spell Wand, 1st",
-        level=5,
-        material_type='Wand',
-        tags=[],
-        description="""
-            This wand grants you knowledge of a single 1st level spell.
-            You must have access to the \\glossterm<mystic sphere> that spell belongs to.
-        """,
-        short_description="Grants knowledge of a 1st level spell",
-    ))
-
-    implements.append(MagicItem(
-        name="Spell Wand, 2nd",
-        level=9,
-        material_type='Wand',
-        tags=[],
-        description="""
-            This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single 2nd level spell.
-        """,
-        short_description="Grants knowledge of a 2nd level spell",
-    ))
-
-    implements.append(MagicItem(
-        name="Spell Wand, 3rd",
-        level=13,
-        material_type='Wand',
-        tags=[],
-        description="""
-            This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single 3rd level spell.
-        """,
-        short_description="Grants knowledge of a 3rd level spell",
-    ))
-
-    implements.append(MagicItem(
-        name="Spell Wand, 4th",
-        level=17,
-        material_type='Wand',
-        tags=[],
-        description="""
-            This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single 4th level spell.
-        """,
-        short_description="Grants knowledge of a 4th level spell",
-    ))
+    implements += [
+        MagicItem(
+            name="Spell Wand, 1st",
+            level=5,
+            material_type='Wand',
+            tags=[],
+            description="""
+                This wand grants you knowledge of a single rank 1 spell.
+                You must have access to the \\glossterm<mystic sphere> that spell belongs to.
+            """,
+            short_description="Grants knowledge of a rank 1 spell",
+        ),
+        MagicItem(
+            name="Spell Wand, 2nd",
+            level=8,
+            material_type='Wand',
+            tags=[],
+            description="""
+                This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single rank 2 spell.
+            """,
+            short_description="Grants knowledge of a rank 2 spell",
+        ),
+        MagicItem(
+            name="Spell Wand, 3rd",
+            level=11,
+            material_type='Wand',
+            tags=[],
+            description="""
+                This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single rank 3 spell.
+            """,
+            short_description="Grants knowledge of a rank 3 spell",
+        ),
+        MagicItem(
+            name="Spell Wand, 4th",
+            level=14,
+            material_type='Wand',
+            tags=[],
+            description="""
+                This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single rank 4 spell.
+            """,
+            short_description="Grants knowledge of a rank 4 spell",
+        ),
+        MagicItem(
+            name="Spell Wand, 5th",
+            level=17,
+            material_type='Wand',
+            tags=[],
+            description="""
+                This item functions like a \\mitem<spell wand>, except that it grants knowledge of a single rank 5 spell.
+            """,
+            short_description="Grants knowledge of a rank 5 spell",
+        ),
+    ]
 
     implements.append(MagicItem(
         name="Staff of Transit",
@@ -64,25 +73,26 @@ def generate_implements():
         short_description="Doubles your teleportation distance",
     ))
 
-    implements.append(MagicItem(
-        name="Extending Staff",
-        level=10,
-        material_type='Staff',
-        description="""
-            You double the range of your \\glossterm<magical> abilities.
-        """,
-        short_description="Doubles range",
-    ))
-
-    implements.append(MagicItem(
-        name="Extending Staff, Greater",
-        level=19,
-        material_type='Staff',
-        description="""
-            You triple the range of your \\glossterm<magical> abilities.
-        """,
-        short_description="Triples range",
-    ))
+    implements += [
+        MagicItem(
+            name="Extending Staff",
+            level=10,
+            material_type='Staff',
+            description="""
+                You double the range of your \\glossterm<magical> abilities.
+            """,
+            short_description="Doubles range",
+        ),
+        MagicItem(
+            name="Extending Staff, Greater",
+            level=19,
+            material_type='Staff',
+            description="""
+                You triple the range of your \\glossterm<magical> abilities.
+            """,
+            short_description="Triples range",
+        ),
+    ]
 
     implements.append(MagicItem(
         name="Reaching Staff",
@@ -94,56 +104,69 @@ def generate_implements():
         short_description="Allows casting from a short distance away",
     ))
 
-    implements.append(MagicItem(
-        name="Protective Staff",
-        level=5,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +1 \\glossterm<magic bonus> to Armor defense.
-        """,
-        short_description="Grants +1 Armor defense",
-    ))
+    implements += [
+        MagicItem(
+            name="Protective Staff",
+            # +2 level since staff is secondary for this effect
+            level=7,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +1 \\glossterm<magic bonus> to Armor defense.
+            """,
+            short_description="Grants +1 Armor defense",
+        ),
+        MagicItem(
+            name="Protective Staff, Greater",
+            level=13,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +2 \\glossterm<magic bonus> to Armor defense.
+            """,
+            short_description="Grants +2 Armor defense",
+        ),
+        MagicItem(
+            name="Protective Staff, Supreme",
+            level=19,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +3 \\glossterm<magic bonus> to Armor defense.
+            """,
+            short_description="Grants +3 Armor defense",
+        ),
+    ]
 
-    implements.append(MagicItem(
-        name="Protective Staff, Greater",
-        level=14,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +2 \\glossterm<magic bonus> to Armor defense.
-        """,
-        short_description="Grants +2 Armor defense",
-    ))
-
-    implements.append(MagicItem(
-        name="Staff of Expansion",
-        level=7,
-        material_type='Staff',
-        tags=[],
-        description="""
-            When you use a \\glossterm<magical> ability that creates a \\glossterm<zone> or \\glossterm<emanation>, you can increase the size of the area by one size category, up to a maximum of \\areahuge.
-            You can only increase the area of one ability at a time in this way.
-            If you increase the area of another ability or lose this staff, the area of the original ability returns to its normal size.
-        """,
-        short_description="Increases area of emanations",
-    ))
-
-    implements.append(MagicItem(
-        name="Staff of Expansion, Greater",
-        level=16,
-        material_type='Staff',
-        tags=[],
-        description="""
-            This item functions like a \\textit<staff of expansion>, except that it increases the area by two size categories.
-            In addition, the maximum area is a 200 foot radius, which is one size category larger than \\areahuge.
-        """,
-        short_description="Greatly increases area of emanations",
-    ))
+    implements += [
+        MagicItem(
+            name="Staff of Expansion",
+            level=7,
+            material_type='Staff',
+            tags=[],
+            description="""
+                When you use a \\glossterm<magical> ability that creates a \\glossterm<zone> or \\glossterm<emanation>, you can increase the size of the area by one size category, up to a maximum of \\areahuge.
+                You can only increase the area of one ability at a time in this way.
+                If you increase the area of another ability or lose this staff, the area of the original ability returns to its normal size.
+            """,
+            short_description="Increases area of zones and emanations",
+        ),
+        MagicItem(
+            name="Staff of Expansion, Greater",
+            level=16,
+            material_type='Staff',
+            tags=[],
+            description="""
+                This item functions like a \\textit<staff of expansion>, except that it increases the area by two size categories.
+                In addition, the maximum area is a 200 foot radius, which is one size category larger than \\areahuge.
+            """,
+            short_description="Greatly increases area of zones and emanations",
+        ),
+    ]
 
     implements.append(MagicItem(
         name="Staff of Focus",
-        level=6,
+        level=5,
         material_type='Staff',
         description="""
             You reduce your \\glossterm<focus penalty> by 1.
@@ -151,49 +174,71 @@ def generate_implements():
         short_description="Reduces \\glossterm<focus penalty> by 1",
     ))
 
-    implements.append(MagicItem(
-        name="Staff of Power",
-        level=8,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +2 \\glossterm<magic bonus> to \\glossterm<power> with \\glossterm<magical> abilities.
-        """,
-        short_description="Grants +2 \\glossterm<magical> power",
-    ))
+    implements += [
+        MagicItem(
+            name="Staff of Power",
+            level=8,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +2 \\glossterm<magic bonus> to \\glossterm<power> with \\glossterm<magical> abilities.
+            """,
+            short_description="Grants +2 \\glossterm<magical> power",
+        ),
+        MagicItem(
+            name="Staff of Power, Greater",
+            level=14,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +4 \\glossterm<magic bonus> to \\glossterm<power> with \\glossterm<magical> abilities.
+            """,
+            short_description="Grants +4 \\glossterm<magical> power",
+        ),
+        MagicItem(
+            name="Staff of Power, Supreme",
+            level=20,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +6 \\glossterm<magic bonus> to \\glossterm<power> with \\glossterm<magical> abilities.
+            """,
+            short_description="Grants +6 \\glossterm<magical> power",
+        ),
+    ]
 
-    implements.append(MagicItem(
-        name="Staff of Power, Greater",
-        level=17,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +4 \\glossterm<magic bonus> to \\glossterm<power> with \\glossterm<magical> abilities.
-        """,
-        short_description="Grants +4 \\glossterm<magical> power",
-    ))
-
-    implements.append(MagicItem(
-        name="Staff of Precision",
-        level=8,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +1 \\glossterm<magic bonus> to \\glossterm<accuracy>.
-        """,
-        short_description="Grants +1 accuracy",
-    ))
-
-    implements.append(MagicItem(
-        name="Staff of Precision, Greater",
-        level=17,
-        material_type='Staff',
-        tags=[],
-        description="""
-            You gain a +2 \\glossterm<magic bonus> to \\glossterm<accuracy>.
-        """,
-        short_description="Grants +2 accuracy",
-    ))
+    implements += [
+        MagicItem(
+            name="Staff of Precision",
+            level=8,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +1 \\glossterm<magic bonus> to \\glossterm<accuracy>.
+            """,
+            short_description="Grants +1 accuracy",
+        ),
+        MagicItem(
+            name="Staff of Precision, Greater",
+            level=14,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +2 \\glossterm<magic bonus> to \\glossterm<accuracy>.
+            """,
+            short_description="Grants +2 accuracy",
+        ),
+        MagicItem(
+            name="Staff of Precision, Supreme",
+            level=20,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You gain a +3 \\glossterm<magic bonus> to \\glossterm<accuracy>.
+            """,
+            short_description="Grants +3 accuracy",
+        ),
+    ]
 
     return implements
 
