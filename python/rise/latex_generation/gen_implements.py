be733316b956eaae40ff9dc81f2f164465f5be49
Kevin Johnson <vadskye@gmail.com>
2020-09-14 17:18:25 -0700

    create implements to replace augments
    Former-commit-id: 658af898b1968a507ce40417e4edf1e848a99e6e

--- python/rise/latex_generation/gen_implements.py
+++ python/rise/latex_generation/gen_implements.py
@@ -62,21 +62,43 @@ def generate_implements():
         ),
     ]
 
-    implements.append(MagicItem(
-        name="Staff of Transit",
-        level=6,
-        material_type='Staff',
-        tags=[],
-        description="""
-            Your \\glossterm<magical> abilities have the maximum distance they can \\glossterm<teleport> targets doubled.
-        """,
-        short_description="Doubles your teleportation distance",
-    ))
+    implements += [
+        MagicItem(
+            name="Staff of Transit",
+            level=6,
+            material_type='Staff',
+            tags=[],
+            description="""
+                Your \\glossterm<magical> abilities have the maximum distance they can \\glossterm<teleport> targets doubled.
+            """,
+            short_description="Doubles your teleportation distance",
+        ),
+        MagicItem(
+            name="Staff of Transit, Greater",
+            level=12,
+            material_type='Staff',
+            tags=[],
+            description="""
+                Your \\glossterm<magical> abilities have the maximum distance they can \\glossterm<teleport> targets tripled.
+            """,
+            short_description="Triples your teleportation distance",
+        ),
+        MagicItem(
+            name="Staff of Transit, Supreme",
+            level=18,
+            material_type='Staff',
+            tags=[],
+            description="""
+                Your \\glossterm<magical> abilities have the maximum distance they can \\glossterm<teleport> targets quadrupled.
+            """,
+            short_description="Quadruples your teleportation distance",
+        )
+    ]
 
     implements += [
         MagicItem(
             name="Extending Staff",
-            level=10,
+            level=9,
             material_type='Staff',
             description="""
                 You double the range of your \\glossterm<magical> abilities.
@@ -85,7 +107,7 @@ def generate_implements():
         ),
         MagicItem(
             name="Extending Staff, Greater",
-            level=19,
+            level=15,
             material_type='Staff',
             description="""
                 You triple the range of your \\glossterm<magical> abilities.
@@ -94,14 +116,149 @@ def generate_implements():
         ),
     ]
 
+    implements += [
+        MagicItem(
+            name="Staff of Giants",
+            level=6,
+            material_type='Staff',
+            description="""
+                Whenever you use a \\glossterm<magical> ability that has a maximum size category for its targets, you increase that maximum by one size category, to a maximum of Colossal.
+            """,
+            short_description="Increases maximum size category of abilities",
+        ),
+        MagicItem(
+            name="Staff of Giants, Greater",
+            level=12,
+            material_type='Staff',
+            description="""
+                Whenever you use a \\glossterm<magical> ability that has a maximum size category for its targets, you increase that maximum by two size categories, to a maximum of Colossal.
+            """,
+            short_description="Significantly increaases maximum size category of abilities",
+        ),
+        MagicItem(
+            name="Staff of Giants, Supreme",
+            level=18,
+            material_type='Staff',
+            description="""
+                Whenever you use a \\glossterm<magical> ability that has a maximum size category for its targets, you increase that maximum by three size categories, to a maximum of Colossal.
+            """,
+            short_description="Drastically increaases maximum size category of abilities",
+        ),
+    ]
+
+    implements += [
+        MagicItem(
+            name="Selective Staff",
+            level=9,
+            material_type='Staff',
+            tags=[],
+            description="""
+                Whenever you use a \\glossterm<magical> ability that affects an area and does not have the \\glossterm<Sustain> or \\glossterm<Attune> tags, you can freely exclude any areas from the ability's effect.
+                All squares in the final area of the spell must be contiguous.
+                You cannot create split a spell's area into multiple completely separate areas.
+            """,
+            short_description="Allows excluding areas",
+        ),
+        MagicItem(
+            name="Selective Staff, Greater",
+            level=15,
+            material_type='Staff',
+            tags=[],
+            description="""
+                This weapon functions like a \\mitem<selective staff> implement, except that you can split the spell's area into two completely separate areas.
+                If you do, each of those two areas must be independently contiguous.
+            """,
+            short_description="Allows excluding and splitting areas",
+        )
+    ]
+
+    implements += [
+        MagicItem(
+            name="Staff of Silence",
+            level=9,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You can cast spells without using \\glossterm<verbal components>.
+            """,
+            short_description="Allows casting spells without verbal components",
+        ),
+        MagicItem(
+            name="Staff of Stillness",
+            level=9,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You can cast spells without using \\glossterm<somatic components>.
+            """,
+            short_description="Allows casting spells without somatic components",
+        ),
+        MagicItem(
+            name="Staff of Tranquility",
+            level=15,
+            material_type='Staff',
+            tags=[],
+            description="""
+                You can cast spells without using \\glossterm<verbal components> or \\glossterm<somatic components>.
+            """,
+            short_description="Allows casting spells without components",
+        ),
+    ]
+
+    implements += [
+        MagicItem(
+            name="Reaching Staff",
+            level=12,
+            material_type='Staff',
+            description="""
+                Whenever you use a \\glossterm<magical> ability that does not have the \\glossterm<Sustain> or \\glossterm<Attune> tags, you may choose a location within \\rngclose range.
+                The ability takes effect as if you were in the chosen location.
+                This affects your \\glossterm<line of effect> for the ability, but not your \\glossterm<line of sight> (since you still see from your normal location).
+                % Wording?
+                Since an ability's range is measured from your location, this augment can allow you to affect targets outside your normal range.
+                For example, a cone that normally bursts out from you would instead originate from your chosen location, potentially avoiding an obstacle between you and your target.
+            """,
+            short_description="Allows ability use from a short distance away",
+        ),
+        MagicItem(
+            name="Reaching Staff, Greater",
+            level=18,
+            material_type='Staff',
+            description="""
+                This implement functions like a \\textit<reaching staff> implement, except that the range increases to \\rngmed range.
+            """,
+            short_description="Allows ability use from a distance away",
+        ),
+    ]
+
+
     implements.append(MagicItem(
-        name="Reaching Staff",
+        name="Cryptic Staff",
+        level=8,
+        material_type='Staff',
+        tags=[],
+        description="""
+            Whenever you cast a spell, you may choose a different spell you know.
+            If you do, the visual effects and magical aura of the spell you are casting change to match your chosen spell.
+            This affects inspection of the spell itself by any means, such as with the Spellsense skill (see \\pcref<Spellsense>).
+            However, it does not alter the mechanical effects of the spell in any way.
+
+            An observer can make a Spellsense check with a \\glossterm<difficulty rating> of 15 \\add your magical \\glossterm<power> to identify the spell's true nature, with a minimum \\glossterm<difficulty rating> of 23.
+            If the spell's effects depend on visual components, the spell may fail to work if you alter the spell's visuals too much.
+        """,
+        short_description="Makes spells hard to identify",
+    ))
+
+    implements.append(MagicItem(
+        name="Quickening Staff",
         level=12,
         material_type='Staff',
+        tags=[],
         description="""
-            Spells you cast with this staff automatically have the benefits of the Reach augment, if applicable (see \\pcref<Augment Descriptions>).
+            Once per round, you can cast a spell as a \\glossterm<minor action>.
+            If you do, you cannot take any actions during the \\glossterm<action phase> or \\glossterm<delayed action phase> of the next round.
         """,
-        short_description="Allows casting from a short distance away",
+        short_description="Allows casting spells very quickly",
     ))
 
     implements += [
@@ -140,27 +297,24 @@ def generate_implements():
 
     implements += [
         MagicItem(
-            name="Staff of Expansion",
-            level=7,
+            name="Widening Staff",
+            level=12,
             material_type='Staff',
             tags=[],
             description="""
-                When you use a \\glossterm<magical> ability that creates a \\glossterm<zone> or \\glossterm<emanation>, you can increase the size of the area by one size category, up to a maximum of \\areahuge.
-                You can only increase the area of one ability at a time in this way.
-                If you increase the area of another ability or lose this staff, the area of the original ability returns to its normal size.
+                Whenever you use a \\glossterm<magical> ability that affects an area and does not have the \\glossterm<Attune> or \\glossterm<Sustain> tags, you may double its area.
             """,
-            short_description="Increases area of zones and emanations",
+            short_description="Doubles area size",
         ),
         MagicItem(
-            name="Staff of Expansion, Greater",
-            level=16,
+            name="Widening Staff, Greater",
+            level=18,
             material_type='Staff',
             tags=[],
             description="""
-                This item functions like a \\textit<staff of expansion>, except that it increases the area by two size categories.
-                In addition, the maximum area is a 200 foot radius, which is one size category larger than \\areahuge.
+                This implement functions like a \\textit<widening staff> implement, except that it triples the area instead of doubling it.
             """,
-            short_description="Greatly increases area of zones and emanations",
+            short_description="Triples area size",
         ),
     ]
 
