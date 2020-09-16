d6775b65357c542ef9c5657e5fa5b7b27e8963e6
Kevin Johnson <vadskye@gmail.com>
2020-09-16 12:35:19 -0700

    restructure giants in a group
    Former-commit-id: 56c460ff9e9a8943c1f5e1512abce91fd57c0b37

--- typescript/src/monsters/types/monstrous_humanoids.ts
+++ typescript/src/monsters/types/monstrous_humanoids.ts
@@ -11,53 +11,6 @@ const boulder: WeaponInput = {
 export const monstrousHumanoidInput: TypelessMonsterInput[] = [
   {
     alignment: "Usually chaotic evil",
-    armorInputs: [{ name: "breastplate" }, { name: "thick skin" }],
-    challengeRating: 3,
-    description: `
-      Skin color among hill giants ranges from light tan to deep ruddy brown. Their hair is brown or black, with eyes the same color. Hill giants wear layers of crudely prepared hides with the fur left on. They seldom wash or repair their garments, preferring to simply add more hides as their old ones wear out.
-
-      Adults are about 15 feet tall. Hill giants can live to be 70 years old.
-    `,
-    languages: ["Giant"],
-    level: 7,
-    name: "Giant, Hill",
-    passiveAbilities: [
-      { name: "Massive Sweep", description: "A hill giant's greatclub has the Sweeping (2) tag." },
-    ],
-    size: "huge",
-    startingAttributes: { str: 5, dex: -2, con: 3, int: -2, per: -2, wil: -2 },
-    tactics: `
-      Hill giants prefer to fight from high, rocky outcroppings, where they can pelt opponents with rocks and boulders while limiting the risk to themselves.
-      They lack the intelligence or desire to retreat if their enemies survive to approach them, and prefer to draw their massive clubs and enter melee.
-      If possible, they smash their foes off of cliffs.
-    `,
-    weaponInput: [{ name: "greatclub" }, boulder],
-  },
-  {
-    alignment: "Usually true neutral",
-    armorInputs: [{ name: "hide" }, { name: "thick skin" }],
-    challengeRating: 3,
-    description: `
-      Stone giants prefer thick leather garments, dyed in shades of brown and gray to match the stone around them. Adults stand about 20 feet tall. Stone giants can live to be 300 years old.
-
-      Young stone giants can be capricious, hunting tiny creatures like goats and humanoids on a whim.
-      Elder stone giants tend to be wiser and more cautious, and avoid unnecessary conflict.
-    `,
-    languages: ["Common", "Giant"],
-    level: 10,
-    name: "Giant, Stone",
-    passiveAbilities: [
-      { name: "Massive Sweep", description: "A stone giant's greatclub has the Sweeping (2) tag." },
-    ],
-    size: "gargantuan",
-    startingAttributes: { str: 7, dex: -1, con: 3, int: 0, per: 0, wil: -2 },
-    tactics: `
-      Stone giants fight from a great distance whenever possible, using their ability to hurl stones up to 1,000 feet.
-    `,
-    weaponInput: [{ name: "greatclub" }, { ...boulder, rangeIncrement: 200 }],
-  },
-  {
-    alignment: "Usually chaotic evil",
     attackInputs: [
       {
         // Uses str for accuracy instead of per
@@ -123,5 +76,80 @@ export const monstrousHumanoidInput: TypelessMonsterInput[] = [
     weaponInput: [{ name: "tentacle" }],
   },
 ];
+
+const baseGiant = {
+  languages: ["Giant"],
+  passiveAbilities: [
+    {
+      description: `
+        A giant can throw objects up to two size categories smaller than itself with ease.
+        Giants prefer to throw boulders, but in a pinch they can throw almost anything.
+        Their \\glossterm{range increment} with objects other than boulders is generally half their range increment with boulders, and depending on the construction of the object it may also deal less damage than a boulder.
+      `,
+      name: "Rock Throwing",
+    },
+    {
+      description: `
+        All melee weapons wielded by a giant gain the \\glossterm{Sweeping} (2) tag (see \\pcref{Weapon Tags)).
+      `,
+      name: "Massive Sweep",
+    },
+  ],
+};
+
+monstrousHumanoidInput.push({
+  description: `
+    Giants are massive humanoid creatures that tower over lesser creatures.
+    All giants have immense strength and unimpressive agility - except when it comes to throwing and catching rocks, which they tend to excel at.
+  `,
+  name: "Giants",
+  monsters: [
+    {
+      ...baseGiant,
+      alignment: "Usually chaotic evil",
+      armorInputs: [{ name: "breastplate" }, { name: "thick skin" }],
+      challengeRating: 3,
+      description: `
+        Skin color among hill giants ranges from light tan to deep ruddy brown. Their hair is brown or black, with eyes the same color. Hill giants wear layers of crudely prepared hides with the fur left on. They seldom wash or repair their garments, preferring to simply add more hides as their old ones wear out.
+
+        Adults are about 15 feet tall. Hill giants can live to be 70 years old.
+      `,
+      level: 7,
+      name: "Hill Giant",
+      size: "huge",
+      startingAttributes: { str: 5, dex: -2, con: 3, int: -2, per: -2, wil: -2 },
+      tactics: `
+        Hill giants prefer to fight from high, rocky outcroppings, where they can pelt opponents with rocks and boulders while limiting the risk to themselves.
+        They lack the intelligence or desire to retreat if their enemies survive to approach them, and prefer to draw their massive clubs and enter melee.
+        If possible, they smash their foes off of cliffs.
+      `,
+      weaponInput: [{ name: "greatclub", tags: ["sweeping 2"] }, boulder],
+    },
+    {
+      ...baseGiant,
+      alignment: "Usually true neutral",
+      armorInputs: [{ name: "hide" }, { name: "thick skin" }],
+      challengeRating: 3,
+      description: `
+        Stone giants prefer thick leather garments, dyed in shades of brown and gray to match the stone around them. Adults stand about 20 feet tall. Stone giants can live to be 300 years old.
+
+        Young stone giants can be capricious, hunting tiny creatures like goats and humanoids on a whim.
+        Elder stone giants tend to be wiser and more cautious, and avoid unnecessary conflict.
+      `,
+      languages: ["Common", "Giant"],
+      level: 10,
+      name: "Stone Giant",
+      size: "gargantuan",
+      startingAttributes: { str: 7, dex: -1, con: 3, int: 0, per: 0, wil: -2 },
+      tactics: `
+        Stone giants fight from a great distance whenever possible, using their ability to hurl stones up to 1,000 feet.
+      `,
+      weaponInput: [
+        { name: "greatclub", tags: ["sweeping 2"] },
+        { ...boulder, rangeIncrement: 200 },
+      ],
+    },
+  ],
+});
 
 export const monstrousHumanoids = addType("monstrous humanoid", monstrousHumanoidInput);
