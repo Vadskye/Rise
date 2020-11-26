9fb42124c44ae37060c066be330fd7fe21844f67
Kevin Johnson <vadskye@gmail.com>
2020-11-26 14:03:32 -0800

    update character sheet
    Former-commit-id: 50c9688781f76b24de8238d6f651f7e51c25d8c5

--- character_sheet/api_scripts/calculate_attack_damage.js
+++ character_sheet/api_scripts/calculate_attack_damage.js
@@ -19,32 +19,6 @@ function getAttribute(characterId, attributeName, createIfNotExist=false) {
     }
 }
 
-const diceByPower = {
-    '-4': '1d3',
-    '-2': '1d4',
-    0: '1d6',
-    2: '1d8',
-    4: '1d10',
-    6: '2d6',
-    8: '2d8',
-    10: '2d10',
-    12: '4d6',
-    14: '4d8',
-    16: '4d10',
-    18: '5d10',
-    20: '6d10',
-    22: '7d10',
-    24: '8d10',
-    26: '8d10+10',
-    28: '8d10+20',
-    30: '8d10+30',
-    32: '8d10+40',
-    34: '8d10+50',
-    36: '8d10+60',
-    38: '8d10+70',
-    40: '8d10+80',
-};
-
 function assert(test, message) {
     if (!test) {
         log(message);
@@ -53,32 +27,34 @@ function assert(test, message) {
     }
 }
 
-function setDamageDiceForAttack({attackName, attackDieBonus, characterId, source}) {
-    assert(attackDieBonus !== undefined, 'attackDieBonus should exist');
+function setDamageForAttack({attackDice, attackName, attackPower, characterId, source}) {
+    assert(attackDice !== undefined, 'attackDice should exist');
     assert(attackName, 'attackName should exist');
     assert(source, 'source should exist');
-    
-    const basePower = getAttrByName(characterId, `${source}_power`);
-    const totalPower = Math.floor((Number(basePower) + Number(attackDieBonus) * 2) / 2) * 2;
-    
-    const diceName = attackName.replace('_attack0_damage', '_attack0_dice');
-    const diceAttribute = getAttribute(characterId, diceName, true);
-    diceAttribute.set('current', diceByPower[totalPower]);
 
-    const effectName = attackName.replace('_attack0_damage', '_attack0_effect');
+    const basePower = Number(getAttrByName(characterId, `${source}_power`));
+    const powerMultiplier = {
+      'full': 1,
+      'half': 0.5,
+      'none': 0,
+    }[attackPower || 'full'];
+    assert(powerMultiplier !== undefined, `Unable to parse attackPower: ${attackPower}`);
+    const totalPower = Math.floor(basePower * powerMultiplier);
+
+    const effectName = attackName.replace('_attack0_name', '_attack0_effect');
     const effectAttribute = getAttribute(characterId, effectName, true);
     assert(effectAttribute, 'effectAttribute should exist')
     const trimmed = (effectAttribute.get('current') || '').trim();
     effectAttribute.set('current', trimmed === '' ? ' ' : trimmed);
-    
-    const buttonName = attackName.replace('_attack0_damage', '_roll_attack');
+
+    const buttonName = attackName.replace('_attack0_name', '_roll_attack');
     const buttonAttribute = getAttribute(characterId, buttonName, true);
     assert(buttonAttribute, 'buttonAttribute should exist')
     buttonAttribute.set(
       'current', 
       "@{character_name} uses @{attack0_name}:"
       + " [[d10! + @{accuracy} + @{attack0_accuracy}]] vs @{attack0_defense}!"
-      + " ([[@{attack0_dice}]]; @{attack0_effect})"
+      + ` ([[@{attack0_dice}+${totalPower}]]; @{attack0_effect})`
     );
     // sendChat('debug', `set ${diceName} based on base ${basePower} + ${attackDieBonus} (${getAttrByName(characterId, attackName)}): ${diceAttribute.get('current')}`);
 }
@@ -92,10 +68,13 @@ function recalculateRepeatingDamageAttributes(characterId, source) {
         return name.startsWith(`repeating_${source}attacks`) && name.endsWith('_attack0_name');
     });
     for (const attr of repeatingDamageAttributes) {
-        const damageAttr = getAttribute(characterId, attr.get('name').replace('_attack0_name', '_attack0_damage'), true);
-        setDamageDiceForAttack({
-            attackDieBonus: damageAttr.get('current'),
-            attackName: damageAttr.get('name'),
+        const attackName = attr.get('name');
+        const diceAttr = getAttribute(characterId, attackName.replace('_attack0_name', '_attack0_dice'), true);
+        const powerAttr = getAttribute(characterId, attackName.replace('_attack0_name', '_attack0_power'), true);
+        setDamageForAttack({
+            attackDice: diceAttr.get('current'),
+            attackName,
+            attackPower: powerAttr.get('current'),
             characterId,
             source,
         });
@@ -110,11 +89,13 @@ on('change:attribute', (obj, oldObj) => {
          recalculateRepeatingDamageAttributes(characterId, 'mundane');
       } else if (name === 'magical_power') {
          recalculateRepeatingDamageAttributes(characterId, 'magical');
-      } else if (name.startsWith('repeating_') && name.endsWith('_attack0_damage')) {
+      } else if (name.startsWith('repeating_') && name.endsWith('_attack0_dice')) {
           const source = name.includes('mundane') ? 'mundane' : 'magical';
-          setDamageDiceForAttack({
-              attackDieBonus: obj.get('current') || '0',
-              attackName: oldObj.name,
+          const powerAttr = getAttribute(characterId, oldObj.name.replace('_attack0_dice', '_attack0_power'), true);
+          setDamageForAttack({
+              attackDice: obj.get('current') || '1d2',
+              attackName: oldObj.name.replace('_attack0_dice', '_attack0_name'),
+              attackPower: powerAttr.get('current'),
               characterId,
               source,
           });
