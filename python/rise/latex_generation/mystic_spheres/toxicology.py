578ecd7bde81ef58e800c910b162886fb8a20613
Kevin Johnson <vadskye@gmail.com>
2021-01-01 12:48:18 -0800

    add more toxicology spells

--- python/rise/latex_generation/mystic_spheres/toxicology.py
+++ python/rise/latex_generation/mystic_spheres/toxicology.py
@@ -23,6 +23,13 @@ toxicology=MysticSphere(
     ],
     lists=['Arcane', 'Nature', 'Pact'],
     spells=[
+        Spell('Corrosive Grasp', 1, 'One creature or object within your \\glossterm<reach>', """
+            This spell does not have the \\glossterm<Focus> tag.
+            You must have a \\glossterm<free hand> to cast this spell.
+
+            Make a melee attack vs. Reflex against the target.
+            \\hit The target takes acid damage equal to 1d10 plus your \\glossterm<power>.
+        """, scaling="damage", tags=[], focus=False),
         Spell('Poison -- Asp Venom', 2, 'One living creature within \\rngmed range', """
             Make an attack vs. Fortitude against the target.
             \\hit The target becomes \\glossterm<poisoned> by the first \\glossterm<poison stage> of asp venom.
@@ -43,12 +50,12 @@ toxicology=MysticSphere(
         """, scaling="""
             The hit point loss from the poison increases by +1d for each rank beyond 3.
         """, tags=['Manifestation']),
-        Spell('Poison Transferance', 3, ['Yourself or an \\glossterm<ally> within \\rngmed range', 'One other living creature within that range'], """
+        Spell('Poison Transferance', 2, ['Yourself or an \\glossterm<ally> within \\rngmed range', 'One other living creature within that range'], """
             The primary target must be currently affected by a poison.
             Make an attack vs. Fortitude against the secondary target.
             \\hit The primary target gains an additional success to resist a poison currently affecting it.
             In addition, the secondary target becomes \\glossterm<poisoned> by that same poison, and immediately suffers the effect of the poison's first \\glossterm<poison stage>.
-            % No \\glance effect; weird shenanigans if you autoremove the poison
+            % No glance effect; weird shenanigans if you autoremove the poison
             \\crit As above, except that the primary target gains two successes to resist its poison.
             In addition, the secondary target immediately reaches the poison's second poison stage.
         """, scaling="accuracy", tags=[]),
@@ -76,9 +83,13 @@ toxicology=MysticSphere(
             \\crit As above, except that the effect lasts until this curse is removed.
             \\glance As above, except that the condition is removed at the end of the next round.
         """, scaling="accuracy", tags=['Curse']),
-        Spell('Acid Orb', 1, 'One creature or object within \\rngmed range', """
+        Spell('Acid Splash', 1, 'One creature or object within \\rngmed range', """
             Make an attack vs. Fortitude against the target.
             \\hit The target takes acid damage equal to 1d10 plus your \\glossterm<power>.
+        """, scaling="damage", tags=['Manifestation']),
+        Spell('Acid Arrow', 2, 'One creature or object within \\rnglong range', """
+            Make an attack vs. Fortitude against the target.
+            \\hit The target takes acid damage equal to 2d6 plus your \\glossterm<power>.
         """, scaling="damage", tags=['Manifestation']),
         Spell('Acid Spray', 1, 'Everything in a \\areasmall cone from you', """
             Make an attack vs. Fortitude against each target.
@@ -92,17 +103,22 @@ toxicology=MysticSphere(
             \\hit Each target takes acid damage equal to 2d6 plus half your \\glossterm<power>.
             \\glance As above, except that that each target takes half damage.
         """, scaling="damage", tags=['Attune (self)']),
-        Spell('Corrosive Orb', 5, 'One creature or object within \\rngmed range', """
+        Spell('Corrosive Splash', 5, 'One creature or object within \\rngmed range', """
             Make an attack vs. Fortitude against the target.
             \\hit The target takes acid damage equal to 4d6 plus your \\glossterm<power>.
             This attack deals double damage to objects.
             \\glance As above, except that that the target takes half damage.
         """, scaling="damage", tags=['Manifestation']),
-        Spell('Acid Rain', 3, 'Everything in a \\areasmall radius within \\rngmed range', """
+        Spell('Acid Rain', 4, 'Everything in a \\areasmall radius, 30 ft.\\ high cylinder within \\rngmed range', """
             Make an attack vs. Fortitude against each target.
-            \\hit Each target takes acid damage equal to 2d6 plus half your \\glossterm<power>.
+            \\hit Each target takes acid damage equal to 2d8 plus half your \\glossterm<power>.
             \\glance As above, except that that each target takes half damage.
         """, scaling="damage", tags=['Manifestation']),
+        Spell('Acid Orb', 3, 'Everything in a \\areasmall radius within \\rngmed range', """
+            Make an attack vs. Reflex against each target.
+            \\hit Each target takes acid damage equal to 2d6 plus half your \\glossterm<power>.
+            \\glance As above, except that that each target takes half damage.
+        """, scaling="damage", tags=[]),
     ],
     rituals=[
     ],
