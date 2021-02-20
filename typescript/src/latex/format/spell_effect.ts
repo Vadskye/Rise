10f03be71f50e50452c58725712b87c696570e6a
Kevin Johnson <vadskye@gmail.com>
2021-02-20 14:58:18 -0800

    improve spell spacing and formatting
    Former-commit-id: 9f0981f9f9bdf72c2ebb8071cf4b5e8125c2f974

--- typescript/src/latex/format/spell_effect.ts
+++ typescript/src/latex/format/spell_effect.ts
@@ -7,29 +7,35 @@ function assertEndsWithPeriod(text: string | null | undefined) {
 }
 
 export function spellEffect(
-  spell: Pick<Spell, "attack" | "effect" | "functionsLike">,
+  spell: Pick<Spell, "attack" | "effect" | "functionsLike" | "name">,
 ): string | null {
-  if (spell.attack) {
-    assertEndsWithPeriod(spell.attack.target);
-    assertEndsWithPeriod(spell.attack.hit);
-    assertEndsWithPeriod(spell.attack.glance);
-    assertEndsWithPeriod(spell.attack.crit);
-    return `
-      ${spell.attack.target.trim()}
-      \\hit ${spell.attack.hit.trim()}
-      ${spell.attack.glance ? `\\glance ${spell.attack.glance.trim()}` : ""}
-      ${spell.attack.crit ? `\\crit ${spell.attack.crit.trim()}` : ""}
-    `;
-  } else if (spell.effect) {
-    return spell.effect;
-  } else if (spell.functionsLike) {
-    assertEndsWithPeriod(spell.functionsLike.exceptThat);
-    return `
-      This spell functions like the \\spell{${
-        spell.functionsLike.spell
-      }} spell, except that ${spell.functionsLike.exceptThat.trim()}
-    `;
-  } else {
-    return null;
+  try {
+    if (spell.attack) {
+      assertEndsWithPeriod(spell.attack.target);
+      assertEndsWithPeriod(spell.attack.hit);
+      assertEndsWithPeriod(spell.attack.glance);
+      assertEndsWithPeriod(spell.attack.crit);
+      return `
+        ${spell.attack.target.trim()}
+        \\vspace{0.25em}
+        \\hit ${spell.attack.hit.trim()}
+        ${spell.attack.glance ? `\\glance ${spell.attack.glance.trim()}` : ""}
+        ${spell.attack.crit ? `\\crit ${spell.attack.crit.trim()}` : ""}
+      `;
+    } else if (spell.effect) {
+      return spell.effect;
+    } else if (spell.functionsLike) {
+      assertEndsWithPeriod(spell.functionsLike.exceptThat);
+      return `
+        This spell functions like the \\spell{${
+          spell.functionsLike.spell
+        }} spell, except that ${spell.functionsLike.exceptThat.trim()}
+      `;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    err.message += `Error converting spell ${spell.name} to LaTeX: ${err.message}`;
+    throw err;
   }
 }
