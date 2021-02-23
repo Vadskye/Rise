8025d17d565454b89dd325d9d048d7309afdec4e
Kevin Johnson <vadskye@gmail.com>
2021-02-22 17:27:16 -0800

    add ability to generate attack typescript blocks
    Former-commit-id: 0aa8f70b7ee64ab2eb18cf41daa904a0ffac2844

--- python/rise/latex/spell.py
+++ python/rise/latex/spell.py
@@ -1,4 +1,5 @@
 from logging import getLogger, WARNING
+import sys
 import re
 from rise.latex.effects import targets_are_plural
 from rise.latex.tags import is_valid_tag, to_latex_tags
@@ -170,43 +171,19 @@ class Spell(object):
         ]
         tags_text = f"tags: {nontype_tags}," if len(nontype_tags) > 0 else ""
 
-        functions_like_matches = re.search(
-            "functions like the \\\\(spell|textit|ritual)<([^>]+)>", self.effect_text
-        )
-        if functions_like_matches:
-            referenced_spell = functions_like_matches[2]
-            if not referenced_spell:
-                raise Exception(
-                    f"Spell ${self.name} has confusing 'functions like' text: '{self.effect_text}'"
-                )
-            except_that_text = re.sub(
-                "This (spell|ritual) functions like the \\\\spell<[^>]+> spell, except that",
-                "",
-                self.effect_text.strip(),
+        try:
+            effect_or_functions_like_text = generate_effect_text(
+                self.effect_text, self.targets
             )
-            functions_like_text = f"""
-      functionsLike: {{
-        exceptThat: `
-          {except_that_text.strip()}
-        `,
-        spell: "{referenced_spell}",
-      }},
-            """.replace(
-                "\\", "\\\\"
-            )
-            effect_text = ""
-        else:
-            functions_like_text = ""
-            effect_text = (
-                "effect: `\n" + self.effect_text.strip().replace("\\", "\\\\") + "\n`,"
-            )
-        # Standardize indentation
-        effect_text = re.sub('^ *', '        ', effect_text, flags=re.MULTILINE)
+            functions_like_text = effect_or_functions_like_text[0]
+            effect_text = effect_or_functions_like_text[1]
+        except:
+            err = sys.exc_info()[0]
+            raise Exception(f"Unable to parse spell {self.name}: {err}")
 
         casting_time_text = (
             f'castingTime: "minor action",'
-            if "You can cast this spell as a \\\\glossterm<minor action"
-            in effect_text
+            if "You can cast this spell as a \\\\glossterm<minor action" in effect_text
             else ""
         )
         casting_time_text = (
@@ -240,10 +217,21 @@ class Spell(object):
         ranges = ["short", "med", "long", "dist", "ext"]
         for area in areas:
             elements_text = elements_text.replace(f"\\\\area{area}", f"\\\\{area}area")
-            elements_text = elements_text.replace(f"\\\\{area}area line", f"\\\\{area}area long line")
+            elements_text = elements_text.replace(
+                f"\\\\{area}area line", f"\\\\{area}area long line"
+            )
         for r in ranges:
-            elements_text = re.sub(f'\\\\rng{r}( range)?', f"\\\\{r}range", elements_text)
-        elements_text = elements_text.replace('<', '{').replace('>', '}')
+            elements_text = re.sub(
+                f"\\\\rng{r}( range)?", f"\\\\{r}range", elements_text
+            )
+        elements_text = (
+            elements_text.replace("<", "{")
+            .replace(">", "}")
+            .replace("Each target", "Each subject")
+            .replace("each target", "each subject")
+            .replace("The target", "The subject")
+            .replace("the target", "the subject")
+        )
 
         return f"""
             {{
@@ -289,3 +277,73 @@ class Spell(object):
             \\vspace<0.25em>
             {self.extra_text.strip() if self.extra_text else ""}
         """
+
+
+def generate_effect_text(original_effect_text, targets):
+    functions_like_matches = re.search(
+        "functions like the \\\\(spell|textit|ritual)<([^>]+)>", original_effect_text
+    )
+    if functions_like_matches:
+        referenced_spell = functions_like_matches[2]
+        if not referenced_spell:
+            raise Exception(
+                f"Spell has confusing 'functions like' text: '{original_effect_text}'"
+            )
+        except_that_text = re.sub(
+            "This (spell|ritual) functions like the \\\\spell<[^>]+> spell, except that",
+            "",
+            original_effect_text.strip(),
+        )
+        functions_like_text = f"""
+  functionsLike: {{
+    exceptThat: `
+      {except_that_text.strip()}
+    `,
+    spell: "{referenced_spell}",
+  }},
+        """
+        effect_text = ""
+    elif "\\hit" in original_effect_text:
+        prefix_match = re.search(
+            "(.*)\\\\hit",
+            original_effect_text,
+            re.DOTALL,
+        )
+        hit_match = re.search(
+            "\\\\hit (.*?)(\\\\glance|\\\\crit|$)",
+            original_effect_text.strip(),
+            re.DOTALL,
+        )
+        glance_match = re.search(
+            "\\\\glance (.*?)(\\\\crit|$)", original_effect_text.strip(), re.DOTALL
+        )
+        crit_match = re.search(
+            "\\\\crit (.*)$", original_effect_text.strip(), re.DOTALL
+        )
+        hit_text = f"hit: `{hit_match[1].strip()}`,"
+        glance_text = f"glance: `{glance_match[1].strip()}`," if glance_match else ""
+        crit_text = f"crit: `{crit_match[1].strip()}`," if crit_match else ""
+
+        effect_text = f"""
+            attack: {{
+                {crit_text}
+                {glance_text}
+                {hit_text}
+                targeting: `
+                    {prefix_match[1].strip()}
+                `,
+            }},
+        """
+        functions_like_text = ""
+    else:
+        functions_like_text = ""
+        effect_text = "effect: `\n" + original_effect_text.strip() + "\n`,"
+    # Standardize indentation
+    functions_like_text = re.sub(
+        "^ *", "        ", functions_like_text, flags=re.MULTILINE
+    ).replace("\\", "\\\\")
+    effect_text = re.sub("^ *", "        ", effect_text, flags=re.MULTILINE).replace(
+        "\\", "\\\\"
+    )
+
+    return [functions_like_text, effect_text]
