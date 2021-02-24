2e5c7986be726eacf9df0cfb59669244441821f9
Kevin Johnson <vadskye@gmail.com>
2021-02-24 10:39:31 -0800

    rituals need listed fatigue points
    Former-commit-id: 066ded02c77b09f9869ad52c174d6b89bb8b4070

--- typescript/src/latex/format/spell_effect.ts
+++ typescript/src/latex/format/spell_effect.ts
@@ -1,4 +1,4 @@
-import { Spell } from "@src/mystic_spheres";
+import { SpellLike } from "@src/mystic_spheres";
 
 function assertEndsWithPeriod(text: string | null | undefined) {
   if (text && !text.trim().endsWith(".")) {
@@ -7,9 +7,21 @@ function assertEndsWithPeriod(text: string | null | undefined) {
 }
 
 export function spellEffect(
-  spell: Pick<Spell, "attack" | "castingTime" | "effect" | "functionsLike" | "name">,
+  spell: Pick<SpellLike, "attack" | "castingTime" | "effect" | "functionsLike" | "rank" | "name">,
 ): string | null {
   try {
+    const spellCategory =
+      spell.castingTime && spell.castingTime !== "minor action" ? "ritual" : "spell";
+
+    let fatiguePointsText = "";
+    if (spellCategory === "ritual") {
+      const fatiguePoints =
+        spell.castingTime === "24 hours" || spell.castingTime === "one week"
+          ? `${Math.pow(spell.rank || 0, 2) * 2} \\glossterm{fatigue points}`
+          : "one \\glossterm{fatigue point}";
+      fatiguePointsText = `\n\nThis ritual requires ${fatiguePoints} from its participants.`;
+    }
+
     if (spell.attack) {
       assertEndsWithPeriod(spell.attack.targeting);
       assertEndsWithPeriod(spell.attack.hit);
@@ -17,14 +29,19 @@ export function spellEffect(
       assertEndsWithPeriod(spell.attack.crit);
       // The terminal % prevents a double-space in weird edge cases
       return `
-        ${spell.attack.targeting.trim()}%
+        ${spell.attack.targeting.trim() + fatiguePointsText}%
         \\vspace{0.25em}
         \\hit ${spell.attack.hit.trim()}
         ${spell.attack.glance ? `\\glance ${spell.attack.glance.trim()}` : ""}
         ${spell.attack.crit ? `\\crit ${spell.attack.crit.trim()}` : ""}
       `;
     } else if (spell.effect) {
-      return spell.effect;
+      return fatiguePointsText
+        ? `
+          ${spell.effect}
+          ${fatiguePointsText}
+        `.trim()
+        : spell.effect.trim();
     } else if (spell.functionsLike) {
       const exceptThat = spell.functionsLike.mass
         ? "it affects up to five creatures of your choice from among yourself and your \\glossterm{allies} within \\medrange."
@@ -34,14 +51,12 @@ export function spellEffect(
         throw new Error(`Must have a defined 'exceptThat' in a 'functionsLike'`);
       }
 
-      const spellCategory =
-        spell.castingTime && spell.castingTime !== "minor action" ? "ritual" : "spell";
-
       return `
         This ${spellCategory} functions like the \\${spellCategory}{${
         spell.functionsLike.spell
       }} ${spellCategory}, except that ${exceptThat.trim()}
-      `;
+        ${fatiguePointsText}
+      `.trim();
     } else {
       return null;
     }
