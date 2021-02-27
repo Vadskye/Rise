18f09c98df01e3e3b710d789e8443742c2f1f091
Kevin Johnson <vadskye@gmail.com>
2021-02-27 09:19:51 -0800

    add gloves of +accuracy
    Former-commit-id: 091d0878dda5404d956cd73a9b45064f7c4a032e

--- python/rise/latex_generation/gen_apparel.py
+++ python/rise/latex_generation/gen_apparel.py
@@ -232,6 +232,40 @@ def generate_armor():
         ),
     ]
 
+    weapons += [
+        MagicItem(
+            name="Gloves of Precision",
+            # +2 levels since gloves are secondary for accuracy
+            level=6,
+            material_type="Gloves",
+            tags=[],
+            description="""
+                You gain a +1 \\glossterm<magic bonus> to accuracy.
+            """,
+            short_description="Grants +1 accuracy bonus",
+        ),
+        MagicItem(
+            name="Gloves of Precision, Greater",
+            level=12,
+            material_type="Gloves",
+            tags=[],
+            description="""
+                You gain a +2 \\glossterm<magic bonus> to accuracy.
+            """,
+            short_description="Grants +2 accuracy bonus",
+        ),
+        MagicItem(
+            name="Gloves of Precision, Supreme",
+            level=18,
+            material_type="Gloves",
+            tags=[],
+            description="""
+                You gain a +3 \\glossterm<magic bonus> to accuracy.
+            """,
+            short_description="Grants +3 accuracy bonus",
+        ),
+    ]
+
     apparel += [
         MagicItem(
             name="Shield of Bashing",
