12cb63fc757e1c2af8f175719352a36ed9fdbb11
Kevin Johnson <vadskye@gmail.com>
2021-03-06 12:17:32 -0800

    fully convert remaining combat styles to typescript
    Former-commit-id: 37117bd20d9e07b9587e109db483f71a20fc5e6b

--- typescript/src/combat_styles/herald_of_war.ts
+++ typescript/src/combat_styles/herald_of_war.ts
@@ -10,11 +10,10 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Boastful Battlecry",
 
-      // original targets: \glossterm{Enemies} within a \arealarge range from you
       effect: `
+        This ability targets all \\glossterm{enemies} within a \\largearea radius from you.
         During the next round, you gain a +4 bonus to \\glossterm{accuracy} with \\glossterm{strikes} against each subject.
-        `,
-
+      `,
       rank: 1,
       scaling: {
         3: "The bonus increases to +5.",
@@ -27,19 +26,14 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Demoralizing Battlecry",
 
-      // original targets: \glossterm{Enemies} in a \areasmall radius from you.
       attack: {
-        crit: `Each subject takes a -2 penalty to defenses as a \\glossterm{condition}.
-
-        \\rankline
-        You gain a +1 bonus to \\glossterm{accuracy} with the attack for each rank beyond 3.`,
-
+        crit: `Each subject takes a -2 penalty to defenses as a \\glossterm{condition}.`,
         hit: `Each subject takes a -2 penalty to defenses until the end of the next round..`,
         targeting: `
-        Make an attack vs. Mental against each subject.
+          Make an attack vs. Mental against all \\glossterm{enemies} in a \\smallarea radius from you.
         `,
       },
-
+      scaling: "accuracy",
       rank: 3,
       type: "Duration",
     },
@@ -47,19 +41,14 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Fearsome Battlecry",
 
-      // original targets: \glossterm{Enemies} in a \areasmall radius from you.
       attack: {
-        crit: `Each subject that has no remaining \\glossterm{resistance} to sonic damage is \\glossterm{frightened} by you as a \\glossterm{condition}.
-
-        \\rankline
-        You gain a +1 bonus to \\glossterm{accuracy} with the attack for each rank beyond 1.`,
-
-        hit: `Each subject that has no remaining \\glossterm{resistance} to sonic damage is \\glossterm{shaken} by you as a \\glossterm{condition}..`,
+        crit: `Each subject that is below its maximum \\glossterm{hit points} is \\glossterm{frightened} by you as a \\glossterm{condition}.`,
+        hit: `Each subject that is below its maximum \\glossterm{hit points} is \\glossterm{shaken} by you as a \\glossterm{condition}.`,
         targeting: `
-        Make an attack vs. Mental against each subject.
+          Make an attack vs. Mental against all \\glossterm{enemies} in a \\smallarea radius from you.
         `,
       },
-
+      scaling: "accuracy",
       rank: 1,
       tags: ["Emotion"],
       type: "Duration",
@@ -68,19 +57,15 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Frightening Battlecry",
 
-      // original targets: \glossterm{Enemies} in a \areasmall radius from you.
       attack: {
-        crit: `Each subject that has no remaining \\glossterm{resistance} to sonic damage is \\glossterm{panicked} by you as a \\glossterm{condition}.
-
-        \\rankline
-        You gain a +1 bonus to \\glossterm{accuracy} with the attack for each rank beyond 4.`,
-        glance: `As above, except that the condition is removed at the end of the next round.`,
-        hit: `Each subject that has no remaining \\glossterm{resistance} to sonic damage is \\glossterm{frightened} by you as a \\glossterm{condition}..`,
+        crit: `Each subject that is below its maximum \\glossterm{hit points} is \\glossterm{panicked} by you as a \\glossterm{condition}.`,
+        glance: "The effect lasts until the end of the next round.",
+        hit: `Each subject that is below its maximum \\glossterm{hit points} is \\glossterm{frightened} by you as a \\glossterm{condition}.`,
         targeting: `
-        Make an attack vs. Mental against each subject.
+          Make an attack vs. Mental against all \\glossterm{enemies} in a \\smallarea radius from you.
         `,
       },
-
+      scaling: "accuracy",
       rank: 4,
       tags: ["Emotion"],
       type: "Duration",
@@ -89,32 +74,26 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Thunderous Shout",
 
-      // original targets: Everything in a \areasmall cone from you
       attack: {
-        hit: `Each subject takes 2d6 + half \\glossterm{power} sonic damage
-
-        \\rankline
-        The damage increases by +1d for each rank beyond 3..`,
+        hit: `Each subject takes 2d6 + half \\glossterm{power} sonic damage.`,
         targeting: `
-        Make an attack vs. Fortitude against each subject.
+          Make an attack vs. Fortitude against everything in a \\smallarea cone from you.
         `,
       },
-
+      scaling: "damage",
       rank: 3,
       type: "Instant",
     },
 
     {
-      name: "Thunderous Bellow",
+      name: "Greater Thunderous Shout",
 
-      // original targets: Everything in a \areahuge cone from you
       attack: {
         hit: `Each subject takes 4d6 + half \\glossterm{power} sonic damage.`,
         targeting: `
-        Make an attack vs. Fortitude against each subject.
+          Make an attack vs. Fortitude against everything in a \\hugearea cone from you.
         `,
       },
-
       rank: 6,
       type: "Instant",
     },
@@ -122,12 +101,10 @@ export const heraldOfWar: CombatStyle = {
     {
       name: "Rally the Troops",
 
-      // original targets: You and your \glossterm{allies} within a \areamed radius from you
       effect: `
-        Each subject can ignore any effects from one \\glossterm{condition} it is already affected by until the end of the next round.
-        Because this ability has the \\glossterm{Swift} tag, it allows your allies to ignore conditions they would be affected by during the current phase.
-        `,
-
+        You and your \\glossterm{allies} within a \\medarea radius from you can each ignore any effects from one \\glossterm{condition} they area already affected by until the end of the next round.
+        Because this ability has the \\glossterm{Swift} tag, it removes any relevant penalties from those conditions during the current phase.
+      `,
       rank: 2,
       scaling: {
         4: "The area increases to a \\largearea radius from you.",
@@ -145,8 +122,7 @@ export const heraldOfWar: CombatStyle = {
         You take a -2d penalty to damage with the strike, and your \\glossterm{power} is halved.
         Each creature that takes damage from the strike takes a -2 penalty to \\glossterm{accuracy} against creatures other than you as a \\glossterm{condition}.
         This condition is removed if another creature applies this condition to the same target.
-        `,
-
+      `,
       rank: 2,
       type: "Duration",
     },
@@ -158,8 +134,7 @@ export const heraldOfWar: CombatStyle = {
         Make a \\glossterm{strike}.
         You take a -2d penalty to damage with the strike.
         Each creature that loses \\glossterm{hit points} from the strike is \\glossterm{shaken} by you as a \\glossterm{condition}.
-        `,
-
+      `,
       rank: 2,
       tags: ["Emotion"],
       type: "Duration",
@@ -172,8 +147,7 @@ export const heraldOfWar: CombatStyle = {
         Make a \\glossterm{strike}.
         You take a -2d penalty to damage with the strike.
         Each creature that loses hit points from the strike is \\glossterm{frightened} by you as a \\glossterm{condition}.
-        `,
-
+      `,
       rank: 5,
       tags: ["Emotion"],
       type: "Duration",
