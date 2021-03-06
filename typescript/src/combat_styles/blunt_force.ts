2f3c3acdec98ba1c8529dda9161459795184f25f
Kevin Johnson <vadskye@gmail.com>
2021-03-06 11:24:21 -0800

    fully convert blunt force, dirty fighting
    Former-commit-id: 0821a147dab779ba8046ee0d150fc3d16dcbfc8e

--- typescript/src/combat_styles/blunt_force.ts
+++ typescript/src/combat_styles/blunt_force.ts
@@ -11,7 +11,7 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a \\glossterm{strike} using a bludgeoning weapon.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         You take a -2d penalty to damage with the strike, and your \\glossterm{power} is halved.
         Each creature that loses hit points from the strike is \\glossterm{sickened} as a \\glossterm{condition}.
         `,
@@ -25,7 +25,7 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a \\glossterm{strike} using a bludgeoning weapon.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         You take a -2d penalty to damage with the strike, and your \\glossterm{power} is halved.
         Each creature that loses hit points from the strike is \\glossterm{nauseated} as a \\glossterm{condition}.
         `,
@@ -38,7 +38,7 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a \\glossterm{strike} using a bludgeoning weapon.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         Your \\glossterm{power} with the strike is halved.
         Each creature that loses hit points from the strike is \\glossterm{nauseated} as a \\glossterm{condition}.
         `,
@@ -52,7 +52,7 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a \\glossterm{strike} using a bludgeoning weapon.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         You take -1d penalty to damage with the strike.
         `,
 
@@ -65,7 +65,7 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a \\glossterm{strike} using a bludgeoning weapon with a -2 penalty to accuracy.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         You gain a +1d bonus to damage with the strike.
         `,
 
@@ -76,19 +76,15 @@ export const bluntForce: CombatStyle = {
     {
       name: "Ground Stomp",
 
-      // original targets: Everything adjacent to you that is on solid ground
       attack: {
-        glance: `Half damage.
-
-        \\rankline
-        The damage increases by +1d for each rank beyond 3.`,
+        glance: `Half damage.`,
         hit: `Each subject takes 2d6 + half \\glossterm{power} bludgeoning damage
         Each Large or smaller creature that loses \\glossterm{hit points} from this damage is knocked \\glossterm{prone}..`,
         targeting: `
-        Make an attack vs. Reflex against each subject.
+          Make an attack vs. Reflex against everything adjacent to you that is on the same stable surface as you.
         `,
       },
-
+      scaling: "damage",
       rank: 3,
       type: "Instant",
     },
@@ -96,17 +92,15 @@ export const bluntForce: CombatStyle = {
     {
       name: "Earthshatter Stomp",
 
-      // original targets: Everything in a \areamed radius from you that is on solid ground
       attack: {
         glance: `Half damage.`,
         hit: `Each subject takes 4d6 + half \\glossterm{power} bludgeoning damage
         Each Large or smaller creature that loses \\glossterm{hit points} from this damage is knocked \\glossterm{prone}..`,
         targeting: `
-        Make an attack vs. Reflex against each subject.
-        You take a -4 penalty to accuracy with this attack against your \\glossterm{allies}.
+          Make an attack vs. Reflex against everything in a \\medarea radius from you that is on the same stable surface as you.
+          You take a -4 penalty to accuracy with this attack against your \\glossterm{allies}.
         `,
       },
-
       rank: 6,
       type: "Instant",
     },
@@ -114,13 +108,12 @@ export const bluntForce: CombatStyle = {
     {
       name: "Ground Slam",
 
-      // original targets: Everything on the ground in a \areasmall, 10 ft. wide line from you
       effect: `
-        Make a melee \\glossterm{strike} using a bludgeoning weapon against each subject.
+        Make a melee \\glossterm{strike} using a bludgeoning weapon against a stable surface.
+        The strike targets everything on that surface in a \\smallarealong, 10 ft. wide line from you.
         Your \\glossterm{power} with the strike is halved.
         All damage dealt by this attack is bludgeoning damage instead of its normal types.
-        `,
-
+      `,
       rank: 3,
       type: "Instant",
     },
@@ -128,13 +121,12 @@ export const bluntForce: CombatStyle = {
     {
       name: "Titanic Slam",
 
-      // original targets: Everything on the ground in a \arealarge, 10 ft. wide line from you
       effect: `
-        Make a melee \\glossterm{strike} using a bludgeoning weapon against each subject.
+        Make a melee \\glossterm{strike} using a bludgeoning weapon against a stable surface.
+        The strike targets everything on that surface in a \\smallarealong, 10 ft. wide line from you.
         Your \\glossterm{power} with the strike is halved.
         All damage dealt by this attack is bludgeoning damage instead of its normal types.
-        `,
-
+      `,
       rank: 5,
       type: "Instant",
     },
@@ -145,8 +137,7 @@ export const bluntForce: CombatStyle = {
       effect: `
         Make a strike using a bludgeoning weapon.
         Damage dealt by the strike is sonic damage in addition to its normal damage types.
-        `,
-
+      `,
       rank: 3,
       type: "Instant",
     },
@@ -156,11 +147,10 @@ export const bluntForce: CombatStyle = {
 
       effect: `
         Make a strike using a bludgeoning weapon.
-        The attack is made against each subject's Fortitude defense instead of its Armor defense.
+        The attack is made against each target's Fortitude defense instead of its Armor defense.
         You take -1d penalty to damage with the strike.
         Damage dealt by the strike is sonic damage in addition to its normal damage types.
-        `,
-
+      `,
       rank: 4,
       type: "Instant",
     },
@@ -207,19 +197,16 @@ export const bluntForce: CombatStyle = {
     {
       name: "Leap Slam",
 
-      // original targets: Everything adjacent to you that is on earth or unworked stone (see text)
       attack: {
-        glance: `Half damage.
-
-        \\rankline
-        The damage increases by +1d for each rank beyond 4.`,
+        glance: `Half damage.`,
         hit: `Each subject takes 2d8 + half \\glossterm{power} bludgeoning damage.`,
         targeting: `
-        You make a Jump check to leap and move as normal for the leap, up to a maximum distance equal to your \\glossterm{base speed} (see \\pcref{Leap}).
-        When you land, if the vertical distance in feet between the highest point of your leap and your landing point was at least ten feet, make an attack vs. Reflex against each subject.
+          You make a Jump check to leap and move as normal for the leap, up to a maximum distance equal to your \\glossterm{base speed} (see \\pcref{Leap}).
+          When you land, if the vertical distance in feet between the highest point of your leap and your landing point was at least ten feet, you emit a small shockwave.
+          If you do, make an attack vs. Reflex against everything adjacent to you that is on the same stable surface as you.
         `,
       },
-
+      scaling: "damage",
       rank: 4,
       type: "Instant",
     },
@@ -227,9 +214,8 @@ export const bluntForce: CombatStyle = {
     {
       name: "Ricochet",
 
-      // original targets: Up to three creatures or objects in a \areasmall radius within \shortrange (see text)
       effect: `
-        Make a thrown \\glossterm{strike} using a bludgeoning weapon against each subject.
+        Make a thrown \\glossterm{strike} using a bludgeoning weapon against up to three creatures or objects in a \\smallarea radius within \\shortrange.
         Your \\glossterm{power} with the strike is halved.
         If you choose yourself as one of the subjects, you can catch the weapon instead of taking damage from it.
       `,
