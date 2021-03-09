27c4636d27408f66b341f67b1ec9c402f89734a9
Kevin Johnson <vadskye@gmail.com>
2021-03-08 18:26:14 -0800

    move skill modifiers to ref tab
    Former-commit-id: f2e46c846a387619861cbdb5310dbb638c91ce6f

--- character_sheet/reference_page.py
+++ character_sheet/reference_page.py
@@ -18,12 +18,13 @@ from sheet_worker import standard_damage_at_power
 
 
 def create_page():
-    return flex_row(
+    return flex_col(
         {"class": "page reference-page"},
-        [
+        flex_row({'class': 'reference-row'}, [
             standard_damage(),
             vital_wound_chart(),
-        ],
+            skill_modifiers(),
+        ]),
     )
 
 
@@ -99,3 +100,40 @@ def vital_roll_effect(roll):
         10: "-1 vital rolls",
         "11+": "No effect",
     }[roll]
+
+def skill_modifiers():
+    return flex_col([
+        flex_wrapper(
+            div(
+                {"class": "section-header skill-modifiers"},
+                "Skill Modifiers",
+            )
+        ),
+        flex_row(
+            {"class": "skill-modifier-reminder"},
+            [
+                flex_col(
+                    [
+                        div(
+                            {"class": "skill-modifier-reminder-header"},
+                            "Training Level",
+                        ),
+                        div("Untrained"),
+                        div("Trained"),
+                        div("Mastered"),
+                    ]
+                ),
+                flex_col(
+                    [
+                        div(
+                            {"class": "skill-modifier-reminder-header"},
+                            "Modifier",
+                        ),
+                        div("Base attribute"),
+                        div("1 + half level + base attribute"),
+                        div("3 + level + base attribute"),
+                    ]
+                ),
+            ],
+        ),
+    ])
