fb74fee43b832b7cb5cf430c339e57d9f4721a36
Kevin Johnson <vadskye@gmail.com>
2021-03-19 08:19:21 -0700

    update hit points of monsters
    Former-commit-id: 59473ea200c595037cbdb44d1c58813b92454dc3

--- character_sheet/sheet_worker.py
+++ character_sheet/sheet_worker.py
@@ -557,8 +557,8 @@ def hit_points():
             var hit_points = hit_points_from_level + constitution + {sum_variables(misc)};
             var cr_multiplier = {{
                 0: 1,
-                0.5: 0.25,
-                1: 0.5,
+                0.5: 0.5,
+                1: 1,
                 2: 1,
                 3: 2,
                 // TODO: represent universal resistance in the roll20 sheet.
