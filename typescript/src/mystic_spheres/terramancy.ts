e6b610f427f1be61ddc6f0e8d1193e5647c62ced
Kevin Johnson <vadskye@gmail.com>
2021-04-26 16:50:41 -0700

    icecraft and earthcraft make more items
    Former-commit-id: 5654ac21eae664cef6a9d60a9ea355805df2b526

--- typescript/src/mystic_spheres/terramancy.ts
+++ typescript/src/mystic_spheres/terramancy.ts
@@ -63,20 +63,19 @@ export const terramancy: MysticSphere = {
       name: "Earthcraft",
 
       effect: `
-        You create a weapon or suit of armor from a body of earth or stone within 5 feet of you.
+        You create up to three weapons, suits of body armor, or shields from a body of earth or stone within 5 feet of you.
         You can create any weapon, shield, or body armor that you are proficient with, and which could normally be made entirely from metal, except for heavy armor.
-        The body targeted must be at least as large as the item you create.
-        The item appears in your hands.
+        The body targeted must be at least as large as the largest item you create.
 
-        The item functions like a normal item of its type, except that any \\glossterm{strikes} that you make with a weapon created with this ability are \\glossterm{magical} abilities, so you use your magical \\glossterm{power} to determine their damage instead of your \\glossterm{mundane} power.
+        An item created with this spell functions like a normal item of its type, except that any \\glossterm{strikes} that you make with a weapon created with this ability are \\glossterm{magical} abilities, so you use your magical \\glossterm{power} to determine their damage instead of your \\glossterm{mundane} power.
       `,
       rank: 1,
       scaling: {
         3: `
           You can also create heavy armor.
-          In addition, the item created is magically enhanced.
+          In addition, the items is magically enhanced.
           A weapon grants a +1 \\glossterm{magic bonus} to \\glossterm{accuracy} with strikes using the weapon,
-            and armor grants a +1 \\glossterm{magic bonus} to Armor defense.
+            and both shields and body armor grant a +1 \\glossterm{magic bonus} to Armor defense.
         `,
         5: `The magic bonus increases to +2.`,
         7: `The magic bonus increases to +3.`,
