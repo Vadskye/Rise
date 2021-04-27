0979c1c413af0788ee4f2bdb4d950a8ef02d5c57
Kevin Johnson <vadskye@gmail.com>
2021-04-27 16:29:17 -0700

    redesign damage script to show resistances
    Former-commit-id: e15cffd1c36f41752922e537709a0dcd979ece31

--- character_sheet/api_scripts/bloodied.js
+++ character_sheet/api_scripts/bloodied.js
@@ -1,4 +1,4 @@
-function updateBloodied(obj) {
+function updateHitPoints(obj) {
     const bar1_value = Number(obj.get("bar1_value"));
     const bar1_max = Number(obj.get("bar1_max"));
     
@@ -12,33 +12,52 @@ function updateBloodied(obj) {
     if (bar1_value >= bar1_max) {
         // At full HP, so remove all wound markers
         obj.set({
-            "status_red": false,
+            "status_green": false,
             "status_half-heart": false,
             "status_dead": false,
         })
     } else if (bar1_value > bar1_max / 2) {
         // The object has taken damage, but is not bloodied
         obj.set({
-            "status_red": true,
+            "status_green": true,
             "status_half-heart": false,
             "status_dead": false,
         });
     } else if (bar1_value > 0) {
         // The object is bloodied
         obj.set({
-            "status_red": false,
+            "status_green": false,
             "status_half-heart": true,
             "status_dead": false,
         });
     } else {
         // The object is dead
         obj.set({
-            "status_red": false,
+            "status_green": false,
             "status_half-heart": false,
             "status_dead": true,
         });
     }
 }
+on("change:graphic:bar1_value", updateHitPoints);
+on("change:graphic:bar1_max", updateHitPoints);
 
-on("change:graphic:bar1_value", updateBloodied);
-on("change:graphic:bar1_max", updateBloodied);
+function generateResistanceUpdater(barNumber, color) {
+  return (obj) => {
+    const value = Number(obj.get(`bar${barNumber}_value`));
+    const max = Number(obj.get(`bar${barNumber}_max`));
+
+    // Set the given value to be the new max for convenience when setting
+    // up tokens for the first time
+    if (!max) {
+      obj.set({ [`bar${barNumber}_max`]: value});
+      return;
+    }
+
+    obj.set({
+      [`status_${color}`]: value <= 0,
+    });
+  }
+}
+on("change:graphic:bar2_value", generateResistanceUpdater(2, "blue"));
+on("change:graphic:bar3_value", generateResistanceUpdater(3, "red"));
