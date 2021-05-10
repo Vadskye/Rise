3c289693f5e3b26f0d7861182fab05795f2a8bac
Kevin Johnson <vadskye@gmail.com>
2021-05-10 09:41:50 -0700

    fix undead vivimancy healing

--- typescript/src/monsters/types/types.ts
+++ typescript/src/monsters/types/types.ts
@@ -149,7 +149,7 @@ export const typeDescriptions: Record<MonsterType, string | null> = {
     \\parhead{Unliving} Undead are not living creatures, and are immune to most abilities that only affect living creatures.
     However, they are affected in a specific way by effects from the \\sphere{vivimancy} mystic sphere.
     Undead are always considered an \\glossterm{ally} for \\sphere{vivimancy} spells.
-    Any effect from the \\sphere{vivimancy} sphere that would directly cause an undead creature to lose hit points without dealing damage causes that creature to regain that many lost hit points instead.
+    Any effect from the \\sphere{vivimancy} sphere that would deal energy damage to an undead creature causes that creature to regain that many lost hit points instead.
     In addition, any effect from the \\sphere{vivimancy} sphere that would cause an undead creature to regain lost hit points instead causes it to lose that many hit points instead.
     \\parhead{Unnatural Mind} Undead are controlled by fragments of the souls of deceased creatures.
     Many undead are \\glossterm{mindless}, and even intelligent undead are immune to \\abilitytag{Compulsion} and \\abilitytag{Emotion} abilities.
