668ad13f6df90152552f32422a0bda9ffee54e68
Kevin Johnson <vadskye@gmail.com>
2021-05-12 15:27:09 -0700

    restore old monster hit points

--- character_sheet/sheet_worker.py
+++ character_sheet/sheet_worker.py
@@ -525,7 +525,7 @@ def hit_points():
     return js_wrapper(
         ["level", "constitution", "challenge_rating", *misc],
         f"""
-            var player_hit_points_from_level = {{
+            var hit_points_from_level = {{
                 '-1': 9  ,
                 0:    10 ,
                 1:    11 ,
@@ -559,41 +559,6 @@ def hit_points():
                 29:   245,
                 30:   260,
             }}[level] || 1;
-            var monster_hit_points_from_level = {{
-                '-1': 9  ,
-                "0":  10 ,
-                "1":  11 ,
-                "2":  12 ,
-                "3":  13 ,
-                "4":  17 ,
-                "5":  19 ,
-                "6":  22 ,
-                "7":  28 ,
-                "8":  31 ,
-                "9":  35 ,
-                "10": 44 ,
-                "11": 50 ,
-                "12": 56 ,
-                "13": 70 ,
-                "14": 78 ,
-                "15": 88 ,
-                "16": 100,
-                "17": 126,
-                "18": 140,
-                "19": 156,
-                "20": 200,
-                "21": 230,
-                "22": 260,
-                "23": 290,
-                "24": 320,
-                "25": 350,
-                "26": 380,
-                "27": 410,
-                "28": 440,
-                "29": 470,
-                "30": 500,
-            }}[level] || 1;
-            var hit_points_from_level = challenge_rating ? monster_hit_points_from_level : player_hit_points_from_level;
 
             var hit_points = hit_points_from_level + constitution + {sum_variables(misc)};
             var cr_multiplier = {{
