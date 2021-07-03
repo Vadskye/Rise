9ab8beee258fe9283acfab5013ab618f30dda186
Kevin Johnson <vadskye@gmail.com>
2021-07-03 16:01:53 -0700

    companions share all magic bonuses

--- rust/src/classes/archetype_rank_abilities/druid.rs
+++ rust/src/classes/archetype_rank_abilities/druid.rs
@@ -663,14 +663,13 @@ pub fn wildspeaker<'a>() -> Vec<RankAbility<'a>> {
             Your magical connection to the animal improves its resilience and strength in combat.
             The animal's statistics use the values below, except that each animal also gains a special ability based on the environment you are in.
             \begin{itemize}
-                \item Its size category is Medium.
+                \item Its size category is Medium, and its \glossterm{base speed} is the normal base speed for its size (see \tref{Size in Combat}).
                 \item Its \glossterm{fatigue tolerance} is 0, and it cannot use abilities that would cause it to increase its \glossterm{fatigue level}.
-                \item Its \glossterm{hit points} are equal to the base value for your level (see \tref{Character Advancement}).
-                \item Its \glossterm{damage resistance} is equal to the base value for your level (see \pcref{Character Advancement}).
-                \item Each of its \glossterm{defenses} is equal to 5 \add your level.
-                \item Its \glossterm{accuracy} is equal to your level \add half your base Perception \add your \glossterm{magic bonuses} to accuracy.
+                \item Its \glossterm{hit points} are equal to the base value for your level \add your \glossterm{magic bonus} to hit points (see \tref{Character Advancement}).
+                \item Its \glossterm{damage resistance} is equal to the base value for your level \add your \glossterm{magic bonus} to damage resistance (see \pcref{Character Advancement}).
+                \item Each of its \glossterm{defenses} is equal to 5 \add your level \add your \glossterm{magic bonus} to each corresponding defense.
+                \item Its \glossterm{accuracy} is equal to your level \add half your base Perception \add your \glossterm{magic bonus} to accuracy.
                 \item Its \glossterm{power} with its attacks is equal to your \glossterm{magic bonus} to \glossterm{magical power}.
-                \item Its \glossterm{base speed} is the normal base speed for its size (see \tref{Size in Combat}).
                 \item It has no \glossterm{attunement points}.
                 \item The damage dealt by its natural weapons increases by \plus1d for each rank in this archetype beyond 1.
             \end{itemize}
