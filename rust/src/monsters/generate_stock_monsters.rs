cc067c7688cad639fa951f6905546a5080b1862f
Kevin Johnson <vadskye@gmail.com>
2021-08-01 08:26:59 -0500

    geenrate a small number of stock monsters

--- rust/src/monsters/generate_stock_monsters.rs
+++ rust/src/monsters/generate_stock_monsters.rs
@@ -8,27 +8,37 @@ use crate::monsters::{FullMonsterDefinition, Monster};
 pub fn generate_stock_monsters() -> Vec<MonsterEntry> {
     let mut stock_monsters: Vec<MonsterEntry> = vec![];
     for creature_type in CreatureType::all() {
-        for level in 1..22 {
-            stock_monsters.push(MonsterEntry::Monster(Monster::fully_defined(
-                FullMonsterDefinition {
-                    alignment: "always true neutral".to_string(),
-                    attributes: creature_type.stock_base_attributes(level),
-                    challenge_rating: ChallengeRating::Two,
-                    creature_type,
-                    description: None,
-                    knowledge: None,
-                    level,
-                    movement_modes: None,
-                    name: format!("Stock {} {}", creature_type.name(), level),
-                    passive_abilities: None,
-                    senses: None,
-                    size: Size::Medium,
-                    special_attacks: None,
-                    special_defense_modifiers: None,
-                    trained_skills: None,
-                    weapons: vec![Weapon::Bite],
-                },
-            )));
+        for level in vec![1, 2, 3, 4, 5] {
+            for challenge_rating in vec![
+                ChallengeRating::Two,
+                ChallengeRating::Four,
+            ] {
+                stock_monsters.push(MonsterEntry::Monster(Monster::fully_defined(
+                    FullMonsterDefinition {
+                        alignment: "always true neutral".to_string(),
+                        attributes: creature_type.stock_base_attributes(level),
+                        creature_type,
+                        description: None,
+                        knowledge: None,
+                        level,
+                        movement_modes: None,
+                        name: format!(
+                            "Stock {} {} (CR {})",
+                            creature_type.name(),
+                            level,
+                            challenge_rating.to_string()
+                        ),
+                        passive_abilities: None,
+                        senses: None,
+                        size: Size::Medium,
+                        special_attacks: None,
+                        special_defense_modifiers: None,
+                        trained_skills: None,
+                        weapons: vec![Weapon::Bite],
+                        challenge_rating,
+                    },
+                )));
+            }
         }
     }
     return stock_monsters;
