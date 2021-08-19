68557d22d0f0cc0e09238e3d27717039396bdaf1
Kevin Johnson <vadskye@gmail.com>
2021-08-19 08:26:46 -0700

    nerf zealous warrior

--- rust/src/classes/archetype_rank_abilities/paladin.rs
+++ rust/src/classes/archetype_rank_abilities/paladin.rs
@@ -17,64 +17,59 @@ pub fn devoted_paragon<'a>() -> Vec<RankAbility<'a>> {
             is_magical: true,
             rank: 1,
             description: r"
-        Your devotion to your alignment affects the world around you, bringing it closer to your ideals.
-        You constantly radiate an aura in a \areamed radius \glossterm{emanation} from you.
-        You can suppress or resume the aura as a \glossterm{minor action}.
-        Whenever you resume the aura, you can choose which creatures within the area are affected by aura as any combination of yourself, your \glossterm{allies}, your \glossterm{enemies}, and other creatures.
-        The effect of the aura depends on your devoted alignment, as described below.
+                Your devotion to your alignment affects the world around you, bringing it closer to your ideals.
+                You constantly radiate an aura in a \areamed radius \glossterm{emanation} from you.
+                You can suppress or resume the aura as a \glossterm{minor action}.
+                Whenever you resume the aura, you can choose which creatures within the area are affected by aura as any combination of yourself, your \glossterm{allies}, your \glossterm{enemies}, and other creatures.
+                The effect of the aura depends on your devoted alignment, as described below.
 
-        \subparhead{Chaos} When a target rolls a 1 on an attack roll with a \glossterm{strike}, it \glossterm{explodes} (see \pcref{Exploding Attacks}.
-        This does not affect bonus dice rolled for exploding attacks (see \pcref{Exploding Attacks}).
-        \subparhead{Evil} Each target suffers a \minus1 penalty to \glossterm{defenses} as long as it is affected by at least one \glossterm{condition}.
-        % TODO: clarify what happens if multiple people try to Good aura the same target
-        \subparhead{Good} When a target gains a \glossterm{vital wound}, you may gain a \glossterm{vital wound} instead.
-        You gain a \plus2 bonus to the \glossterm{vital roll} of each \glossterm{vital wound} you gain this way.
-        The target suffers any other effects of the attack normally.
-        \subparhead{Law} When a target rolls a 1 on an attack roll with a \glossterm{strike}, the attack roll is treated as a 6.
-        This does not affect bonus dice rolled for exploding attacks (see \pcref{Exploding Attacks}).
-
-                ",
+                \subparhead{Chaos} When a target rolls a 1 on an attack roll with a \glossterm{strike}, it \glossterm{explodes} (see \pcref{Exploding Attacks}.
+                This does not affect bonus dice rolled for exploding attacks (see \pcref{Exploding Attacks}).
+                \subparhead{Evil} Each target suffers a \minus1 penalty to \glossterm{defenses} as long as it is affected by at least one \glossterm{condition}.
+                % TODO: clarify what happens if multiple people try to Good aura the same target
+                \subparhead{Good} When a target gains a \glossterm{vital wound}, you may gain a \glossterm{vital wound} instead.
+                You gain a \plus2 bonus to the \glossterm{vital roll} of each \glossterm{vital wound} you gain this way.
+                The target suffers any other effects of the attack normally.
+                \subparhead{Law} When a target rolls a 1 on an attack roll with a \glossterm{strike}, the attack roll is treated as a 6.
+                This does not affect bonus dice rolled for exploding attacks (see \pcref{Exploding Attacks}).
+            ",
         },
         RankAbility {
             name: "Aligned Immunity",
             is_magical: true,
             rank: 2,
             description: r"
-        Your devotion to your alignment grants you immunities.
+                Your devotion to your alignment grants you immunities.
 
-        \subparhead{Chaos} You are immune to the \slowed, \decelerated, and \immobilized effects.
-        \subparhead{Evil} You are immune to poisons and diseases.
-        \subparhead{Good} You are immune to the \shaken, \frightened, and \panicked effects.
-        \subparhead{Law} You are immune to the \dazed, \stunned, \disoriented, and \confused effects.
-
-                ",
+                \subparhead{Chaos} You are immune to the \slowed, \decelerated, and \immobilized effects.
+                \subparhead{Evil} You are immune to poisons and diseases.
+                \subparhead{Good} You are immune to the \shaken, \frightened, and \panicked effects.
+                \subparhead{Law} You are immune to the \dazed, \stunned, \disoriented, and \confused effects.
+            ",
         },
         RankAbility {
             name: "Paragon Power",
             is_magical: false,
             rank: 3,
             description: r"
-        
-        You gain a \plus2 bonus to your \glossterm{power} with all abilities.
-
-                ",
+                You gain a \plus2 bonus to your \glossterm{power} with all abilities.
+            ",
         },
         RankAbility {
             name: "Greater Aligned Aura",
             is_magical: true,
             rank: 4,
             description: r"
-        The effect of your \textit{aligned aura} becomes stronger, as described below.
+                The effect of your \textit{aligned aura} becomes stronger, as described below.
 
-        \subparhead{Chaos} The effect applies to all attacks, not just \glossterm{strikes}.
-        % TODO: explain how this works on monsters
-        \subparhead{Evil} Whenever a target removes a \glossterm{condition}, it takes 2d8 \add half \glossterm{power} energy damage.
-        This damage increases by \plus1d for each rank beyond 4.
-        \subparhead{Good} When a target would lose \glossterm{hit points}, you may lose those hit points instead.
-        The target suffers any other effects of the attack normally, though it is not treated as if it lost hit points from the attack for the purpose of special attack effects.
-        \subparhead{Law} The effect applies to all attacks, not just \glossterm{strikes}.
-
-                ",
+                \subparhead{Chaos} The effect applies to all attacks, not just \glossterm{strikes}.
+                % TODO: explain how this works on monsters
+                \subparhead{Evil} Whenever a target removes a \glossterm{condition}, it takes 2d8 \add half \glossterm{power} energy damage.
+                This damage increases by \plus1d for each rank beyond 4.
+                \subparhead{Good} When a target would lose \glossterm{hit points}, you may lose those hit points instead.
+                The target suffers any other effects of the attack normally, though it is not treated as if it lost hit points from the attack for the purpose of special attack effects.
+                \subparhead{Law} The effect applies to all attacks, not just \glossterm{strikes}.
+            ",
         },
         RankAbility {
             name: "Greater Aligned Immunity",
@@ -395,111 +390,93 @@ pub fn zealous_warrior<'a>() -> Vec<RankAbility<'a>> {
             is_magical: false,
             rank: 0,
             description: r"
-         You gain a \plus2 bonus to any roll that you use the \textit{desperate exertion} ability on.
-        This bonus stacks with the normal \plus2 bonus provided by that ability.
-
-
-                ",
+                You gain a \plus2 bonus to any roll that you use the \textit{desperate exertion} ability on.
+                This bonus stacks with the normal \plus2 bonus provided by that ability.
+            ",
         },
         RankAbility {
             name: "Smite",
             is_magical: true,
             rank: 1,
             description: r"
-        You can use the \textit{smite} ability as a standard action.
-        \begin{instantability}{Smite}[Instant]
-            \abilitytag{Magical}
-            \rankline
-            Make a \glossterm{strike} with a \plus1d damage bonus.
-            Because this is a \glossterm{magical} ability, you use your magical \glossterm{power} to determine your damage instead of your \glossterm{mundane} power.
-            If your target shares your devoted alignment, you take a single point of \glossterm{energy damage} as feedback from the attack warning you that you are persecuting a creature that shares your alignment.
+                You can use the \textit{smite} ability as a standard action.
+                \begin{instantability}{Smite}[Instant]
+                    \abilitytag{Magical}
+                    \rankline
+                    Make a \glossterm{strike} with a \plus1d damage bonus.
+                    Because this is a \glossterm{magical} ability, you use your magical \glossterm{power} to determine your damage instead of your \glossterm{mundane} power.
+                    If your target shares your devoted alignment, you take a single point of \glossterm{energy damage} as feedback from the attack warning you that you are persecuting a creature that shares your alignment.
 
-            \rankline
-            \rank{3} The damage bonus increases to \plus2d.
-            \rank{5} The damage bonus increases to \plus3d.
-            \rank{7} The damage bonus increases to \plus4d.
-        \end{instantability}
-
-                ",
+                    \rankline
+                    \rank{3} The damage bonus increases to \plus2d.
+                    \rank{5} The damage bonus increases to \plus3d.
+                    \rank{7} The damage bonus increases to \plus4d.
+                \end{instantability}
+            ",
         },
         RankAbility {
-            name: "Zealous Offense",
-            is_magical: false,
+            name: "Zealous Fixation",
+            is_magical: true,
             rank: 2,
             description: r"
-         You gain a \plus1 bonus to \glossterm{accuracy}.
-
-                ",
+                Whenever you hit a creature with a \glossterm{strike}, you ignore all \glossterm{miss chances} against that creature with your attacks until you take a \glossterm{short rest} or until you hit a different creature with a strike.
+                If you hit multiple creatures with the same strike, you may freely choose which creature to fixate on with this ability.
+            ",
         },
         RankAbility {
             name: "Glancing Strikes",
             is_magical: false,
             rank: 3,
             description: r"
-         Whenever you miss by 2 or less with a \glossterm{strike}, the target takes half damage from the strike.
-        This is called a \glossterm{glancing blow}.
-
-                ",
+                Whenever you miss by 2 or less with a \glossterm{strike}, the target takes half damage from the strike.
+                This is called a \glossterm{glancing blow}.
+            ",
         },
         RankAbility {
             name: "Forceful Zeal",
             is_magical: false,
             rank: 4,
             description: r"
-         You gain a \plus1d bonus to your damage with all weapons.
-
-                ",
+                You gain a \plus1d bonus to your damage with all weapons.
+            ",
         },
         RankAbility {
             name: "Zealous Purge",
             is_magical: true,
             rank: 4,
             description: r"
-        You can use your \textit{zealous purge} ability as a standard action.
-        \begin{instantability}{Zealous Purge}[Instant]
-            \abilitytag{Magical}
-            \rankline
-            Make a \glossterm{strike} with a \minus1d damage penalty.
-            You add half your \glossterm{power} to damage with the strike instead of your full power.
-            Because this is a \glossterm{magical} ability, you use your \glossterm{magical} \glossterm{power} to determine your damage instead of your \glossterm{mundane} power.
-            If your target shares your devoted alignment, you take a single point of \glossterm{energy damage} as feedback from the attack warning you that you are persecuting a creature that share your alignment.
+                You can use your \textit{zealous purge} ability as a standard action.
+                \begin{instantability}{Zealous Purge}[Instant]
+                    \abilitytag{Magical}
+                    \rankline
+                    Make a \glossterm{strike} with a \minus1d damage penalty.
+                    You add half your \glossterm{power} to damage with the strike instead of your full power.
+                    Because this is a \glossterm{magical} ability, you use your \glossterm{magical} \glossterm{power} to determine your damage instead of your \glossterm{mundane} power.
+                    If your target shares your devoted alignment, you take a single point of \glossterm{energy damage} as feedback from the attack warning you that you are persecuting a creature that shares your alignment.
 
-            If the target takes damage from the strike, it stops being \glossterm{attuned} to one effect.
-            It can freely choose which effect it releases its attunement to.
+                    If the target takes damage from the strike, it stops being \glossterm{attuned} to one effect.
+                    It can freely choose which effect it releases its attunement to.
 
-            \rankline
-            \rank{6} The target stops being attuned to two effects instead of one.
-        \end{instantability}
-
-                ",
+                    \rankline
+                    \rank{6} The target stops being attuned to two effects instead of one.
+                \end{instantability}
+            ",
         },
         RankAbility {
-            name: "Greater Zealous Offense",
-            is_magical: false,
+            name: "Greater Zealous Fixation",
+            is_magical: true,
             rank: 5,
             description: r"
-         The bonus from your \textit{zealous offense} ability increases to \plus2.
-
-                ",
+                Your \textit{zealous fixation} ability also grants you a +1 \glossterm{accuracy} bonus against the creature you are fixating on.
+            ",
         },
         RankAbility {
             name: "Greater Zealous Exertion",
             is_magical: false,
             rank: 6,
             description: r"
-         The bonus from your \textit{zealous exertion} ability increases to \plus5.
-
-                ",
-        },
-        RankAbility {
-            name: "Zealous Fixation",
-            is_magical: true,
-            rank: 6,
-            description: r"
-         Whenever you hit a creature with a \glossterm{strike}, you ignore \glossterm{concealment} and all \glossterm{miss chances} against that creature with your attacks until you take a \glossterm{short rest} or until you hit a different creature with a strike.
-        If you hit multiple creatures with the same strike, you may freely choose which creature to fixate on with this ability.
-
-                ",
+                The bonus from your \textit{zealous exertion} ability increases to \plus4.
+            ",
         },
         RankAbility {
             name: "Greater Forceful Zeal",
@@ -514,21 +491,20 @@ pub fn zealous_warrior<'a>() -> Vec<RankAbility<'a>> {
             is_magical: true,
             rank: 7,
             description: r"
-         You can use the \textit{pass judgment} ability as a \glossterm{minor action}.
-        \begin{durationability}{Pass Judgment}[Duration]
-            \abilitytag{Magical}
-            \rankline
-            \target{One creature within \rnglong range}
-            The target is treated as if it had the alignment opposed to your devoted alignment for the purpose of all abilities.
-            This only affects its alignment along the alignment axis your devoted alignment is on.
-            For example, if your devoted alignment was evil, a chaotic neutral target would be treated as chaotic good.
-            This ability lasts until you \glossterm{dismiss} it as a \glossterm{free action}.
+                You can use the \textit{pass judgment} ability as a \glossterm{minor action}.
+                \begin{durationability}{Pass Judgment}[Duration]
+                    \abilitytag{Magical}
+                    \rankline
+                    \target{One creature within \rnglong range}
+                    The target is treated as if it had the alignment opposed to your devoted alignment for the purpose of all abilities.
+                    This only affects its alignment along the alignment axis your devoted alignment is on.
+                    For example, if your devoted alignment was evil, a chaotic neutral target would be treated as chaotic good.
+                    This ability lasts until you \glossterm{dismiss} it as a \glossterm{free action}.
 
-            You can use this ability to do battle against foes who share your alignment, but you should exercise caution in doing so.
-            Persecution of those who share your ideals can lead you to fall and become an ex-paladin.
-        \end{durationability}
-
-                ",
+                    You can use this ability to do battle against foes who share your alignment, but you should exercise caution in doing so.
+                    Persecution of those who share your ideals can lead you to fall and become an ex-paladin.
+                \end{durationability}
+            ",
         },
     ];
 }
