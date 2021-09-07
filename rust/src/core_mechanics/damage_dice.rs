cd36c3c499d479534db77d9b142a95a429277815
Kevin Johnson <vadskye@gmail.com>
2021-09-07 10:37:09 -0700

    add tests to damage dice

--- rust/src/core_mechanics/damage_dice.rs
+++ rust/src/core_mechanics/damage_dice.rs
@@ -11,10 +11,10 @@ impl DamageDice {
         return 4;
     }
     pub fn d8() -> i32 {
-        return 4;
+        return 5;
     }
     pub fn d10() -> i32 {
-        return 4;
+        return 6;
     }
 
     pub fn new(increments: i32) -> DamageDice {
@@ -72,5 +72,41 @@ impl DamageDice {
 
     pub fn single_target_damage(rank: i32) -> Self {
         return Self::new(Self::d10() + (rank - 1));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn standard_dice_values() {
+        let d6 = DamageDice::new(DamageDice::d6());
+        assert_eq!("1d6", d6.to_string());
+        let d8 = DamageDice::new(DamageDice::d8());
+        assert_eq!("1d8", d8.to_string());
+        let d10 = DamageDice::new(DamageDice::d10());
+        assert_eq!("1d10", d10.to_string());
+    }
+
+    #[test]
+    fn increasing_size() {
+        let d6 = DamageDice::new(DamageDice::d6());
+        assert_eq!("1d8", d6.add(1).to_string());
+        assert_eq!("1d10", d6.add(2).to_string());
+        assert_eq!("2d6", d6.add(3).to_string());
+        assert_eq!("2d8", d6.add(4).to_string());
+    }
+
+    #[test]
+    fn decreasing_size() {
+        let twod8 = DamageDice::new(DamageDice::d6() + 4);
+        assert_eq!("2d8", twod8.to_string());
+        assert_eq!("2d6", twod8.add(-1).to_string());
+        assert_eq!("1d10", twod8.add(-2).to_string());
+        assert_eq!("1d6", twod8.add(-4).to_string());
+        assert_eq!("1d3", twod8.add(-6).to_string());
+        assert_eq!("1d2", twod8.add(-7).to_string());
+        assert_eq!("1", twod8.add(-8).to_string());
     }
 }
