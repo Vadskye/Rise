85330fb261618671004719c38110caf9b5856095
Kevin Johnson <vadskye@gmail.com>
2021-09-07 13:36:23 -0700

    add HasModifiers infrastructure

--- rust/tests/creature_test.rs
+++ rust/tests/creature_test.rs
@@ -1,3 +1,4 @@
+use rise::core_mechanics::creatures::{Modifier, HasModifiers};
 use rise::core_mechanics::creatures::creature::Creature;
 use rise::core_mechanics::{Defense, HasDefenses};
 use rise::equipment::{Armor, HasArmor};
@@ -38,5 +39,37 @@ fn it_calculates_armor_effects() {
         0,
         creature.calc_skill_modifier(&Skill::Devices),
         "Should have 0 Devices"
+    );
+}
+
+#[test]
+fn it_calculates_modifiers() {
+    let mut creature = Creature::new(1);
+    assert_eq!(
+        0,
+        creature.calc_defense(&Defense::Armor),
+        "Should have 0 AD"
+    );
+    assert_eq!(
+        0,
+        creature.calc_skill_modifier(&Skill::Climb),
+        "Should have 0 Climb"
+    );
+    assert_eq!(
+        0,
+        creature.calc_skill_modifier(&Skill::Devices),
+        "Should have 0 Devices"
+    );
+
+    creature.add_modifier(Modifier::Defense(Defense::Armor, 2));
+    assert_eq!(
+        2,
+        creature.calc_defense(&Defense::Armor),
+        "Should have 2 AD"
+    );
+    assert_eq!(
+        0,
+        creature.calc_defense(&Defense::Fortitude),
+        "Should have 0 Fort"
     );
 }
