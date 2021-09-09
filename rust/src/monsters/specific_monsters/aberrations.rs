e9b2e3fa0e919853525248a9f6d234e162a6898e
Kevin Johnson <vadskye@gmail.com>
2021-09-09 09:19:21 -0700

    introduce standard attacks

--- rust/src/monsters/specific_monsters/aberrations.rs
+++ rust/src/monsters/specific_monsters/aberrations.rs
@@ -1,6 +1,7 @@
 use crate::core_mechanics::creatures::attack_effects::{
     self, AttackEffect, AttackEffectDuration, DamageEffect, DebuffEffect,
 };
+use crate::core_mechanics::creatures::StandardAttack;
 use crate::core_mechanics::creatures::attacks::{
     AreaSize, AreaTargets, Attack, AttackRange, AttackTargeting, UsageTime,
 };
@@ -62,25 +63,6 @@ pub fn aberrations() -> Vec<MonsterEntry> {
     let mut monsters: Vec<MonsterEntry> = vec![];
 
     // TODO: add ritual casting
-    let mut aboleth_slam = Attack::from_weapon(Weapon::Slam);
-    if let Some(e) = aboleth_slam.damage_effect_mut() {
-        e.lose_hp_effects = Some(vec![attack_effects::AttackEffect::Poison(
-            attack_effects::PoisonEffect {
-                stage1: vec![Debuff::Nauseated],
-                stage3_debuff: None,
-                stage3_vital: Some(attack_effects::VitalWoundEffect {
-                    special_effect: Some("
-                        Instead of making a \\glossterm{vital roll} for the \\glossterm{vital wound},
-                          the target's skin is transformed into a clear, slimy membrane.
-                        Every 5 minutes, an afflicted creature must be moistened with cool, fresh water
-                          or it will gain two \\glossterm<fatigue points>.
-                        This effect lasts until the \\glossterm{vital wound} is removed.
-                    ".to_string()),
-                }),
-            },
-        )]);
-    }
-    aboleth_slam.glance = Some(AttackEffect::HalfDamage);
     monsters.push(MonsterEntry::Monster(aberration(FullAberrationDefinition {
         alignment: "Usually lawful evil".to_string(),
         attributes: vec![3, -1, 4, 3, 2, 4],
@@ -124,57 +106,9 @@ pub fn aberrations() -> Vec<MonsterEntry> {
         senses: Some(vec![Sense::Darkvision(240), Sense::Telepathy(900)]),
         size: Size::Huge,
         special_attacks: Some(vec![
-            aboleth_slam,
-            // Large enemies-only cone is a rank 4 effect
-            Attack {
-                accuracy: 0,
-                cooldown: None,
-                crit: None,
-                defense: Defense::Mental,
-                glance: Some(AttackEffect::HalfDamage),
-                hit: AttackEffect::Damage(
-                    DamageEffect {
-                        damage_dice: DamageDice::aoe_damage(5),
-                        damage_modifier: 0,
-                        damage_types: vec![DamageType::Energy],
-                        lose_hp_effects: None,
-                        power_multiplier: 0.5,
-                        take_damage_effects: None,
-                    },
-                ),
-                is_magical: true,
-                name: "Psionic Blast".to_string(),
-                targeting: AttackTargeting::Cone(AreaSize::Large, AreaTargets::Enemies),
-                usage_time: UsageTime::Standard,
-                weapon: None,
-            },
-            Attack {
-                accuracy: 2,
-                cooldown: None,
-                crit: None,
-                defense: Defense::Mental,
-                glance: Some(AttackEffect::HalfDamage),
-                hit: AttackEffect::Damage(
-                    DamageEffect {
-                        damage_dice: DamageDice::single_target_damage(5),
-                        damage_modifier: 0,
-                        damage_types: vec![DamageType::Energy],
-                        lose_hp_effects: Some(vec![
-                            AttackEffect::Debuff(DebuffEffect {
-                                debuffs: vec![Debuff::Dazed],
-                                duration: AttackEffectDuration::Condition
-                            }),
-                        ]),
-                        power_multiplier: 1.0,
-                        take_damage_effects: None,
-                    },
-                ),
-                is_magical: true,
-                name: "Mind Crush".to_string(),
-                targeting: AttackTargeting::Creature(AttackRange::Long),
-                usage_time: UsageTime::Minor,
-                weapon: None,
-            },
+            StandardAttack::AbolethSlam.attack(),
+            StandardAttack::AbolethPsionicBlast.attack(),
+            StandardAttack::MindCrush(5).attack(),
         ]),
         special_defense_modifiers: None,
         trained_skills: Some(vec![
