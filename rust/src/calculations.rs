9348c1c42f9c651badd0e727c96004e58a926dd9
Kevin Johnson <vadskye@gmail.com>
2021-09-10 09:51:23 -0700

    move damageable_creature to separate file

--- rust/src/calculations.rs
+++ rust/src/calculations.rs
@@ -1,1 +1,6 @@
+mod damageable_creature;
+pub mod randomized_combat;
 pub mod statistical_combat;
+
+use crate::core_mechanics::creatures::HasCreatureMechanics;
+pub use damageable_creature::DamageableCreature;
