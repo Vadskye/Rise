cacff3c4e2a63d1127178f90dee08b8df1ed404d
Kevin Johnson <vadskye@gmail.com>
2021-09-10 13:29:12 -0700

    move character to creatures folder

--- rust/src/classes.rs
+++ rust/src/classes.rs
@@ -1,11 +1,9 @@
 mod archetype_rank_abilities;
 mod archetypes;
 mod basic_class_abilities;
-mod character;
 mod class;
 
-pub use archetype_rank_abilities::{archetype_rank_abilities, RankAbility};
+pub use archetype_rank_abilities::{archetype_rank_abilities, calc_rank_abilities, RankAbility};
 pub use archetypes::ClassArchetype;
 pub use basic_class_abilities::generate_latex_basic_class_abilities;
-pub use character::{calc_rank_abilities, Character};
 pub use class::Class;
