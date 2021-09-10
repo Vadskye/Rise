657382496af73583aa25a883d406a398ac9a9bae
Kevin Johnson <vadskye@gmail.com>
2021-09-10 09:51:23 -0700

    move damageable_creature to separate file

---
+++ rust/src/calculations/damageable_creature.rs
@@ -0,0 +1,31 @@
+use crate::core_mechanics::creatures::HasCreatureMechanics;
+
+pub struct DamageableCreature<'a, T: HasCreatureMechanics> {
+    pub creature: &'a T,
+    pub damage_taken: i32,
+}
+
+impl<'a, T: HasCreatureMechanics> DamageableCreature<'a, T> {
+    pub fn from_creature(creature: &'a T) -> Self {
+        return DamageableCreature {
+            creature,
+            damage_taken: 0,
+        };
+    }
+
+    pub fn total_damage_absorption(&self) -> i32 {
+        return self.creature.calc_hit_points() + self.creature.calc_damage_resistance();
+    }
+
+    pub fn remaining_damage_absorption(&self) -> i32 {
+        return self.total_damage_absorption() - self.damage_taken;
+    }
+
+    pub fn is_alive(&self) -> bool {
+        return self.remaining_damage_absorption() > 0;
+    }
+
+    pub fn take_damage(&mut self, damage: f64) {
+        self.damage_taken += damage.ceil() as i32;
+    }
+}
