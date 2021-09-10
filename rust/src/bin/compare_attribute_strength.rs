1914ab50c6932aea47dda216d209724b6e9b3f93
Kevin Johnson <vadskye@gmail.com>
2021-09-09 17:47:34 -0700

    simplify attribute comparison calculator

--- rust/src/bin/compare_attribute_strength.rs
+++ rust/src/bin/compare_attribute_strength.rs
@@ -1,32 +1,30 @@
 use rise::classes::Character;
 use rise::core_mechanics::{Attribute, HasAttributes};
 
+use rise::monsters::{ChallengeRating, Monster};
 use rise::simulation::combat::run_combat;
 
 fn main() {
     for level in vec![2, 8, 14, 20] {
-        for attribute_pair in vec![
-            [Attribute::Strength, Attribute::Dexterity],
-            [Attribute::Strength, Attribute::Constitution],
-            [Attribute::Dexterity, Attribute::Constitution],
-            [Attribute::Strength, Attribute::Perception],
-            [Attribute::Dexterity, Attribute::Perception],
-            [Attribute::Constitution, Attribute::Perception],
-            [Attribute::Strength, Attribute::Intelligence],
-            [Attribute::Dexterity, Attribute::Intelligence],
-            [Attribute::Constitution, Attribute::Intelligence],
-            [Attribute::Perception, Attribute::Intelligence],
+        for attribute in vec![
+            Attribute::Strength,
+            Attribute::Dexterity,
+            Attribute::Constitution,
+            Attribute::Perception,
         ] {
             let mut blue = vec![Character::standard_character(level, false)];
-            blue[0].set_base_attribute(attribute_pair[0], 4);
-            let mut red = vec![Character::standard_character(level, false)];
-            red[0].set_base_attribute(attribute_pair[1], 4);
+            blue[0].set_base_attribute(attribute, 4);
+            let red = vec![Monster::standard_monster(
+                ChallengeRating::Two,
+                level,
+                Some(0),
+                None,
+            )];
             let results = run_combat(blue, red);
             println!(
-                "L{:>2}, A1{}, A2{}, {}",
+                "L{:>2}, A{}, {}",
                 level,
-                attribute_pair[0].shorthand_name(),
-                attribute_pair[1].shorthand_name(),
+                attribute.shorthand_name(),
                 results
             );
         }
