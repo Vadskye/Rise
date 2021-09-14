4a9807f6d9b9ddb1fe269038e115dd1bfa18a720
Kevin Johnson <vadskye@gmail.com>
2021-09-14 09:17:51 -0700

    add support for enabling global glancing strikes

--- rust/src/classes/archetype_rank_abilities/monk.rs
+++ rust/src/classes/archetype_rank_abilities/monk.rs
@@ -164,7 +164,7 @@ pub fn esoteric_warrior<'a>() -> Vec<RankAbility<'a>> {
                 Whenever you miss by 2 or less with a \glossterm{strike}, the target takes half damage from the strike.
                 This is called a \glossterm{glancing blow}.
             ",
-            modifiers: None,
+            modifiers: Some(vec![Modifier::EnableGlancingStrikes]),
         },
         RankAbility {
             name: "Combat Style Rank (4)",
