2183ad96e46c81c716504fa886fbec1b086fb3a4
Kevin Johnson <vadskye@gmail.com>
2021-09-21 09:28:29 -0700

    replace CR 3 with CR 6

--- rust/src/creatures/monster/tests.rs
+++ rust/src/creatures/monster/tests.rs
@@ -115,64 +115,6 @@ fn standard_monster_statistics_level_1_cr2() {
 }
 
 #[test]
-fn standard_monster_statistics_level_1_cr3() {
-    let creature = Monster::standard_monster(ChallengeRating::Three, 1, None, None).creature;
-
-    // HasAttacks
-    assert_eq!(1, creature.calc_accuracy(), "Accuracy: 1 per",);
-    assert_eq!(
-        2,
-        creature.calc_power(true),
-        "Magical power: 1 scaling + 1 wil",
-    );
-    assert_eq!(
-        4,
-        creature.calc_power(false),
-        "Mundane power: 1 scaling + 3 str",
-    );
-
-    // HasAttributes
-    assert_eq!(
-        vec![6, 2, 2, 2, 2, 2],
-        Attribute::all()
-            .iter()
-            .map(|a| creature.calc_total_attribute(&a))
-            .collect::<Vec<i32>>(),
-        "Attributes",
-    );
-
-    // HasDefenses
-    assert_eq!(
-        7,
-        creature.calc_defense(&Defense::Armor),
-        "Armor: 5 monster + 1 dex + 1 con",
-    );
-    assert_eq!(
-        7,
-        creature.calc_defense(&Defense::Fortitude),
-        "Fort: 5 monster + 2 con",
-    );
-    assert_eq!(
-        7,
-        creature.calc_defense(&Defense::Reflex),
-        "Ref: 5 monster + 2 dex",
-    );
-    assert_eq!(
-        7,
-        creature.calc_defense(&Defense::Mental),
-        "Ment: 5 monster + 2 wil",
-    );
-
-    // HasDamageAbsorption
-    assert_eq!(39, creature.calc_hit_points(), "HP: (11 level + 2 con) * 3",);
-    assert_eq!(
-        20,
-        creature.calc_damage_resistance(),
-        "DR: (3 level + 2 con) * 4",
-    );
-}
-
-#[test]
 fn standard_monster_statistics_level_1_cr4() {
     let creature = Monster::standard_monster(ChallengeRating::Four, 1, None, None).creature;
 
