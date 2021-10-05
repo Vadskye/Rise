403447a9a1d700657687421b7bc70eefbf284d8c
Kevin Johnson <vadskye@gmail.com>
2021-10-05 08:57:06 -0500

    adjust war scaling, fix heavy-only hack

--- rust/src/classes/class.rs
+++ rust/src/classes/class.rs
@@ -1125,11 +1125,13 @@ impl Class {
                         For example, a scrying sensor created by a \abilitytag{Scrying} effect would be unable to detect your presence, and a creature with magical \glossterm{darkvision} would not be able to see you without light.
 
                     \subsubsection{War}
-                        \parhead{Gift} You gain proficiency with heavy armor and an additional \glossterm{weapon group} of your choice.
-                        \parhead{Aspect} You gain a \plus2 bonus to your \glossterm{power} with all abilities.
+                        \parhead{Gift} You gain proficiency an additional \glossterm{weapon group} of your choice.
+                        In addition, you gain proficiency with an additional \glossterm{usage class} of armor.
+                        You must be proficient with light armor to become proficient with medium armor, and you must be proficient with medium armor to become proficient with heavy armor.
+                        \parhead{Aspect} You gain a \plus1d bonus to your damage with all weapons.
                         \parhead{Essence} Whenever you miss by 2 or less with a \glossterm{strike}, the target takes half damage from the strike.
                         This is called a \glossterm{glancing blow}.
-                        \parhead{Mastery} The bonus from this domain's aspect increases to \plus6.
+                        \parhead{Mastery} The bonus from this domain's aspect increases to \plus2d.
 
                     \subsubsection{Water}
                         If you choose this domain, you add the \sphere{aquamancy} \glossterm{mystic sphere} to your list of divine mystic spheres (see \pcref{Mystic Spheres}).
