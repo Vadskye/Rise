1673abd7f8912ccb53b934f431484c37314562c6
Kevin Johnson <vadskye@gmail.com>
2021-10-12 08:15:57 -0500

    add planeforged to rust

--- rust/src/monsters/specific_monsters.rs
+++ rust/src/monsters/specific_monsters.rs
@@ -5,13 +5,30 @@ mod dragons;
 mod humanoids;
 mod magical_beasts;
 mod monstrous_humanoids;
+mod planeforged;
 mod undead;
 
-pub use aberrations::aberrations;
-pub use animals::animals;
-pub use animates::animates;
-pub use dragons::dragons;
-pub use humanoids::humanoids;
-pub use magical_beasts::magical_beasts;
-pub use monstrous_humanoids::monstrous_humanoids;
-pub use undead::undeads;
+use aberrations::aberrations;
+use animals::animals;
+use animates::animates;
+use dragons::dragons;
+use humanoids::humanoids;
+use magical_beasts::magical_beasts;
+use monstrous_humanoids::monstrous_humanoids;
+use planeforged::planeforgeds;
+use undead::undeads;
+use super::monster_entry::MonsterEntry;
+
+pub fn all_specific_monster_functions() -> Vec<fn() -> Vec<MonsterEntry>> {
+    return vec![
+        aberrations,
+        animals,
+        animates,
+        dragons,
+        humanoids,
+        magical_beasts,
+        monstrous_humanoids,
+        planeforgeds,
+        undeads,
+    ];
+}
---
+++ rust/src/monsters/specific_monsters/planeforged.rs
@@ -0,0 +1,79 @@
+use crate::core_mechanics::{DamageType, MovementMode, Sense, Size};
+use crate::creatures::{Modifier, Monster};
+use crate::equipment::{StandardWeapon, Weapon};
+use crate::monsters::challenge_rating::ChallengeRating;
+use crate::monsters::creature_type::CreatureType::Planeforged;
+use crate::monsters::knowledge::Knowledge;
+use crate::monsters::monster_entry::MonsterEntry;
+use crate::monsters::{monster_group, FullMonsterDefinition};
+use crate::skills::Skill;
+
+struct FullPlaneforgedDefinition {
+    alignment: String,
+    attributes: Vec<i32>,
+    challenge_rating: ChallengeRating,
+    description: Option<&'static str>,
+    knowledge: Option<Knowledge>,
+    level: i32,
+    modifiers: Option<Vec<Modifier>>,
+    movement_modes: Option<Vec<MovementMode>>,
+    name: String,
+    senses: Option<Vec<Sense>>,
+    size: Size,
+    trained_skills: Option<Vec<Skill>>,
+    weapons: Vec<Weapon>,
+}
+
+impl FullPlaneforgedDefinition {
+    fn monster(self) -> Monster {
+        return FullMonsterDefinition {
+            // From self
+            alignment: self.alignment,
+            attributes: self.attributes,
+            challenge_rating: self.challenge_rating,
+            description: self.description,
+            knowledge: self.knowledge,
+            level: self.level,
+            modifiers: self.modifiers,
+            movement_modes: self.movement_modes,
+            name: self.name,
+            senses: self.senses,
+            size: self.size,
+            trained_skills: self.trained_skills,
+            weapons: self.weapons,
+
+            // Default values
+            creature_type: Planeforged,
+        }
+        .monster();
+    }
+}
+
+pub fn planeforgeds() -> Vec<MonsterEntry> {
+    let mut monsters: Vec<MonsterEntry> = vec![];
+
+    monsters.push(MonsterEntry::MonsterGroup(monster_group::MonsterGroup {
+        name: "Imps".to_string(),
+        knowledge: None,
+        monsters: vec![FullPlaneforgedDefinition {
+            alignment: "Always chaotic evil".to_string(),
+            attributes: vec![2, 3, 2, 1, 0, -2],
+            challenge_rating: ChallengeRating::Half,
+            description: None,
+            knowledge: None,
+            level: 13,
+            modifiers: None,
+            movement_modes: None,
+            name: "Flamefist Imp".to_string(),
+            senses: None,
+            size: Size::Small,
+            trained_skills: None,
+            weapons: vec![StandardWeapon::Slam
+                .weapon()
+                .except(|w| w.damage_types.push(DamageType::Fire))],
+        }
+        .monster()],
+    }));
+
+    return monsters;
+}
