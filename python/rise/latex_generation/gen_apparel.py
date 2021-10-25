bf49495e10ec6b4832c8877ae0c42f9f045077b4
Kevin Johnson <vadskye@gmail.com>
2021-10-25 09:15:07 -0500

    standardize vital regeneration wording

--- python/rise/latex_generation/gen_apparel.py
+++ python/rise/latex_generation/gen_apparel.py
@@ -1209,7 +1209,7 @@ def generate_apparel():
             material_type="Ring",
             tags=[],
             description="""
-                At the end of each round, if are not \\glossterm<unconscious> due to \\glossterm<fatigue>, you automatically remove one of your \\glossterm<vital wounds>.
+                At the end of each round, if you are not \\glossterm<unconscious> due to \\glossterm<fatigue>, you automatically remove one of your \\glossterm<vital wounds>.
                 You can choose to stop this regeneration if you are conscious, but it happens automatically if you are unconscious due to vital wounds.
                 This cannot remove a vital wound you gained during the current round.
                 When you remove a vital wound in this way, you increase your \\glossterm<fatigue level> by three.
