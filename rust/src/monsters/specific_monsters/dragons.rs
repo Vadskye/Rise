4d90719b7c5c2b4997e3c206f75cb9da9f2eb3f8
Kevin Johnson <vadskye@gmail.com>
2021-11-12 08:49:18 -0600

    remove typescript dragons

--- rust/src/monsters/specific_monsters/dragons.rs
+++ rust/src/monsters/specific_monsters/dragons.rs
@@ -562,6 +562,54 @@ fn dragon(dragon_type: &DragonType, age_category: &AgeCategory) -> Monster {
 pub fn dragons() -> Vec<MonsterEntry> {
     let mut monsters: Vec<MonsterEntry> = vec![];
 
+    monsters.push(MonsterEntry::MonsterGroup(MonsterGroup {
+        knowledge: Some(Knowledge::new(vec![
+            (0, "
+              Legends speak of reptilian flying creatures called dragons.
+              Their love of gold and gems is as legendary as their awe-inspiring power.
+              Dragons keep their wealth in massive hoards, and the search for these hoards has been the death of many a greedy adventurer.
+            "),
+            (5, "
+              Dragons are inherently magical creatures, and they enjoy powerful magic items almost as much as they enjoy gold.
+              As dragons age, they grow continually in power and size.
+              All dragons have damaging breath weapons, and the size and shape of the breath depends on the type and age of the dragon.
+              They also have extremely keen senses, and are very difficult to sneak up on.
+            "),
+            (10, "
+              There are two types of dragons: metallic dragons and chromatic dragons.
+              Metallic dragons have shiny, glistening scales, and all metallic dragons are named after metals.
+              Chromatic dragons have intensely colored scales, and all chromatic dragons are named after colors.
+              Metallic dragons tend to be good-aligned, and chromatic dragons tend to be evil-aligned.
+
+              Dragon bones and scales retain some of the magical power of their original owner.
+              They can be used to craft powerful weapons and armor, and can be quite valuable to the right buyer.
+            "),
+            (15, "
+              In combat, dragons take full advantage of their myriad attack options.
+              They fight at whatever range they consider optimal.
+              In general, they are most dangerous in melee, but they may choose to remain at a distance to avoid powerful melee opponents.
+              In that case, they use their their spells and breath weapon to pick off opponents that cannot fight effectively at range.
+
+              Dragons can fly extremely quickly, and they can use this ability to escape a losing fight or to pick off isolated creatures trying to keep their distance.
+              They generally avoid grappling foes, possibly because they find it demeaning, but large dragons may swallow smaller opponents whole.
+            "),
+            (20, "
+              Newly hatched dragons are a few feet long, while the oldest dragons are among the most massive and dangerous creatures in existence.
+              Although ancient dragons are immensely powerful, they are also rarely active, requiring weeks or months of sleep between days of activity.
+              Eventually, it is said that the most ancient dragons simply go to sleep and may never wake up, though they live indefinitely in that state.
+            "),
+            (25, "
+              There is a practical side to the famous greed of dragons.
+              Dragons can metabolize gold and magical energy from items they eat to fuel their immense power and bulk.
+              In desperate times, a dragon may be forced to eat part of its hoard to accelerate its recovery from injuries or increase its power.
+              As dragons approach the inevitable torpor of their old age, they can stave it off or recover from a long rest by consuming part of their hoard.
+              This is a difficult decision for a dragon to make, and most dragons never eat a single gold piece.
+            "),
+        ])),
+        name: "Dragons".to_string(),
+        monsters: vec![],
+    }));
+
     for dragon_type in DragonType::all() {
         let dragons = AgeCategory::all()
             .iter()
--- typescript/src/monsters/types/types.ts
+++ typescript/src/monsters/types/types.ts
@@ -2,7 +2,6 @@ import { defenseBonusesByMonsterType } from "@src/calculate/defense_bonuses_by_m
 import { MonsterType } from "@src/data";
 import * as format from "@src/latex/format";
 import { MonsterInput } from "@src/monsters/reformat_monster_input";
-import { dragons } from "./dragons";
 import { humanoids } from "./humanoids";
 import { magicalBeasts } from "./magical_beasts";
 import { monstrousHumanoids } from "./monstrous_humanoids";
@@ -10,7 +9,6 @@ import { planeforged } from "./planeforged";
 import { undead } from "./undead";
 
 export const monsterInputsByType: Record<MonsterType, MonsterInput[]> = {
-  "dragon": dragons,
   "humanoid": humanoids,
   "magical beast": magicalBeasts,
   "monstrous humanoid": monstrousHumanoids,
@@ -31,7 +29,6 @@ function formatDefenseBonuses(monsterType: MonsterType) {
 
 // Individual monsters may have different knowledge skills
 export const knowledgeSkillsByMonsterType: Record<MonsterType, string[]> = {
-  "dragon": ["arcana"],
   "humanoid": ["local"],
   "magical beast": ["arcana"],
   "monstrous humanoid": ["nature"],
@@ -40,49 +37,6 @@ export const knowledgeSkillsByMonsterType: Record<MonsterType, string[]> = {
 };
 
 export const typeDescriptions: Record<MonsterType, string | null> = {
-  "dragon": `
-    ${formatDefenseBonuses("dragon")}
-    \\parhead{Knowledge (arcana) 0}
-      Legends speak of reptilian flying creatures called dragons.
-      Their love of gold and gems is as legendary as their awe-inspiring power.
-      Dragons keep their wealth in massive hoards, and the search for these hoards has been the death of many a greedy adventurer.
-
-    \\parhead{Knowledge (arcana) 5}
-      Dragons are inherently magical creatures, and they enjoy powerful magic items almost as much as they enjoy gold.
-      As dragons age, they grow continually in power and size.
-      All dragons have damaging breath weapons, and the size and shape of the breath depends on the type and age of the dragon.
-      They also have extremely keen senses, and are very difficult to sneak up on.
-
-    \\parhead{Knowledge (arcana) 10}
-      There are two types of dragons: metallic dragons and chromatic dragons.
-      Metallic dragons have shiny, glistening scales, and all metallic dragons are named after metals.
-      Chromatic dragons have intensely colored scales, and all chromatic dragons are named after colors.
-      Metallic dragons tend to be good-aligned, and chromatic dragons tend to be evil-aligned.
-
-      Dragon bones and scales retain some of the magical power of their original owner.
-      They can be used to craft powerful weapons and armor, and can be quite valuable to the right buyer.
-
-    \\parhead{Knowledge (arcana) 15}
-      In combat, dragons take full advantage of their myriad attack options.
-      They fight at whatever range they consider optimal.
-      In general, they are most dangerous in melee, but they may choose to remain at a distance to avoid powerful melee opponents.
-      In that case, they use their their spells and breath weapon to pick off opponents that cannot fight effectively at range.
-
-      Dragons can fly extremely quickly, and they can use this ability to escape a losing fight or to pick off isolated creatures trying to keep their distance.
-      They generally avoid grappling foes, possibly because they find it demeaning, but large dragons may swallow smaller opponents whole.
-
-    \\parhead{Knowledge (arcana) 20}
-      Newly hatched dragons are a few feet long, while the oldest dragons are among the most massive and dangerous creatures in existence.
-      Although ancient dragons are immensely powerful, they are also rarely active, requiring weeks or months of sleep between days of activity.
-      Eventually, it is said that the most ancient dragons simply go to sleep and may never wake up, though they live indefinitely in that state.
-
-    \\parhead{Knowledge (arcana) 25}
-      There is a practical side to the famous greed of dragons.
-      Dragons can metabolize gold and magical energy from items they eat extremely efficiently to fuel their immense power and bulk.
-      In desperate times, a dragon may be forced to eat part of its hoard to accelerate its recovery from injuries or increase its power.
-      As dragons approach the inevitable torpor of their old age, they can stave it off or recover from a long rest by consuming part of their hoard.
-      This is a difficult decision for a dragon to make, and most dragons never eat a single gold piece.
-  `,
   "humanoid": `
     ${formatDefenseBonuses("humanoid")}
   `,
