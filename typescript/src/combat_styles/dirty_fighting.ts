5479d38577059a3d4c437b14ba1674a0923ce636
Kevin Johnson <vadskye@gmail.com>
2021-12-11 09:54:55 -0600

    add missing backslash

--- typescript/src/combat_styles/dirty_fighting.ts
+++ typescript/src/combat_styles/dirty_fighting.ts
@@ -94,7 +94,7 @@ export const dirtyFighting: CombatStyle = {
         abilityType: "ability",
         exceptThat: `
           if the subject has no remaining \\glossterm{damage resistance}, the subject is pinned completely instead of simply grappled.
-          It is \\glossterm{helpless}, and the only physical ability it can use is the \\textit{escape grapple} ability (see \pcref{Escape Grapple}).
+          It is \\glossterm{helpless}, and the only physical ability it can use is the \\textit{escape grapple} ability (see \\pcref{Escape Grapple}).
           Any accuracy bonuses you have that apply specifically to the \\textit{grapple} ability also apply to this ability.
         `,
         name: "grapple",
