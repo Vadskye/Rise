06a50862068a9de83507756e2825e15a88140e35
Kevin Johnson <vadskye@gmail.com>
2021-12-22 17:07:03 -0600

    expand debug

--- rust/src/bin/debug.rs
+++ rust/src/bin/debug.rs
@@ -3,10 +3,7 @@ use rise::{
     monsters::ChallengeRating,
 };
 
-fn main() {
-    let standard_monster = Monster::standard_monster(ChallengeRating::Two, 10, None, None);
-    println!("standard monster {}", standard_monster.to_section(None));
-
+fn compare_classes() {
     let standard_character = Character::standard_character(1, true);
     println!("standard character {}", standard_character.description());
     let standard_sorcerer = Character::standard_sorcerer(1, true);
@@ -23,4 +20,21 @@ fn main() {
     println!("standard barbarian {}", standard_barbarian.description());
     let standard_sorcerer = Character::standard_sorcerer(20, true);
     println!("standard sorcerer {}", standard_sorcerer.description());
+}
+
+fn level_scaling() {
+    println!("Level 2: {}", Character::standard_character(2, true).description());
+    println!("Level 5: {}", Character::standard_character(5, true).description());
+    println!("Level 8: {}", Character::standard_character(8, true).description());
+    println!("Level 14: {}", Character::standard_character(14, true).description());
+    println!("Level 20: {}", Character::standard_character(20, true).description());
+}
+
+fn main() {
+    let standard_monster = Monster::standard_monster(ChallengeRating::Two, 10, None, None);
+    println!("standard monster {}", standard_monster.to_section(None));
+
+
+    // compare_classes();
+    level_scaling();
 }
