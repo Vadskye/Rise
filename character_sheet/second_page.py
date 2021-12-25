9937ecd210b9ef3c3da003293afa61d2a0388981
Kevin Johnson <vadskye@gmail.com>
2021-12-25 10:56:02 -0600

    update sheet for half wil fatigue

--- character_sheet/second_page.py
+++ character_sheet/second_page.py
@@ -314,7 +314,7 @@ def calc_fatigue_tolerance():
                     ),
                     plus(),
                     underlabel(
-                        "(Con+Wil)",
+                        "(Attr)",
                         number_input(
                             {
                                 "disabled": True,
