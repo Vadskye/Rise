8f1afa696b23257f31d47d4bb18acab9e698c9c4
Kevin Johnson <vadskye@gmail.com>
2021-12-28 08:38:44 -0600

    clarify mystic insight wording, remove spell knowledge insight

--- rust/src/classes/archetype_rank_abilities/sorcerer.rs
+++ rust/src/classes/archetype_rank_abilities/sorcerer.rs
@@ -317,14 +317,12 @@ pub fn arcane_spell_mastery<'a>() -> Vec<RankAbility<'a>> {
             description: r"
                 You gain your choice of one of the following abilities.
                 You can also spend \glossterm{insight points} to learn one additional \textit{mystic insight} per insight point.
-                You cannot apply the benefits of more than two \textit{mystic insight} abilities to the same spell.
+                You cannot choose the same spell with more than two \textit{mystic insight} abilities.
                 {
                     \parhead{Distant Spell} Choose an arcane \glossterm{spell} you know with a standard \glossterm{range}: Short, Medium, Long, Distant, or Extreme.
                         You increase that spell's range to the next standard range category, to a maximum of Extreme range.
                         You can choose this ability multiple times, choosing a different spell each time.
                         Whenever you learn a new spell, you may change which of your spells this ability affects.
-                    \parhead{Spell Knowledge} You learn an additional arcane \glossterm{spell} from a \glossterm{mystic sphere} you have access to.
-                        You can choose this ability multiple times, learning an additional spell each time.
                     \parhead{Precise Spell} Choose an arcane \glossterm{spell} you know.
                         You gain a \plus1 bonus to \glossterm{accuracy} with that spell.
                         You can choose this ability multiple times, choosing a different spell each time.
