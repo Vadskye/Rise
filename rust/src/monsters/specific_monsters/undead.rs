9c858c6993083437984da694bec2ade0aa6246bd
Kevin Johnson <vadskye@gmail.com>
2022-01-03 11:21:28 -0600

    add zombies

--- rust/src/monsters/specific_monsters/undead.rs
+++ rust/src/monsters/specific_monsters/undead.rs
@@ -1,4 +1,7 @@
-use crate::core_mechanics::{MovementMode, Sense, Size, StandardPassiveAbility};
+use crate::core_mechanics::{
+    DamageType, MovementMode, Sense, Size, SpecialDefenseModifier, SpecialDefenseType,
+    StandardPassiveAbility,
+};
 use crate::creatures::{Modifier, Monster};
 use crate::equipment::{StandardWeapon, Weapon};
 use crate::monsters::challenge_rating::ChallengeRating;
@@ -54,24 +57,118 @@ fn undead(def: FullUndeadDefinition) -> Monster {
 pub fn undeads() -> Vec<MonsterEntry> {
     let mut monsters: Vec<MonsterEntry> = vec![];
 
+    let mindless = Modifier::PassiveAbility(StandardPassiveAbility::Mindless.ability());
+
+    let skeleton_vulnerability = Modifier::SpecialDefense(SpecialDefenseModifier::Vulnerable(
+        SpecialDefenseType::Damage(DamageType::Bludgeoning),
+    ));
+
     monsters.push(MonsterEntry::MonsterGroup(monster_group::MonsterGroup {
         name: "Skeletons".to_string(),
         knowledge: None,
-        monsters: vec![undead(FullUndeadDefinition {
-            alignment: "Always true neutral".to_string(),
-            attributes: vec![2, 2, 0, 0, 0, -1],
-            challenge_rating: ChallengeRating::One,
-            description: None,
-            knowledge: None,
-            level: 1,
-            modifiers: None,
-            movement_modes: None,
-            name: "Skeleton Guard".to_string(),
-            senses: None,
-            size: Size::Medium,
-            trained_skills: None,
-            weapons: vec![StandardWeapon::Scimitar.weapon()],
-        })],
+        monsters: vec![
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![2, 3, 1, 0, 0, -1],
+                challenge_rating: ChallengeRating::One,
+                description: None,
+                knowledge: None,
+                level: 1,
+                modifiers: Some(vec![mindless.clone(), skeleton_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Skeleton Guard".to_string(),
+                senses: None,
+                size: Size::Medium,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Scimitar.weapon()],
+            }),
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![2, 3, 0, 0, 3, -1],
+                challenge_rating: ChallengeRating::One,
+                description: None,
+                knowledge: None,
+                level: 2,
+                modifiers: Some(vec![mindless.clone(), skeleton_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Skeleton Archer".to_string(),
+                senses: None,
+                size: Size::Medium,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Longbow.weapon()],
+            }),
+        ],
+    }));
+
+    let zombie_vulnerability = Modifier::SpecialDefense(SpecialDefenseModifier::Vulnerable(
+        SpecialDefenseType::Damage(DamageType::Slashing),
+    ));
+
+    monsters.push(MonsterEntry::MonsterGroup(monster_group::MonsterGroup {
+        name: "Zombies".to_string(),
+        knowledge: None,
+        monsters: vec![
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![3, -2, 3, 0, 0, -2],
+                challenge_rating: ChallengeRating::Half,
+                description: None,
+                knowledge: None,
+                level: 1,
+                modifiers: Some(vec![mindless.clone(), zombie_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Zombie Shambler".to_string(),
+                senses: None,
+                size: Size::Medium,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Slam.weapon()],
+            }),
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![4, -2, 4, 0, 0, -2],
+                challenge_rating: ChallengeRating::One,
+                description: None,
+                knowledge: None,
+                level: 1,
+                modifiers: Some(vec![mindless.clone(), zombie_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Zombie Walker".to_string(),
+                senses: None,
+                size: Size::Medium,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Slam.weapon()],
+            }),
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![5, -2, 5, 0, 0, -2],
+                challenge_rating: ChallengeRating::Two,
+                description: None,
+                knowledge: None,
+                level: 2,
+                modifiers: Some(vec![mindless.clone(), zombie_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Zombie Brute".to_string(),
+                senses: None,
+                size: Size::Medium,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Slam.weapon()],
+            }),
+            undead(FullUndeadDefinition {
+                alignment: "Always neutral evil".to_string(),
+                attributes: vec![6, -2, 6, 0, 0, -2],
+                challenge_rating: ChallengeRating::Four,
+                description: None,
+                knowledge: None,
+                level: 3,
+                modifiers: Some(vec![mindless.clone(), zombie_vulnerability.clone()]),
+                movement_modes: None,
+                name: "Zombie Hulk".to_string(),
+                senses: None,
+                size: Size::Large,
+                trained_skills: None,
+                weapons: vec![StandardWeapon::Slam.weapon()],
+            }),
+        ],
     }));
 
     return monsters;
