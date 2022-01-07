01b159147adac93003ea989e5df20af8b74f7ef5
Kevin Johnson <vadskye@gmail.com>
2022-01-07 14:24:14 -0600

    more js sheet worker

--- character_sheet/sheet_worker.js
+++ character_sheet/sheet_worker.js
@@ -60,6 +60,38 @@ function onGet(variables, options, callback = null) {
   });
 }
 
+const SKILLS_BY_ATTRIBUTE = {
+  strength: ["Climb", "Jump", "Swim"],
+  dexterity: ["Balance", "Flexibility", "Ride", "Sleight of Hand", "Stealth"],
+  constitution: ["Endurance"],
+  intelligence: [
+    "Craft1",
+    "Craft2",
+    "Deduction",
+    "Devices",
+    "Disguise",
+    "Knowledge1",
+    "Knowledge2",
+    "Linguistics",
+    "Medicine",
+  ],
+  perception: [
+    "Awareness",
+    "Creature Handling",
+    "Social Insight",
+    "Survival",
+  ],
+  willpower: [],
+  other: [
+    "Deception",
+    "Intimidate",
+    "Perform1",
+    "Perform2",
+    "Persuasion",
+    "Profession",
+  ],
+};
+
 const VARIABLES_WITH_CUSTOM_MODIFIERS = new Set([
   "accuracy",
   "all_defenses",
@@ -128,6 +160,12 @@ function handleEverything() {
 function handleCoreStatistics() {
   handleAccuracy();
   handleDefenses();
+  handleDamageResistance();
+  handleEncumbrance();
+  handleFatiguePenalty();
+  handleHitPoints();
+  handleInitiative();
+  handleLandSpeed();
   handlePower();
   handleUnknownStatistic();
 }
@@ -141,6 +179,23 @@ function handleDefenses() {
 
 function handleResources() {
   handleAttunementPoints();
+  handleFatigueTolerance();
+  handleInsightPoints();
+  handleSkillPoints();
+  handleSkillPointsSpent();
+}
+
+function calcDefenseLevelScaling(level, challengeRating) {
+  let levelScaling = Math.floor(level / 2);
+  if (challengeRating > 0) {
+    levelScaling += Math.max(0, Math.floor((level + 6) / 9));
+  }
+  if (challengeRating === 4) {
+    levelScaling += 1;
+  } else if (challengeRating === 6) {
+    levelScaling += 2;
+  }
+  return levelScaling;
 }
 
 function handleAccuracy() {
@@ -163,19 +218,6 @@ function handleAccuracy() {
       setAttrs({ accuracy });
     }
   );
-}
-
-function calcDefenseLevelScaling(level, challengeRating) {
-  let levelScaling = Math.floor(level / 2);
-  if (challengeRating > 0) {
-    levelScaling += Math.max(0, Math.floor((level + 6) / 9));
-  }
-  if (challengeRating === 4) {
-    levelScaling += 1;
-  } else if (challengeRating === 6) {
-    levelScaling += 2;
-  }
-  return levelScaling;
 }
 
 function handleArmorDefense() {
@@ -284,6 +326,273 @@ function handleAttunementPoints() {
   );
 }
 
+function handleDamageResistance() {
+  onGet(
+    {
+      miscName: "damage_resistance_bonus",
+      miscCount: 4,
+      numeric: [
+        "constitution",
+        "level",
+        "challenge_rating",
+        "damage_resistance_bonus_armor",
+        "damage_resistance_bonus_vital_wound_multiplier",
+      ],
+      numericWithoutListen: ["damage_resistance", "damage_resistance_maximum"],
+    },
+    (v) => {
+      var fromLevel = 0;
+      var levelish = v.level + v.constitution;
+      if (levelish > 0) {
+        var multiplier = 1;
+        while (levelish > 21) {
+          levelish -= 6;
+          multiplier += 1;
+        }
+        fromLevel =
+          {
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 7,
+            8: 9,
+            9: 10,
+            10: 12,
+            11: 13,
+            12: 15,
+            13: 16,
+            14: 18,
+            15: 20,
+            16: 22,
+            17: 25,
+            18: 28,
+            19: 32,
+            20: 36,
+            21: 40,
+          }[levelish] * multiplier;
+      }
+      var crMultiplier = {
+        0: 1,
+        0.5: 0,
+        1: 2,
+        2: 4,
+        4: 8,
+        6: 16,
+      }[v.challenge_rating || 0];
+      const totalValue = Math.floor(
+        (fromLevel + v.damage_resistance_bonus_armor + v.misc) *
+          crMultiplier *
+          (v.damage_resistance_bonus_vital_wound_multiplier || 1)
+      );
+
+      let attrs = {
+        damage_resistance_from_level: fromLevel * crMultiplier,
+        damage_resistance_max: totalValue,
+        damage_resistance_maximum: totalValue,
+      };
+      let should_set_current_dr =
+        totalValue < v.damage_resistance ||
+        v.damage_resistance === v.damage_resistance_maximum ||
+        !v.damage_resistance_maximum;
+      if (should_set_current_dr) {
+        attrs.damage_resistance = totalValue;
+      }
+      setAttrs(attrs);
+    }
+  );
+}
+
+function handleEncumbrance() {
+  onGet(
+    {
+      miscName: "encumbrance",
+      miscCount: 2,
+      numeric: ["level", "body_armor_encumbrance", "strength"],
+    },
+    (v) => {
+      const totalValue = Math.max(
+        0,
+        v.body_armor_encumbrance - Math.max(0, v.strength) - v.misc
+      );
+      setAttrs({ encumbrance: totalValue });
+    }
+  );
+}
+
+function handleFatiguePenalty() {
+  onGet(
+    {
+      numeric: ["fatigue_points", "fatigue_tolerance"],
+    },
+    (v) => {
+      const totalValue = Math.max(0, v.fatigue_points - v.fatigue_tolerance);
+      setAttrs({
+        fatigue_penalty: totalValue,
+      });
+    }
+  );
+}
+
+function handleFatigueTolerance() {
+  onGet(
+    {
+      miscName: "fatigue_tolerance",
+      miscCount: 2,
+      numeric: ["level", "fatigue_tolerance_base", "constitution", "willpower"],
+    },
+    (v) => {
+      const fromAttributes = v.constitution + Math.floor(v.willpower / 2);
+      const totalValue = Math.max(
+        0,
+        v.fatigue_tolerance_base + fromAttributes + v.misc
+      );
+      setAttrs({
+        fatigue_tolerance_attributes: fromAttributes,
+        fatigue_tolerance: totalValue,
+        // for red bars
+        fatigue_points_max: totalValue,
+      });
+    }
+  );
+}
+
+function handleHitPoints() {
+  onGet(
+    {
+      miscName: "hit_points",
+      miscCount: 4,
+      numeric: [
+        "level",
+        "constitution",
+        "challenge_rating",
+        "hit_points_vital_wound_multiplier",
+      ],
+      numericWithoutListen: ["hit_points", "hit_points_maximum"],
+    },
+    (v) => {
+      let levelish = v.level + v.constitution;
+      let hpFromLevel = 9 + levelish;
+      if (levelish > 0) {
+        var multiplier = 1;
+        while (levelish > 21) {
+          levelish -= 6;
+          multiplier += 1;
+        }
+        hpFromLevel =
+          multiplier *
+            {
+              1: 10,
+              2: 11,
+              3: 12,
+              4: 13,
+              5: 14,
+              6: 16,
+              7: 18,
+              8: 20,
+              9: 22,
+              10: 25,
+              11: 28,
+              12: 32,
+              13: 36,
+              14: 40,
+              15: 44,
+              16: 50,
+              17: 56,
+              18: 64,
+              19: 72,
+              20: 80,
+              21: 88,
+              22: 100,
+            }[levelish] || 1;
+      }
+
+      let crMultiplier = {
+        0: 1,
+        0.5: 1,
+        1: 1,
+        2: 3,
+        4: 4,
+        6: 6,
+      }[v.challenge_rating || 0];
+
+      const totalValue = Math.floor(
+        (hpFromLevel + v.misc) *
+          crMultiplier *
+          (v.hit_points_vital_wound_multiplier || 1)
+      );
+
+      let attrs = {
+        hit_points_from_level: hpFromLevel * crMultiplier,
+        hit_points_max: totalValue,
+        hit_points_maximum: totalValue,
+      };
+      let shouldSetCurrentHp =
+        totalValue < v.hit_points ||
+        totalValue === v.hit_points_maximum ||
+        !v.hit_points_maximum;
+      if (shouldSetCurrentHp) {
+        attrs.hit_points = totalValue;
+      }
+      setAttrs(attrs);
+    }
+  );
+}
+
+function handleInitiative() {
+  onGet(
+    {
+      miscName: "initiative",
+      miscCount: 3,
+      numeric: ["dexterity", "perception", "fatigue_penalty"],
+    },
+    (v) => {
+      const attributeModifier = v.dexterity + v.perception;
+      setAttrs({
+        initiative: attributeModifier + v.misc - v.fatigue_penalty,
+        initiative_scaling: attributeModifier,
+      });
+    }
+  );
+}
+
+function handleInsightPoints() {
+  onGet(
+    {
+      miscName: "insight_points",
+      miscCount: 3,
+      numeric: ["insight_points_base", "intelligence"],
+    },
+    (v) => {
+      const totalValue = Math.max(
+        0,
+        v.insight_points_base + v.intelligence + v.misc
+      );
+      setAttrs({
+        insight_points: totalValue,
+      });
+    }
+  );
+}
+
+function handleLandSpeed() {
+  onGet(
+    {
+      miscName: "speed",
+      miscCount: 2,
+      numeric: ["level", "speed_size", "speed_armor"],
+    },
+    (v) => {
+      const totalValue = v.speed_size - v.speed_armor + v.misc;
+      setAttrs({
+        land_speed: totalValue,
+      });
+    }
+  );
+}
+
 function handleNonArmorDefense(defense, attribute) {
   onGet(
     {
@@ -352,41 +661,55 @@ function handlePower() {
   );
 }
 
-function handleSkills() {
-  const SKILLS_BY_ATTRIBUTE = {
-    strength: ["Climb", "Jump", "Swim"],
-    dexterity: ["Balance", "Flexibility", "Ride", "Sleight of Hand", "Stealth"],
-    constitution: ["Endurance"],
-    intelligence: [
-      "Craft1",
-      "Craft2",
-      "Deduction",
-      "Devices",
-      "Disguise",
-      "Knowledge1",
-      "Knowledge2",
-      "Linguistics",
-      "Medicine",
-    ],
-    perception: [
-      "Awareness",
-      "Creature Handling",
-      "Social Insight",
-      "Survival",
-    ],
-    willpower: [],
-    other: [
-      "Deception",
-      "Intimidate",
-      "Perform1",
-      "Perform2",
-      "Persuasion",
-      "Profession",
-    ],
-  };
+function handleSkillPoints() {
+  onGet(
+    {
+      miscName: "skill_points",
+      miscCount: 3,
+      numeric: ["level", "skill_points_base", "intelligence"],
+    },
+    (v) => {
+      const fromInt = Math.max(0, v.intelligence);
+      const totalValue = v.skill_points_base + fromInt + v.misc;
+      setAttrs({
+        skill_points: totalValue,
+        skill_points_intelligence: fromInt,
+      });
+    }
+  );
+}
 
+function handleSkillPointsSpent() {
+  const skills = [];
+  for (const attributeSkills of Object.values(SKILLS_BY_ATTRIBUTE)) {
+    for (const skill of attributeSkills) {
+      skills.push(skill);
+    }
+  }
+  const skillsAreTrained = skills.map((s) => s.toLowerCase().replace(' ', '_') + '_is_trained');
+
+  onGet(
+    {
+      boolean: skillsAreTrained,
+    },
+    (v) => {
+      let skillPointsSpent = 0;
+      for (const skillIsTrained of skillsAreTrained) {
+        if (v[skillIsTrained]) {
+          skillPointsSpent += 1;
+        }
+      }
+      setAttrs({
+        skill_points_spent: skillPointsSpent,
+      });
+    }
+  );
+}
+
+function handleSkills() {
   for (const attribute of Object.keys(SKILLS_BY_ATTRIBUTE)) {
-    for (const skill of SKILLS_BY_ATTRIBUTE[attribute]) {
+    for (let skill of SKILLS_BY_ATTRIBUTE[attribute]) {
+      skill = skill.toLowerCase().replace(' ', '_');
       const numeric = [
         "all_skills_custom_modifier",
         "fatigue_penalty",
