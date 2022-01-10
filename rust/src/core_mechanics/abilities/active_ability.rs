5ddfecd20582849db8f548c164ad10ace38a4257
Kevin Johnson <vadskye@gmail.com>
2022-01-10 14:20:53 -0600

    more fully define active abilities

--- rust/src/core_mechanics/abilities/active_ability.rs
+++ rust/src/core_mechanics/abilities/active_ability.rs
@@ -1,16 +1,25 @@
-use crate::creatures::Creature;
-use crate::core_mechanics::abilities::Cooldown;
+use super::latex::latex_ability_block;
+use crate::core_mechanics::abilities::{AbilityTag, AbilityType, Cooldown, UsageTime};
 
 pub struct ActiveAbility {
+    pub ability_type: AbilityType,
     pub cooldown: Option<Cooldown>,
     pub effect: String,
     pub is_magical: bool,
     pub name: String,
+    pub tags: Option<Vec<AbilityTag>>,
+    pub usage_time: Option<UsageTime>,
 }
 
 // LaTeX generation functions
 impl ActiveAbility {
-    pub fn latex_ability_block(&self, creature: &Creature) -> String {
-        return "TODO".to_string();
+    pub fn latex_ability_block(self) -> String {
+        return latex_ability_block(
+            self.ability_type,
+            self.effect,
+            self.name,
+            self.tags,
+            self.usage_time,
+        );
     }
 }
