485f0bebfa58ba26dad0a20cdbe23b878989e986
Kevin Johnson <vadskye@gmail.com>
2022-01-18 11:53:30 -0600

    fully remove typescript planeforged

--- typescript/src/monsters/types/planeforged.ts
+++
@@ -1,5 +0,0 @@
-import { addType, TypelessMonsterInput } from "./add_type";
-
-export const planeforgedInput: TypelessMonsterInput[] = [];
-
-export const planeforged = addType("planeforged", planeforgedInput);
