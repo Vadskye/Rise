d1d1031a49a889525a4eef0a49539a7604b78839
Kevin Johnson <vadskye@gmail.com>
2022-01-29 20:53:06 -0600

    fix second wind link

--- typescript/src/combat_styles/unbreakable_defense.ts
+++ typescript/src/combat_styles/unbreakable_defense.ts
@@ -133,7 +133,7 @@ export const unbreakableDefense: CombatStyle = {
         When you use this ability, you increase your \\glossterm{fatigue level} by two.
 
         You regain hit points equal to half your maximum \\glossterm{hit points}.
-        If you take damage in the same phase that you use this ability, the healing and damage offset, which can prevent you from gaining vital wounds from dropping below 0 hit points (see \\pcref{Regaining Hit Points and Resistances}).
+        If you take damage in the same phase that you use this ability, the healing and damage offset, which can prevent you from gaining vital wounds from dropping below 0 hit points (see \\pcref{Resolving Simultaneous Damage}).
 
         After you use this ability, you cannot use it again until you take a \\glossterm{short rest}.
       `,
