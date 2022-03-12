065a166a59a92f94bfe02063d83b64c399609e20
Kevin Johnson <vadskye@gmail.com>
2022-03-12 15:00:24 -0600

    buff unicorn healing

--- typescript/src/mystic_spheres/summoning.ts
+++ typescript/src/mystic_spheres/summoning.ts
@@ -243,7 +243,7 @@ export const summoning: MysticSphere = {
         exceptThat: `
         the summoned creature appears to be a unicorn.
         Its attacks deal 2d6 piercing damage, and you can command it to heal instead of attack.
-        If you do, during each \\glossterm{action phase} it causes one of your \\glossterm{allies} within \\shortrange of it to regain 2d6 \\glossterm{hit points}.
+        If you do, during each \\glossterm{action phase} it causes one of your \\glossterm{allies} within \\shortrange of it to regain 2d8 \\glossterm{hit points}.
         This healing cannot increase the target's hit points above half its maximum hit points.
         You can tell it which creature to heal.
         If you do not instruct it to heal a specific creature, it will automatically heal the ally closest to it that appears to have lost the most hit points.
