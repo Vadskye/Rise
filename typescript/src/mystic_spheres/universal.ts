21018cff1c2804b9104b664c5dfbc0b57d1808c0
Kevin Johnson <vadskye@gmail.com>
2022-04-06 20:25:50 -0500

    universal spells also have different targets

--- typescript/src/mystic_spheres/universal.ts
+++ typescript/src/mystic_spheres/universal.ts
@@ -12,7 +12,7 @@ export const universal: MysticSphere = {
     Whenever you learn a spell or ritual from the universal mystic sphere, you must choose a different mystic sphere that you have access to.
     Treat that spell or ritual as if it belonged to your chosen mystic sphere instead of this one.
     This allows you to gain the benefits of any sphere-specific effects when you use the spell or ritual, such as a wizard's \\textit{specialization} ability (see \\pcref{Sphere Specialization}).
-    In addition, the chosen mystic sphere determines the damage type dealt by that spell, as listed in \\tref{Universal Mystic Spheres}.
+    In addition, the chosen mystic sphere determines the spell's damage type and which targets are valid, as listed in \\tref{Universal Mystic Spheres}.
     If the spell is \\spell{mystic bolt}, your chosen mystic sphere also determines the spell's defense.
     You can learn the same spell from this mystic sphere any number of times, choosing different base mystic spheres for that spell each time.
 
@@ -20,35 +20,34 @@ export const universal: MysticSphere = {
     As with all other spells, you should feel free to rename them for your own character to make them more interesting and relevant for you.
     For example, a pyromancer might rename their \\spell{mystic bolt} spell to something more fire-themed, like \\textit{firebolt} or \\textit{flamelance}.
 
-    \\begin{dtable}
+    \\begin{dtable!*}
       \\lcaption{Universal Mystic Spheres}
-      \\begin{dtabularx}{\\columnwidth}{l >{\\lcol}X >{\\lcol}X}
-        \\tb{Mystic Sphere} & \\tb{Mystic Bolt Defense} & \\tb{Damage Type} \\tableheaderrule
-        Aeromancy       & Armor     & Bludgeoning \\\\
-        Aquamancy       & Armor     & Bludgeoning \\\\
-        Astromancy      & Mental    & Energy      \\\\
-        Barrier         & Armor     & Physical    \\\\
-        Bless           & Mental    & Energy and \\glossterm{subdual} \\\\
-        Channel Divinity  & Mental    & Energy    \\\\
-        Chronomancy     & Fortitude & Energy      \\\\
-        Cryomancy       & Fortitude & Cold        \\\\
-        Electromancy    & Fortitude & Electricity \\\\
-        Enchantment     & Mental    & Energy and \\glossterm{subdual} \\\\
-        Fabrication     & Armor     & Physical    \\\\
-        Photomancy      & Fortitude & Energy      \\\\
-        Polymorph       & Fortitude & Physical    \\\\
-        Pyromancy       & Armor     & Fire        \\\\
-        Revelation      & Mental    & Energy and \\glossterm{subdual} \\\\
-        Summoning       & Armor     & Physical    \\\\
-        Telekinesis     & Armor     & Physical    \\\\
-        Terramancy      & Armor     & Bludgeoning \\\\
-        Thaumaturgy     & Fortitude & Energy      \\\\
-        Toxicology      & Fortitude & Acid        \\\\
-        Umbramancy      & Fortitude & Cold        \\\\
-        Verdamancy      & Armor     & Physical    \\\\
-        Vivimancy       & Fortitude & Energy      \\\\
+      \\begin{dtabularx}{\\textwidth}{l >{\\lcol}X >{\\lcol}X >{\\lcol}X}
+        \\tb{Mystic Sphere} & \\tb{Affected} & \\tb{Mystic Bolt Defense} & \\tb{Damage Type} \\tableheaderrule
+        Aeromancy       & Creatures and objects & Armor     & Bludgeoning \\\\
+        Aquamancy       & Creatures and objects & Armor     & Bludgeoning \\\\
+        Astromancy      & Creatures and objects & Mental    & Energy      \\\\
+        Bless           & Creatures             & Mental    & Energy and \\glossterm{subdual} \\\\
+        Channel Divinity  & Creatures           & Mental    & Energy      \\\\
+        Chronomancy     & Creatures and objects & Fortitude & Energy      \\\\
+        Cryomancy       & Creatures and objects & Fortitude & Cold        \\\\
+        Electromancy    & Creatures and objects & Fortitude & Electricity \\\\
+        Enchantment     & Creatures             & Mental    & Energy and \\glossterm{subdual} \\\\
+        Fabrication     & Creatures and objects & Armor     & Physical    \\\\
+        Photomancy      & Creatures and objects & Fortitude & Energy      \\\\
+        Polymorph       & Creatures and objects & Fortitude & Physical    \\\\
+        Pyromancy       & Creatures and objects & Armor     & Fire        \\\\
+        Revelation      & Creatures             & Mental    & Energy and \\glossterm{subdual} \\\\
+        Summoning       & Creatures and objects & Armor     & Physical    \\\\
+        Telekinesis     & Creatures and objects & Armor     & Physical    \\\\
+        Terramancy      & Creatures and objects & Armor     & Bludgeoning \\\\
+        Thaumaturgy     & Creatures and objects & Fortitude & Energy      \\\\
+        Toxicology      & Creatures and objects & Fortitude & Acid        \\\\
+        Umbramancy      & Creatures and objects & Fortitude & Cold        \\\\
+        Verdamancy      & Creatures and objects & Armor     & Physical    \\\\
+        Vivimancy       & Living creatures      & Fortitude & Energy      \\\\
       \\end{dtabularx}
-    \\end{dtable}
+    \\end{dtable!*}
   `,
   spells: [
     {
