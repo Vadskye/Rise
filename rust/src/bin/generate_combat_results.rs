5b7e486c1e7599a52b1a6c93a2e82c9992e1ec2b
Kevin Johnson <vadskye@gmail.com>
2022-06-19 08:54:49 -0500

    convert to csv output

--- rust/src/bin/generate_combat_results.rs
+++ rust/src/bin/generate_combat_results.rs
@@ -1,8 +1,8 @@
-use rise::calculations::statistical_combat::{run_combat, CombatResult};
+use rise::calculations::statistical_combat::run_combat;
 use rise::creatures::{Character, Monster};
 use rise::monsters::ChallengeRating;
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use serde::Serialize;
+use std::io;
 
 fn main() {
     let mut standard_combat_results = vec![];
@@ -18,18 +18,28 @@ fn main() {
         }
     }
 
-    let json_string: Result<String> = serde_json::to_string_pretty(&standard_combat_results);
-
-    if let Ok(s) = json_string {
-        println!("{}", s);
+    let mut writer = csv::Writer::from_writer(io::stdout());
+    for r in &standard_combat_results {
+        if let Err(err) = writer.serialize(r) {
+            eprintln!("{}", err);
+        }
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Serialize)]
 struct StandardCombatResult {
     cr: ChallengeRating,
     level: i32,
-    result: CombatResult,
+    // All fields from CombatResult
+    blue_damage_per_round: i32,
+    blue_living_count: usize,
+    blue_rounds_to_live: f64,
+    blue_survival_percent: f64,
+    rounds: f64,
+    red_damage_per_round: i32,
+    red_living_count: usize,
+    red_rounds_to_live: f64,
+    red_survival_percent: f64,
 }
 
 fn run_standard_combat(level: i32, cr: ChallengeRating) -> StandardCombatResult {
@@ -48,11 +58,24 @@ fn run_standard_combat(level: i32, cr: ChallengeRating) -> StandardCombatResult 
     };
     let mut monsters = vec![];
     for _ in 0..count {
-        monsters.push(Monster::standard_monster(cr, level, None, None).creature);
+        if matches!(cr, ChallengeRating::Six) && level >= 3 {
+            monsters.push(Monster::standard_monster(cr, level - 2, None, None).creature);
+        } else {
+            monsters.push(Monster::standard_monster(cr, level, None, None).creature);
+        }
     }
+    let combat_result = run_combat(pcs, monsters);
     return StandardCombatResult {
         cr,
         level,
-        result: run_combat(pcs, monsters),
+        blue_damage_per_round: combat_result.blue_damage_per_round,
+        blue_living_count: combat_result.blue_living_count,
+        blue_rounds_to_live: combat_result.blue_rounds_to_live,
+        blue_survival_percent: combat_result.blue_survival_percent,
+        rounds: combat_result.rounds,
+        red_damage_per_round: combat_result.red_damage_per_round,
+        red_living_count: combat_result.red_living_count,
+        red_rounds_to_live: combat_result.red_rounds_to_live,
+        red_survival_percent: combat_result.red_survival_percent,
     };
 }
