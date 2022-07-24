c406a36bed0f43294c9c9a27d5d24caf821df922
Kevin Johnson <vadskye@gmail.com>
2022-07-24 11:30:16 -0500

    add new upgrade infrastructure

--- python/rise/latex/magic_item.py
+++ python/rise/latex/magic_item.py
@@ -5,6 +5,17 @@ from rise.latex.tags import glosstermify, is_valid_tag
 
 logger = getLogger(__name__)
 
+class Upgrade(object):
+    def __init__(
+            self,
+            description,
+            rank,
+            short_description=None,
+    ):
+        self.description = description
+        self.rank = rank
+        self.short_description = short_description
+
 
 class MagicItem(object):
     @classmethod
@@ -46,6 +57,7 @@ class MagicItem(object):
         materials=None,
         tags=None,
         targeting=None,
+        upgrades=None,
     ):
         self.consumable = consumable
         self.description = description
@@ -54,6 +66,7 @@ class MagicItem(object):
         self.tags = tags
         self.short_description = short_description
         self.material_type = material_type
+        self.upgrades = upgrades
 
         self.effects = effects
         try:
@@ -86,6 +99,14 @@ class MagicItem(object):
         else:
             return None
 
+    def upgrades_text(self):
+        if self.upgrades is None:
+            return None
+        return "\n".join([
+            f"\\rank<{u.rank}> {u.description}"
+            for u in self.upgrades
+        ])
+
     def latex_tags(self):
         return (
             ", ".join([glosstermify(tag) for tag in sorted(self.tags)])
@@ -93,19 +114,20 @@ class MagicItem(object):
             else ""
         )
 
-    def price(self):
-        if self.consumable:
-            return consumable_item_prices[self.rank]
-        else:
-            return item_prices[self.rank]
+    def latex_table_rows(self, include_type=True):
+        rows = [self.latex_table_row(self.rank, self.short_description, include_type)]
+        if self.upgrades is not None:
+            for i, u in enumerate(self.upgrades):
+                rows.append(self.latex_table_row(u.rank, u.short_description or self.short_description, include_type, i+1))
+        return rows
 
-    def latex_table_row(self, include_type=True):
+    def latex_table_row(self, rank, short_description, include_type, upgrade_tier=0):
         material_type = self.material_type or ''
         if material_type == "Body armor":
             material_type = 'Body'
         # \\tb<Name> & \\tb<Item Rank (Cost)> & \\tb<Type> & \\tb<Description> & \\tb<Page> \\tableheaderrule
         type_text = f" & {material_type}" if include_type else ""
-        return f"\\itemref<{self.name}> & {self.rank} ({self.price()} gp) {type_text} & {self.short_description} & \\itempref<{self.name}> \\\\"
+        return f"\\itemref<{plus_suffix(self.name, upgrade_tier)}> & {rank_price_text(self, rank)} {type_text} & {short_description} & \\itempref<{self.name}> \\\\"
 
     def tag_text(self):
         return f"{self.latex_tags()}" if self.tags else ""
@@ -121,7 +143,7 @@ class MagicItem(object):
             if self.material_type is None
             else f"{self.material_type} ({materials_text})"
         )
-        rank_text = f"Rank~{self.rank} ({self.price()} gp)"
+        rank_text = f"Rank~{rank_price_text(self, self.rank)}"
         tag_argument = '[' + self.tag_text() + ']' if self.tag_text() else ''
         return join(
             f"""
@@ -131,4 +153,18 @@ class MagicItem(object):
                 {self.description}
             """,
             self.latex_ability(),
+            self.upgrades_text(),
         )
+
+
+def price(item, rank):
+    if item.consumable:
+        return consumable_item_prices[rank]
+    else:
+        return item_prices[rank]
+
+def plus_suffix(name, upgrade_tier):
+    return name + '+' * upgrade_tier
+
+def rank_price_text(item, rank):
+    return f"{rank} ({price(item, rank)} gp)"
