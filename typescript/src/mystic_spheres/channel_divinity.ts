c6bc578228dba7e5a738d2aadc030fb083486086
Kevin Johnson <vadskye@gmail.com>
2022-07-24 08:26:09 -0500

    rebalance some mental attacks

--- typescript/src/mystic_spheres/channel_divinity.ts
+++ typescript/src/mystic_spheres/channel_divinity.ts
@@ -206,7 +206,7 @@ export const channelDivinity: MysticSphere = {
           As a \\glossterm{condition}, the target takes a -2 penalty to Mental defense.
         `,
         targeting: `
-          Make an attack vs. Mental with a +4 \\glossterm{accuracy} bonus against one creature within \\longrange.
+          Make an attack vs. Mental with a +4 \\glossterm{accuracy} bonus against one creature within \\medrange.
         `,
       },
       rank: 1,
