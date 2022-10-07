7995d2de3e0e40074fb7f0d8bb2f0441fd25cdd5
Kevin Johnson <vadskye@gmail.com>
2022-10-07 07:53:06 -0500

    fix typo

--- rust/src/classes/archetype_rank_abilities/wizard.rs
+++ rust/src/classes/archetype_rank_abilities/wizard.rs
@@ -148,7 +148,7 @@ pub fn arcane_magic<'a>() -> Vec<RankAbility<'a>> {
             is_magical: true,
             rank: 1,
             description: r"
-                Your soulkeeper grants you the ability to use pact magic.
+                Your extensive studies grant you the ability to use arcane magic.
                 You gain access to one arcane \glossterm{mystic sphere}, plus the \sphere{universal} mystic sphere (see \pcref{Arcane Mystic Spheres}).
                 You may spend \glossterm{insight points} to gain access to one additional arcane \glossterm{mystic sphere} per two \glossterm{insight points}.
                 You can only learn arcane spells from arcane mystic spheres that you have access to.
