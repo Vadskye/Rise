875ab3fd94551ce0e5f7119cfd6f5cedc71eead5
Kevin Johnson <vadskye@gmail.com>
2022-10-23 09:37:26 -0500

    update bloodied script

--- character_sheet/api_scripts/damage_dots.js
+++ character_sheet/api_scripts/damage_dots.js
@@ -16,25 +16,25 @@ function updateHitPoints(obj) {
             "status_half-heart": false,
             "status_dead": false,
         })
-    } else if (bar1_value > bar1_max / 2) {
-        // The object has taken damage, but is not bloodied
+    } else if (bar1_value > 0) {
+        // The object has lost HP
         obj.set({
             "status_green": true,
             "status_half-heart": false,
             "status_dead": false,
         });
-    } else if (bar1_value > 0) {
-        // The object is bloodied
+    } else if (bar1_value === 0) {
+        // The object is at 0, so probably has vital wounds
         obj.set({
-            "status_green": true,
+            "status_green": false,
             "status_half-heart": true,
             "status_dead": false,
         });
     } else {
         // The object is dead
         obj.set({
-            "status_green": true,
-            "status_half-heart": true,
+            "status_green": false,
+            "status_half-heart": false,
             "status_dead": true,
         });
     }
@@ -42,21 +42,29 @@ function updateHitPoints(obj) {
 on("change:graphic:bar1_value", updateHitPoints);
 on("change:graphic:bar1_max", updateHitPoints);
 
-function generateResistanceUpdater(barNumber, color) {
-  return (obj) => {
-    const value = Number(obj.get(`bar${barNumber}_value`));
-    const max = Number(obj.get(`bar${barNumber}_max`));
-
-    // Set the given value to be the new max for convenience when setting
-    // up tokens for the first time
-    if (!max) {
-      obj.set({ [`bar${barNumber}_max`]: value});
-      return;
+function updateDamageResistance(obj) {
+    const bar2_value = Number(obj.get("bar2_value"));
+    const bar2_max = Number(obj.get("bar2_max"));
+    
+    if (!bar2_max) {
+        // Set the given value to be the new max for convenience when setting
+        // up tokens for the first time
+        obj.set({ "bar2_max": bar2_value });
+        return;
     }
-
-    obj.set({
-      [`status_${color}`]: value <= 0,
-    });
-  }
+    
+    if (bar2_value >= bar2_max) {
+        // At full DR, so remove all markers
+        obj.set({
+            "status_blue": false,
+        })
+    } else {
+        // At less than full DR, so note DR loss
+        obj.set({
+            "status_blue": true,
+        })
+    }
 }
-on("change:graphic:bar2_value", generateResistanceUpdater(2, "blue"));
+
+on("change:graphic:bar2_value", updateDamageResistance);
+on("change:graphic:bar2_max", updateDamageResistance);
