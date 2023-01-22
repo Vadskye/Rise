010de75fa8308a90f71ba9a51e80f8a9a3e59698
Kevin Johnson <vadskye@gmail.com>
2023-01-22 07:20:55 -0600

    update core classes

--- rust/src/creatures/character.rs
+++ rust/src/creatures/character.rs
@@ -41,11 +41,6 @@ impl Character {
                 None,
             );
         }
-        creature.add_modifier(
-            Modifier::Power(class.power_progression().calc_power((level + 2) / 3)),
-            Some(class.name()),
-            None,
-        );
 
         return Character {
             // archetypes,
