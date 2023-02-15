7c5ad4744b67441a8ede057cde4988e2baf62c50
Kevin Johnson <vadskye@gmail.com>
2023-02-14 21:04:07 -0600

    buff some high level maneuvers

--- typescript/src/combat_styles/rip_and_tear.ts
+++ typescript/src/combat_styles/rip_and_tear.ts
@@ -154,6 +154,9 @@ export const ripAndTear: CombatStyle = {
 
       effect: `
         Make a thrown \\glossterm{strike} using a slashing or bludgeoning weapon against up to four creatures or objects within \\medrange of you.
+        You can choose the same target multiple times, but not twice in a row.
+        If you choose the same target twice, it takes double damage from the attack.
+
         Each target must be within your maximum \\glossterm{range limit} with your weapon, and you take the normal longshot penalty for attacking a creature at long range (see \\pcref{Weapon Range Limits}).
         If you choose yourself as one of the targets, you can catch the weapon instead of taking damage from it.
       `,
