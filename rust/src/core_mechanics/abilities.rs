edd8bf94868cc1820f9044090e0403bcac71e327
Kevin Johnson <vadskye@gmail.com>
2023-03-16 19:43:56 -0500

    remove power progression

--- rust/src/core_mechanics/abilities.rs
+++ rust/src/core_mechanics/abilities.rs
@@ -3,7 +3,6 @@ mod ability_type;
 mod active_ability;
 mod extra_context;
 mod latex;
-mod power_progression;
 mod targeting;
 mod usage_time;
 
@@ -12,6 +11,5 @@ pub use ability_type::AbilityType;
 pub use active_ability::ActiveAbility;
 pub use extra_context::{AbilityExtraContext, AbilityMovement, Cooldown};
 pub use latex::latex_ability_block;
-pub use power_progression::PowerProgression;
 pub use targeting::{AreaSize, AreaTargets, Range, Targeting};
 pub use usage_time::UsageTime;
