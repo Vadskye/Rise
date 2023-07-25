ff5d9797d8c598c98d815e18eb30a5cb35cc65c1
Kevin Johnson <vadskye@gmail.com>
2023-07-25 18:44:21 -0500

    remove unused stock monster generation

--- rust/src/monsters.rs
+++ rust/src/monsters.rs
@@ -1,7 +1,6 @@
 mod challenge_rating;
 mod creature_type;
 mod full_monster_definition;
-mod generate_stock_monsters;
 mod knowledge;
 mod monster_entry;
 mod monster_group;
@@ -12,7 +11,6 @@ pub mod specific_monsters;
 pub use challenge_rating::ChallengeRating;
 pub use creature_type::CreatureType;
 pub use full_monster_definition::FullMonsterDefinition;
-pub use generate_stock_monsters::generate_stock_monsters;
 pub use knowledge::Knowledge;
 pub use monster_entry::generate_monster_entries;
 pub use monster_entry::latex_by_name;
