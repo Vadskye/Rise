033024b0828d4fc0b3cd1db7a778796bba648784
Kevin Johnson <vadskye@gmail.com>
2023-10-01 17:22:32 -0500

    add incomplete attribute-based sheet page

---
+++ character_sheet/paper_attributes/willpower.py
@@ -0,0 +1,110 @@
+from cgi_simple import (
+    checkbox,
+    div,
+    equation,
+    equation_misc,
+    equation_misc_repeat,
+    fieldset,
+    flex_col,
+    flex_row,
+    flex_wrapper,
+    labeled_text_input,
+    minus,
+    number_input,
+    plus,
+    span,
+    text_input,
+    underlabel,
+)
+from sheet_data import (
+    ATTRIBUTE_SHORTHAND,
+    ATTRIBUTE_SKILLS,
+    ATTRIBUTES,
+    ROLL20_CALC,
+    SUBSKILLS,
+)
+
+
+def calc_magical_power():
+    return flex_row(
+        {"class": "magical-power"},
+        [
+            div({"class": "calc-header"}, "Magical Power"),
+            equation(
+                [
+                    underlabel(
+                        "Lvl/2",
+                        number_input(
+                            {
+                                "disabled": True,
+                                "name": "magical_power_scaling",
+                                "value": "floor(@{level}/2)",
+                            }
+                        ),
+                    ),
+                    plus(),
+                    underlabel(
+                        "Wil",
+                        number_input(
+                            {
+                                "disabled": True,
+                                "name": "magical_power_attribute",
+                                "value": "@{willpower}",
+                            }
+                        ),
+                    ),
+                    plus(),
+                    equation_misc_repeat("magical_power", 2),
+                ],
+                result_attributes={
+                    "disabled": True,
+                    "name": "magical_power_display",
+                    "value": "@{magical_power}",
+                },
+            ),
+        ],
+    )
+
+
+def calc_mental():
+    return flex_row(
+        [
+            div({"class": "calc-header"}, "Ment"),
+            equation(
+                [
+                    underlabel(
+                        "Lvl/2",
+                        number_input(
+                            {
+                                "disabled": True,
+                                "name": "mental_scaling_display",
+                                "value": "(floor(@{level}/2))",
+                            }
+                        ),
+                    ),
+                    plus(),
+                    underlabel(
+                        "Wil",
+                        number_input(
+                            {
+                                "disabled": True,
+                                "name": "mental_attribute",
+                                "value": "(@{willpower})",
+                            }
+                        ),
+                    ),
+                    plus(),
+                    underlabel(
+                        "Class", number_input({"name": "mental_class", "value": "5"})
+                    ),
+                    plus(),
+                    equation_misc_repeat("mental", 3),
+                ],
+                result_attributes={
+                    "disabled": "true",
+                    "name": "mental_display",
+                    "value": "@{mental}",
+                },
+            ),
+        ]
+    )
