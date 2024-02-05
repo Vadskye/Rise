de3514a0942b7b5d0b49a6f7cb41da26242eb289
Kevin Johnson <vadskye@gmail.com>
2024-02-04 19:01:28 -0600

    avoid duplicate spells known

--- rust/src/classes/archetype_rank_abilities/cleric.rs
+++ rust/src/classes/archetype_rank_abilities/cleric.rs
@@ -27,8 +27,7 @@ pub fn divine_magic<'a>() -> Vec<RankAbility<'a>> {
                     you can forget any number of spells you know to learn that many new spells in exchange,
                     including spells of the higher rank.
 
-                \advancement At ranks 2, 4, and 7, you learn an additional divine spell.
-                The maximum rank of divine spells that you can learn is equal to your rank in this archetype.
+                \advancement The maximum rank of divine spells that you can learn is equal to your rank in this archetype.
                 Divine spells also increase in power in unique ways based on your rank in this archetype, as indicated in their descriptions.
             ",
             modifiers: None,
