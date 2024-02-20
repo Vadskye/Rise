26f40511b26f0d0ccd25d018a81867a01725c9ec
Kevin Johnson <vadskye@gmail.com>
2024-02-20 07:20:58 -0600

    improve wand access

--- rust/src/equipment/implements/wands.rs
+++ rust/src/equipment/implements/wands.rs
@@ -10,11 +10,12 @@ pub fn wands() -> Vec<Implement> {
         short_description: String::from(r"Grants knowledge of a rank 1 spell"),
         description: String::from(r"
             This wand grants you knowledge of a single rank 1 spell that does not have the \abilitytag<Attune> or \abilitytag<Sustain> tags.
-            Each wand is associated with a specific spell, and a single \glossterm<magic source> that can grant access to that spell.
-            You must have the ability to cast spells of the given rank from the same \glossterm<magic source> as the wand.
+            Each wand is associated with a specific spell.
+            You must have the ability to cast spells that are no more than one rank lower than the wand's rank.
+            In addition, your \glossterm{magic source} must grant access to the \glossterm{mystic sphere} that the spell belongs to.
             However, you do not need to have access to the \glossterm<mystic sphere> that the spell belongs to.
-            Spells you know because of a spell wand gain any rank upgrades appropriate to your rank with that form of spellcasting.
 
+            If you can cast spells of a higher rank than the wand's rank, the spell from the wand gains any appropriate rank upgrades.
             If you stop wielding this wand, deattune from it, or otherwise lose access to its magical effects, the effects of any active spells that you know because of the wand also end, regardless of their normal duration.
         "),
         ..Implement::default()
