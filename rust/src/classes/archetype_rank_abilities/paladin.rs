f15bc350cd55e068f5e48a4d6a994ad2b3871793
Kevin Johnson <vadskye@gmail.com>
2024-06-20 20:27:23 -0500

    buff class feature healing

--- rust/src/classes/archetype_rank_abilities/paladin.rs
+++ rust/src/classes/archetype_rank_abilities/paladin.rs
@@ -259,10 +259,11 @@ pub fn stalwart_guardian<'a>() -> Vec<RankAbility<'a>> {
             rank: 1,
             description: r"
                 You can use the \textit{lay on hands} ability as a standard action.
+                % dr2 for close range, +1dr for touch or healing bonus idk, free poison/disease because class feature
                 \begin{magicalactiveability}{Lay on Hands}[\abilitytag{Swift}]
                     \rankline
                     Choose yourself or a living \glossterm{ally} you \glossterm{touch}.
-                    The target regains 1d8 \glossterm{hit points} +1 per 2 power.
+                    The target regains 1d8 \glossterm{hit points} +1 per power.
                     In addition, it removes all poisons and diseases affecting it, and it \glossterm{briefly} becomes immune to poisons and diseases.
                     % TODO: wording
                     This immunity and removal can only affect poisons and diseases with a rank no more than two ranks higher than your rank in this archetype.
@@ -271,12 +272,12 @@ pub fn stalwart_guardian<'a>() -> Vec<RankAbility<'a>> {
                     If you increase your \glossterm{fatigue level} by one, you can ignore this limitation.
 
                     \rankline
-                    \rank{2} The bonus healing increases to \plus1 per power.
-                    \rank{3} The base healing increases to 1d10.
+                    \rank{2} The bonus healing increases to 1d6 per 3 power.
+                    \rank{3} The base healing increases to 2d8.
                     \rank{4} The bonus healing increases to 1d8 per 3 power.
-                    \rank{5} The base healing increases to 2d8.
+                    \rank{5} The base healing increases to 4d8.
                     \rank{6} The bonus healing increases to 1d8 per 2 power.
-                    \rank{7} The base healing increases to 4d8.
+                    \rank{7} The base healing increases to 6d8.
                 \end{magicalactiveability}
 
             ",
