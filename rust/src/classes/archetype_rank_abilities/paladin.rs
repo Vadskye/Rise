013b4d867601b36f20afc9501cc4ca17381169f0
Kevin Johnson <vadskye@gmail.com>
2024-10-15 20:17:25 -0500

    clarify pass judgment

--- rust/src/classes/archetype_rank_abilities/paladin.rs
+++ rust/src/classes/archetype_rank_abilities/paladin.rs
@@ -421,7 +421,7 @@ pub fn zealous_warrior<'a>() -> Vec<RankAbility<'a>> {
                     \rankline
                     Choose one creature within \distrange.
                     You always gain the benefit of your \textit{zealous fixation} ability against that creature.
-                    In addition, the target is treated as if it had the alignment opposed to your devoted alignment for the purpose of all abilities.
+                    In addition, the target is treated as if it had the alignment opposed to your devoted alignment for the purpose of all abilities, not just your own.
                     This only affects its alignment along the alignment axis your devoted alignment is on.
                     For example, if your devoted alignment was evil, a chaotic neutral target would be treated as chaotic good.
                     This effect lasts until you treat that creature as an \glossterm{ally}, or until you \glossterm{dismiss} this ability.
