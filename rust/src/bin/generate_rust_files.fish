15e9b302f37092cd361aded93a7782ac870496d4
Kevin Johnson <vadskye@gmail.com>
2024-12-21 12:28:34 -0600

    Tome of Creation -> Comprehensive Codex

--- rust/src/bin/generate_rust_files.fish
+++ rust/src/bin/generate_rust_files.fish
@@ -3,7 +3,7 @@ set FILENAME (status filename)
 set DIR (dirname $FILENAME)
 cd "$DIR/../../.."
 mkdir -p core_book/generated
-mkdir -p tome_of_creation/generated
+mkdir -p comprehensive_codex/generated
 cd rust
 
 cargo build; or exit 1
@@ -13,25 +13,25 @@ cargo build; or exit 1
 cargo run --bin monsters_chapter > ../core_book/generated/monster_descriptions.tex
 cargo run --bin monster_reference_table > ../core_book/generated/monster_reference_table.tex
 # cargo run --bin modules_chapter > ../core_book/generated/modules.tex
-cargo run --bin classes_chapter > ../tome_of_creation/generated/classes.tex
+cargo run --bin classes_chapter > ../comprehensive_codex/generated/classes.tex
 
-cargo run --bin item_latex -- --category 'apparel' --descriptions > ../tome_of_creation/generated/apparel.tex
-cargo run --bin item_latex -- --category 'apparel' --table > ../tome_of_creation/generated/apparel_table.tex
+cargo run --bin item_latex -- --category 'apparel' --descriptions > ../comprehensive_codex/generated/apparel.tex
+cargo run --bin item_latex -- --category 'apparel' --table > ../comprehensive_codex/generated/apparel_table.tex
 
-cargo run --bin item_latex -- --category 'implements' --descriptions > ../tome_of_creation/generated/implements.tex
-cargo run --bin item_latex -- --category 'implements' --table > ../tome_of_creation/generated/implements_table.tex
+cargo run --bin item_latex -- --category 'implements' --descriptions > ../comprehensive_codex/generated/implements.tex
+cargo run --bin item_latex -- --category 'implements' --table > ../comprehensive_codex/generated/implements_table.tex
 
-cargo run --bin item_latex -- --category 'magic armor' --descriptions > ../tome_of_creation/generated/magic_armor.tex
-cargo run --bin item_latex -- --category 'magic armor' --table > ../tome_of_creation/generated/magic_armor_table.tex
+cargo run --bin item_latex -- --category 'magic armor' --descriptions > ../comprehensive_codex/generated/magic_armor.tex
+cargo run --bin item_latex -- --category 'magic armor' --table > ../comprehensive_codex/generated/magic_armor_table.tex
 
-cargo run --bin item_latex -- --category 'magic weapons' --descriptions > ../tome_of_creation/generated/magic_weapons.tex
-cargo run --bin item_latex -- --category 'magic weapons' --table > ../tome_of_creation/generated/magic_weapons_table.tex
+cargo run --bin item_latex -- --category 'magic weapons' --descriptions > ../comprehensive_codex/generated/magic_weapons.tex
+cargo run --bin item_latex -- --category 'magic weapons' --table > ../comprehensive_codex/generated/magic_weapons_table.tex
 
-cargo run --bin item_latex -- --category 'consumable tools' --descriptions > ../tome_of_creation/generated/consumable_tools.tex
-cargo run --bin item_latex -- --category 'consumable tools' --table > ../tome_of_creation/generated/consumable_tools_table.tex
+cargo run --bin item_latex -- --category 'consumable tools' --descriptions > ../comprehensive_codex/generated/consumable_tools.tex
+cargo run --bin item_latex -- --category 'consumable tools' --table > ../comprehensive_codex/generated/consumable_tools_table.tex
 
-cargo run --bin item_latex -- --category 'permanent tools' --descriptions > ../tome_of_creation/generated/permanent_tools.tex
-cargo run --bin item_latex -- --category 'permanent tools' --table > ../tome_of_creation/generated/permanent_tools_table.tex
+cargo run --bin item_latex -- --category 'permanent tools' --descriptions > ../comprehensive_codex/generated/permanent_tools.tex
+cargo run --bin item_latex -- --category 'permanent tools' --table > ../comprehensive_codex/generated/permanent_tools_table.tex
 
 mkdir -p combat_results
 cargo run --bin generate_combat_results
