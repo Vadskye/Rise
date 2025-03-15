04d75835c15b726965d6af600178bc5a1630af5f
Kevin Johnson <vadskye@gmail.com>
2025-03-15 13:40:27 -0500

    redesign feral frenzy

--- rust/src/classes/archetype_rank_abilities/barbarian.rs
+++ rust/src/classes/archetype_rank_abilities/barbarian.rs
@@ -519,11 +519,14 @@ pub fn totemist<'a>() -> Vec<RankAbility<'a>> {
             rank: 3,
             // TODO: run this through the spreadsheet
             description: r"
+                At the end of each round, if you dealt damage to a creature other than yourself that round, you gain a frenzy point, to a maximum of 4.
+                Otherwise, you lose a frenzy point, to a minimum of 0.
+                Frenzy points increase the power of your \ability{feral frenzy} ability.
                 \begin{activeability}{Feral Frenzy}
                     \abilityusagetime Standard action.
                     \rankline
                     Make a melee or thrown \glossterm{strike}.
-                    If you used this ability last round, you can make an additional melee or thrown strike.
+                    You can spend three frenzy points to make an additional melee or thrown strike.
 
                     \rankline
                     \rank{4} You gain a +2 accuracy bonus with the first strike you make with this ability each round.
