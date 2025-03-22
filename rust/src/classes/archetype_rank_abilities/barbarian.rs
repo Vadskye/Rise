8c8a76729444800e24f7ae72f997888a4c3e45e1
Kevin Johnson <vadskye@gmail.com>
2025-03-22 09:08:12 -0500

    redesign devastating maneuver

--- rust/src/classes/archetype_rank_abilities/barbarian.rs
+++ rust/src/classes/archetype_rank_abilities/barbarian.rs
@@ -399,9 +399,7 @@ pub fn primal_warrior<'a>() -> Vec<RankAbility<'a>> {
                 Whenever you increase your rank in this archetype, you can change your augments.
                 However, you must still apply them to rank 1 primal maneuvers.
                 {
-                    \parhead{Devastating Maneuver} The maneuver deals double damage, but you take an accuracy penalty equal to 5 - your excess rank.
-                    If your excess rank is at least 5, this becomes an accuracy bonus.
-                    You cannot apply this augment to \atBrawling maneuvers or maneuvers which cause you to make a \glossterm{strike}.
+                    \parhead{Devastating Maneuver} The maneuver deals double damage, but you take an accuracy penalty equal to 6 - your excess rank.
 
                     \parhead{Finishing Maneuver} You gain an accuracy bonus equal to twice your excess rank against creatures who are at less than their maximum \glossterm{hit points}.
 
