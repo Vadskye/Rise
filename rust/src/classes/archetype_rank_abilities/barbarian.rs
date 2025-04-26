f530606fa8ae25828bd46e921d74090598158111
Kevin Johnson <vadskye@gmail.com>
2025-04-26 13:19:57 -0500

    clarify augments

--- rust/src/classes/archetype_rank_abilities/barbarian.rs
+++ rust/src/classes/archetype_rank_abilities/barbarian.rs
@@ -393,6 +393,9 @@ pub fn primal_warrior<'a>() -> Vec<RankAbility<'a>> {
             description: r"
                 You gain the ability to customize your primal maneuvers.
                 For each rank 1 primal maneuver you know, choose one augment from the list below and apply it to that maneuver.
+                The augment permanently changes the maneuver, so you can't use an unaugmented version of the maneuver.
+                However, you can learn the same maneuver more than once and apply different augments to each version.
+
                 Augments scale in power with your ``excess rank''.
                 Your excess rank with a maneuver is equal to your rank in this archetype minus the rank of the maneuver.
 
