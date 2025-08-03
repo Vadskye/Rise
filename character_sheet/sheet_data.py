740a6299def9b08f75d18de86d502bacb928a843
Kevin Johnson <vadskye@gmail.com>
2025-08-03 07:18:07 -0500

    reorder mental and reflex defenses

--- character_sheet/sheet_data.py
+++ character_sheet/sheet_data.py
@@ -1,7 +1,7 @@
 from cgi_simple import value_sum
 
 ATTRIBUTES = "Strength Dexterity Constitution Intelligence Perception Willpower".split()
-DEFENSES = "Armor Brawn Fortitude Reflex Mental".split()
+DEFENSES = "Armor Brawn Fortitude Mental Reflex".split()
 ATTRIBUTE_SHORTHAND = {
     "strength": "Str",
     "dexterity": "Dex",
