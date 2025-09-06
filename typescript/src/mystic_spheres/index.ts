632d1bab26a1c1335af0846ee61920d2e23e5fcc
Kevin Johnson <vadskye@gmail.com>
2025-09-05 20:48:09 -0500

    update verdamancy

--- typescript/src/mystic_spheres/index.ts
+++ typescript/src/mystic_spheres/index.ts
@@ -71,7 +71,7 @@ export interface BaseSpellLike {
   name: string;
   narrative?: string;
   // TODO: make this mandatory
-  roles?: AbilityRole[];
+  roles: AbilityRole[];
   scaling?: 'accuracy' | 'double_accuracy' | 'damage' | 'healing' | Record<string, string>;
   tableText?: string;
   tags?: string[];
