7c61cd035087feea7eefee5fc164afd09728304a
Kevin Johnson <vadskye@gmail.com>
2025-09-14 08:07:05 -0500

    add maneuver script variants

---
+++ typescript/src/scripts/maneuver_rank_breakdown.ts
@@ -0,0 +1,20 @@
+import { combatStyles, Maneuver } from '../combat_styles';
+import { run, BreakdownConfig } from '../util/breakdown_cli';
+
+type ManeuverRank = 1 | 3 | 5 | 7;
+const RANKS: ManeuverRank[] = [1, 3, 5, 7] as const;
+
+const config: BreakdownConfig<any, Maneuver, ManeuverRank> = {
+  entities: combatStyles,
+  entityTypePlural: 'styles',
+  entityTypeSingular: 'Style',
+  getItemGroups: maneuver => maneuver.rank,
+  getItems: style => style.maneuvers || [],
+  groups: RANKS,
+  groupType: 'Rank',
+  tableOrientation: 'columnsAsGroups',
+};
+
+if (require.main === module) {
+  run(config);
+}
