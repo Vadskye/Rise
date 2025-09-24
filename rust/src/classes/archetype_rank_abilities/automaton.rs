dd606560ab1a65bd00d74cc12dd1d3a39ba7d6ee
Kevin Johnson <vadskye@gmail.com>
2025-09-23 21:00:37 -0500

    partially implement uncommon species classes in rust

--- rust/src/classes/archetype_rank_abilities/automaton.rs
+++ rust/src/classes/archetype_rank_abilities/automaton.rs
@@ -1,6 +1,149 @@
 use crate::classes::archetype_rank_abilities::RankAbility;
+use crate::core_mechanics::Resource;
+use crate::creatures::Modifier;
 
 pub fn automaton<'a>() -> Vec<RankAbility<'a>> {
-    // TODO
-    vec![]
+    vec![
+        RankAbility {
+            complexity: 1,
+            name: "Modular Carapace",
+            is_magical: false,
+            rank: 1,
+            description: r"
+                You can adjust the density and layering of your hardened exterior to augment your defenses.
+                Changing your configuration in this way requires 10 minutes of work, and spare armor parts that you generally keep with you.
+                You can choose to treat your carapace as being either light, medium, or heavy armor.
+                To gain the full benefits of your carapace, you must have proficiency with armor of the appropriate usage class.
+                The benefits from this ability are considered to come from body armor, and do not stack with actual body armor.
+
+                You can use magic armor to build your carapace.
+                If you do, the magic armor becomes embedded in your body.
+                You can attune to it to benefit from its effects.
+                If you use light modular carapace, this can allow you to benefit from two different magic armor effects.
+
+                \begin{raggeditemize}
+                    \item Light armor: You gain a \plus3 bonus to your Armor defense and a \plus2 bonus to your \glossterm{durability}.
+                    You can wear body armor on top of this carapace.
+                    Although the benefits of that armor do not stack with the carapace, you can use the higher Armor defense value and durability bonus from either armor.
+                    \item Medium armor: You gain a \plus5 bonus to your Armor defense, a \plus4 bonus to your \glossterm{durability}, and a \plus1 bonus to \glossterm{vital rolls}.
+                    However, your Dexterity bonus to your Armor defense is halved, and you cannot wear body armor.
+                    \item Heavy armor: You gain a \plus6 bonus to your Armor defense, a \plus8 bonus to your \glossterm{durability}, and a \plus2 bonus to \glossterm{vital rolls}.
+                    However, your Dexterity bonus to your Armor defense is halved, you take a \minus10 foot penalty to your speed with all movement modes, and you cannot wear body armor.
+                    Unlike normal for heavy body armor, you do not need a minimum Strength to use this armor.
+                \end{raggeditemize}
+
+                If you lose your original armor parts, you can create or buy new parts that are suited to your body.
+                These parts are considered a Rank 1 (40 gp) item.
+            ",
+            modifiers: None, // Too complex for simple modifiers, description handles it
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Modular Armaments",
+            is_magical: false,
+            rank: 2,
+            description: r"
+                You can customize your arms.
+                Changing your arm configuration in this way requires 10 minutes of work, and spare arm and weapon parts that you generally keep with you.
+                It also requires at least one \glossterm{free hand}.
+                You can combine any number of different customizations with this ability.
+                Some customizations apply to both of your arms, but others apply to only one arm, as indicated.
+                \begin{raggeditemize}
+                    \item Bulky: You augment both of your arms with additional strength.
+                    You gain a \plus1 bonus to your \glossterm{mundane power}, and to your Strength for the purpose of determining your \glossterm{weight limits}.
+                    However, you increase your \glossterm{encumbrance} by 2.
+                    \item Fortified: You add additional protective plating to both of your arms.
+                    You gain a \plus1 bonus to your Armor defense, but you increase your \glossterm{encumbrance} by 1.
+                    This does not require a \glossterm{free hand}, but it is still considered to come from a shield, and it does not stack with the benefit from using a shield.
+                    \item Slim: You trim away excess muscle from both of your arms to make their movements more precise.
+                    You gain a \plus1 bonus to your \glossterm{accuracy} with \glossterm{strikes}.
+                    However, you take a \minus1 penalty to your \glossterm{mundane power}.
+                    \item Weapon: You convert one of your arms into a manufactured weapon of your choice that you are \glossterm{proficient} with.
+                    It is considered either a \glossterm{natural weapon}, a manufactured weapon, or both whenever it would be beneficial for you.
+                    However, that arm no longer has a \glossterm{free hand}.
+                    You can incorporate a magic weapon into this process or find a smith to imbue your arm parts as if they were a magic weapon.
+                    If you do, you can attune to the magic weapon property, and it affects this weapon.
+                \end{raggeditemize}
+
+                If you lose your original arm and weapon parts, you can create or buy new parts that are suited to your body.
+                These parts are considered a Rank 1 (40 gp) item.
+            ",
+            modifiers: None, // Too complex for simple modifiers, description handles it
+        },
+        RankAbility {
+            complexity: 2,
+            name: "Sharpening Slash",
+            is_magical: true,
+            rank: 3,
+            description: r"
+                \begin{activeability}{Sharpening Slash}
+                    \abilityusagetime Standard action.
+                    \rankline
+                    Make a \glossterm{strike} that deals \glossterm{extra damage} equal to half your \glossterm{power}.
+                    Then, if you did not get a critical hit with the strike, you are \glossterm{briefly} \honed.
+
+                    \rankline
+                    \rank{4} The extra damage increases to 1d6 \add half your power.
+                    \rank{5} The extra damage increases to 1d6 \add your power.
+                    \rank{6} The extra damage increases to 3d6 \add your power.
+                    \rank{7} The strike deals double \glossterm{weapon damage}.
+                \end{activeability}
+            ",
+            modifiers: None, // Damage scaling handled by description
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Embedded Apparel",
+            is_magical: true,
+            rank: 4,
+            description: r"
+                You can embed magic apparel into your body with an hour of work.
+                The item becomes a part of your body, and would require another hour of work to remove.
+                This allows you to use one additional apparel item from each body slot (see \pcref{Body Slots}).
+                For example, you could embed one set of magic boots into your feet and then wear another pair of magic boots over them.
+                You also gain an additional \glossterm{attunement point} that you can only use to attune to items embedded into your body.
+                You cannot embed a \glossterm{legacy item} in this way.
+            ",
+            modifiers: Some(vec![Modifier::Resource(Resource::AttunementPoint, 1)]),
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Reassembly",
+            is_magical: false,
+            rank: 5,
+            description: r"
+                You can recover from vital wounds more easily by simply replacing broken parts. 
+                You can remove a vital wound with ten minutes of work.
+                This increases your \glossterm{fatigue level} by three, and it requires replacement parts that you generally keep with you.
+                The parts are considered a consumable Rank 3 (200 gp) item.
+
+                This can even save you from death, though that is more difficult and requires more advanced parts.
+                A creature can spend eight hours replacing broken parts of your corpse to \glossterm{resurrect} you (see \pcref{Resurrection}).
+                This requires a \glossterm{difficulty value} 20 \glossterm{extended check} using a Craft skill appropriate to the composition of your body.
+                The parts required to perform this feat are considered a consumable Rank 5 (5,000 gp) item.
+            ",
+            modifiers: None, // Complex effect, description handles it
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Artificial Mind",
+            is_magical: false,
+            rank: 6,
+            description: r"
+                You become immune to \abilitytag{Compulsion} and \abilitytag{Emotion} attacks.
+            ",
+            modifiers: None, // Immunity, description handles it
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Infinite Edge",
+            is_magical: false,
+            rank: 7,
+            description: r"
+                You are always \honed.
+                When an ability would cause you to become honed, such as your \ability{sharpening slash} ability, you become \empowered instead.
+            ",
+            modifiers: None, // Complex effect, description handles it
+        },
+    ]
 }
---
+++ rust/src/classes/archetype_rank_abilities/treant.rs
@@ -0,0 +1,113 @@
+use crate::classes::archetype_rank_abilities::RankAbility;
+use crate::core_mechanics::Attribute;
+use crate::creatures::Modifier;
+
+pub fn treant<'a>() -> Vec<RankAbility<'a>> {
+    vec![
+        RankAbility {
+            complexity: 1,
+            name: "Nourishing Ingrain",
+            is_magical: false,
+            rank: 1,
+            description: r##"
+                At the end of each round while you are \ability{ingrained}, you regain hit points equal to your rank in this archetype, and you may choose to remove a \glossterm{condition}.
+                If you do, you increase your \glossterm{fatigue level} by one.
+            "##,
+            modifiers: None,
+        },
+        RankAbility {
+            complexity: 0,
+            name: "Sturdy as the Mighty Oak",
+            is_magical: false,
+            rank: 2,
+            description: r##"
+                You gain a \plus3 bonus to your \glossterm{durability}.
+            "##,
+            modifiers: None,
+        },
+        RankAbility {
+            complexity: 2,
+            name: "Animate Plants",
+            is_magical: true,
+            rank: 3,
+            description: r##"
+                \begin{magicalactiveability}{Animate Plants}[\abilitytag{Manifestation}]
+                    \abilityusagetime Standard action.
+                    \rankline
+                    Make an attack vs. Reflex against one Large or smaller \glossterm{grounded} creature within \medrange.
+                    You gain a \plus2 accuracy bonus if the target is in \glossterm{undergrowth}.
+
+                    \hit The target is \glossterm{briefly} \slowed.
+                    In addition, it is attacked by plants as a \glossterm{condition}.
+                    It takes 1d8 damage immediately, and during each of your subsequent actions while this condition lasts.
+
+                    This condition can be removed if the target makes a \glossterm{difficulty value} 10 Strength check as a \glossterm{move action} to break the plants.
+                    If the target makes this check as a standard action, it gains a \plus5 bonus.
+                    In addition, this condition is removed if the target takes damage from a \atFire ability.
+                    \crit The condition must be removed an additional time before the effect ends.
+                    \rankline
+                    For each rank beyond 3, the attack's \glossterm{accuracy} increases by \plus2 and the \glossterm{difficulty value} to break the plants increases by 2.
+                    In addition, the damage increases at each rank as described below.
+                    \rank{4} 1d10 damage.
+                    \rank{5} 2d6 damage.
+                    \rank{6} 2d8 damage.
+                    \rank{7} 2d10 damage.
+                \end{magicalactiveability}
+            "##,
+            modifiers: None,
+        },
+        RankAbility {
+            complexity: 0,
+            name: "Tall as the Noble Pine",
+            is_magical: false,
+            rank: 4,
+            description: r##"
+                Your size category increases to Large.
+                Unlike normal for increasing your size, this does not increase your \glossterm{base speed}.
+                You also gain a \plus1 bonus to your Strength, and a \minus1 penalty to your Dexterity.
+            "##,
+            modifiers: Some(vec![
+                Modifier::Attribute(Attribute::Strength, 1),
+                Modifier::Attribute(Attribute::Dexterity, -1),
+            ]),
+        },
+        RankAbility {
+            complexity: 1,
+            name: "Nourishing Ingrain+",
+            is_magical: false,
+            rank: 5,
+            description: r##"
+                The healing from your \textit{nourishing ingrain} ability increases to three times your rank in this archetype.
+                In addition, removing a condition with that ability no longer increases your fatigue level.
+            "##,
+            modifiers: None,
+        },
+        RankAbility {
+            complexity: 0,
+            name: "Sturdy as the Mighty Oak+",
+            is_magical: false,
+            rank: 6,
+            description: r##"
+                The durability bonus increases to \plus5.
+            "##,
+            modifiers: None,
+        },
+        RankAbility {
+            complexity: 0,
+            name: "Tall as the Noble Pine+",
+            is_magical: false,
+            rank: 7,
+            description: r##"
+                Your size category increases to Huge.
+                This increases your \glossterm{base speed} to 40 feet.
+                Your normal movement speed is still only 30 feet due to the penalty from \textit{unhurried and unfaltering}.
+                The modifiers to Strength and Dexterity increase to \plus2 and \minus2, respectively.
+            "##,
+            modifiers: Some(vec![
+                Modifier::BaseSpeed(10),
+                Modifier::Attribute(Attribute::Strength, 1),
+                Modifier::Attribute(Attribute::Dexterity, -1),
+            ]),
+        },
+    ]
+}
