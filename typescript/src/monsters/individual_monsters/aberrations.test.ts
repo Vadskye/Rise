3ffe50bf97d5e60e7bf5f8dc76f3671cc8cd1d10
Kevin Johnson <vadskye@gmail.com>
2025-10-04 10:49:23 -0500

    use Typescript monster manual by default

--- typescript/src/monsters/individual_monsters/aberrations.test.ts
+++ typescript/src/monsters/individual_monsters/aberrations.test.ts
@@ -2,24 +2,37 @@ import t from 'tap';
 import { Grimoire } from '@src/monsters/grimoire';
 import { addAberrations } from './aberrations';
 
-t.test('can create an aboleth', (t) => {
+t.test('aboleth', (t) => {
   const grimoire = new Grimoire();
   addAberrations(grimoire);
+  const aboleth = grimoire.getMonster('Aboleth');
 
-  const aboleth = grimoire.getMonster('aboleth');
-  const vals = aboleth.getPropertyValues([
-    'awareness',
-    'hit_points',
-    'level',
-    'name',
-    'perception',
-  ]);
-  t.match(vals, {
-    awareness: 13,
-    hit_points: 270,
-    level: 12,
-    name: 'aboleth',
-    perception: 4,
+  t.test('has basic combat values', (t) => {
+    const vals = aboleth.getPropertyValues([
+      'awareness',
+      'hit_points',
+      'level',
+      'name',
+      'perception',
+      'size',
+      'speed',
+    ]);
+    t.match(vals, {
+      awareness: 13,
+      hit_points: 270,
+      level: 12,
+      name: 'Aboleth',
+      perception: 4,
+      size: 'huge',
+      speed: 50,
+    });
+    t.end();
   });
+
+  t.test('has movement speeds', (t) => {
+    t.match(aboleth.getCustomMovementSpeeds(), ['Swim (normal)', 'Land (slow)']);
+    t.end();
+  });
+
   t.end();
 });
