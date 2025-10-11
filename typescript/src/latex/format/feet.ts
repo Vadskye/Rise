327fa2f78398724b1e76bdcd834e69befff6c2f8
Kevin Johnson <vadskye@gmail.com>
2025-10-11 09:07:50 -0500

    add more tests

--- typescript/src/latex/format/feet.ts
+++ typescript/src/latex/format/feet.ts
@@ -1,7 +1,9 @@
 export function feet(value: number): string {
   if (value % 1 === 0.5) {
     return `${Math.floor(value)}-1/2 ft.`;
-  } else {
+  } else if (value % 1 === 0) {
     return `${value} ft.`;
+  } else {
+    throw new Error(`It doesn't make sense to have decimal feet: '${value}'`);
   }
 }
