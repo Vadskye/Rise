1ebe339488046372db3c6798b71ab1e007d0d33f
Kevin Johnson <vadskye@gmail.com>
2025-10-16 18:21:08 -0500

    add more humanoids to manual

--- typescript/src/monsters/weapons.ts
+++ typescript/src/monsters/weapons.ts
@@ -2,7 +2,7 @@
 // weapons, we'll need to be more organized.
 // If the name is plural, it indicates that the monster is using two of the weapon to
 // make a dual strike.
-export type MonsterWeapon = 'bite' | 'claws' | 'horn' | 'ram' | 'stinger' | 'talons' | 'tentacle' | 'heavy crossbow' | 'spear' | 'longbow';
+export type MonsterWeapon = 'bite' | 'claws' | 'horn' | 'ram' | 'stinger' | 'talons' | 'tentacle' | 'heavy crossbow' | 'spear' | 'longbow' | 'sickle' | 'club' | 'lance';
 
 export function getWeaponTag(weaponName: MonsterWeapon): string | null {
   return {
@@ -12,10 +12,13 @@ export function getWeaponTag(weaponName: MonsterWeapon): string | null {
     horn: 'Keen',
     longbow: 'Projectile (90/270)', // Ignore Heavy tag
     ram: 'Impact',
-    spear: 'Long, Thrown (30/60)', // Ignore Versatile Grip tag
+    spear: 'Thrown (30/60)', // Ignore Versatile Grip tag and assume one-handing, so not Long
     stinger: null,
     talons: null,
     tentacle: null, // This has the Maneuverable tag, but that doesn't affect strikes.
+    sickle: null,
+    club: null,
+    lance: 'Mounted',
   }[weaponName];
 }
 
@@ -40,6 +43,9 @@ export function getWeaponDamageDice(weaponName: MonsterWeapon): DicePool {
     stinger: xdy(1, 6),
     talons: xdy(2, 4),
     tentacle: xdy(1, 6),
+    sickle: xdy(1, 4),
+    club: xdy(1, 8),
+    lance: xdy(1, 6),
   }[weaponName];
 }
 
@@ -55,6 +61,9 @@ export function getWeaponAccuracy(weaponName: MonsterWeapon): number {
     stinger: 1,
     talons: 2,
     tentacle: 0,
+    sickle: 0,
+    club: 0,
+    lance: 0,
   }[weaponName];
 }
 
@@ -71,6 +80,9 @@ export function getWeaponPowerMultiplier(weaponName: MonsterWeapon): 0.5 | 1 {
       stinger: 1,
       talons: 0.5,
       tentacle: 1,
+      sickle: 0.5,
+      club: 1,
+      lance: 0.5,
     } as const
   )[weaponName];
 }
