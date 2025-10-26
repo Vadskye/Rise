94fe63ef8d3a5c16ff5ed285f7909895e5484056
Kevin Johnson <vadskye@gmail.com>
2025-10-25 21:49:02 -0500

    fix monster_type generation

---
+++ typescript/src/scripts/explain_monster.ts
@@ -0,0 +1,21 @@
+import { Grimoire } from '@src/monsters/grimoire';
+import { Creature } from '@src/character_sheet/creature';
+
+function main(monsterNameInput: string) {
+  const grimoire = new Grimoire();
+  grimoire.addAllMonsters();
+
+  let monster: Creature;
+  if (/\./.test(monsterNameInput)) {
+    const [groupName, monsterName] = monsterNameInput.split('.');
+    monster = grimoire.getMonsterGroup(groupName)!.monsters.find((monster) => monster.name === monsterName)!;
+  } else {
+    monster = grimoire.getMonster(monsterNameInput)!;
+  }
+
+  console.log('monster.getCommonExplanations()', monster.getCommonExplanations());
+}
+
+if (require.main === module) {
+  main(process.argv[2]);
+}
