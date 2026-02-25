bfb5ab45e2d70484c5b8f4041fb6253c261b0971
Kevin Johnson <vadskye@gmail.com>
2026-02-24 18:11:44 -0600

    combat round test draft

---
+++ typescript/src/combat/static_creatures.ts
@@ -0,0 +1,8 @@
+import { Grimoire } from "@src/monsters/grimoire";
+import { StockCharacters } from "@src/character_sheet/stock_characters";
+
+export const grimoire = new Grimoire();
+grimoire.addAllMonsters();
+
+export const stockCharacters = new StockCharacters();
+stockCharacters.addAllCharacters();
