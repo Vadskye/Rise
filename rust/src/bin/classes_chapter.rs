a2f1a3af8df0faf97f4413644426035e25789df1
Kevin Johnson <vadskye@gmail.com>
2026-05-07 18:18:59 -0500

    cleanup

--- rust/src/bin/classes_chapter.rs
+++
@@ -1,9 +0,0 @@
-use rise::classes::Class;
-
-fn main() {
-    let classes = Class::core_classes();
-    for class in classes {
-        println!("{}\n", class.latex_section());
-    }
-    Class::validate_points();
-}
