6fb014a33ad1d90ea2afcd9406446d943d6cdd10
Kevin Johnson <vadskye@gmail.com>
2026-05-09 07:50:13 -0500

    remove redundant lint stuff

--- typescript/eslint.config.mjs
+++ typescript/eslint.config.mjs
@@ -9,6 +9,7 @@ export default [
   ...tseslint.configs.recommended,
   {
     rules: {
+      '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
