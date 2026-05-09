a93d5e38cf63ad73e9372b79e0646bba1d7822c4
Kevin Johnson <vadskye@gmail.com>
2026-05-08 20:27:31 -0500

    improve skill rendering

--- typescript/src/latex/format/skill_name.ts
+++ typescript/src/latex/format/skill_name.ts
@@ -3,16 +3,16 @@ import { uppercaseFirst } from './uppercase_first';
 import { titleCase } from './title_case';
 
 export function skillName(skill: RiseSkill): string {
-  if (skill === 'craft_untrained') {
-    return 'Craft';
-  }
-  if (skill === 'sleight_of_hand') {
-    return 'Sleight of Hand';
-  }
-  const subskillMatch = skill.match(/^(craft|knowledge)_(\w+)/);
+  const subskillMatch = skill.match(/^(craft|knowledge|profession|perform)_(\w+)/);
   if (subskillMatch) {
-    const subskill = subskillMatch[2].replaceAll('_', ' ');
-    return `${uppercaseFirst(subskillMatch[1])} (${subskill})`;
+    const category = uppercaseFirst(subskillMatch[1]);
+    const subskill = subskillMatch[2];
+
+    if (subskill === 'untrained') {
+      return category;
+    }
+
+    return `${category} (${subskill.replaceAll('_', ' ')})`;
   } else {
     return titleCase(skill.replaceAll('_', ' '));
   }
