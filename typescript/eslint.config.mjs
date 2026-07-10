6540bce0c073e045741498a754c4d895475ae4a2
Kevin Johnson <vadskye@gmail.com>
2026-07-09 22:05:13 -0500

    also eslint tsx files

--- typescript/eslint.config.mjs
+++ typescript/eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,mjs,cjs,ts,tsx}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
