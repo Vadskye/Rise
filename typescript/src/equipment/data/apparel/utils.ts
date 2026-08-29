de9f8f00eb1be322d37829cc8e8564f5d9ed768a
Kevin Johnson <vadskye@gmail.com>
2026-08-29 08:11:27 -0500

    buff attribute items

--- typescript/src/equipment/data/apparel/utils.ts
+++ typescript/src/equipment/data/apparel/utils.ts
@@ -1,17 +1,23 @@
 import { Attribute } from '../../../types/core';
 import { StandardItem } from '../../types';
 
+// In terms of combat power, these items are not very strong. However, raw attributes
+// provide bonuses beyond their combat potential, so it's dangerous to make them too
+// cheap.
+// Circlet of Strength is a deep attunement for +1 power. In general, +2 power is about
+// +10% damage. 
+// Circlet of Perception is a deep attunement for +0.5 accuracy. That's very weak.
 export function attributeItem(name: string, attribute: Attribute): StandardItem {
   const title = attribute.charAt(0).toUpperCase() + attribute.slice(1);
   return {
     name,
-    rank: 3,
+    rank: 2,
     short_description: `Grants +1 ${attribute}`,
     description: `You gain a +1 \\glossterm{enhancement bonus} to your ${title}`,
     magical: true,
     upgrades: [
       {
-        rank: 7,
+        rank: 6,
         short_description: `Grants +2 ${attribute}`,
         description: 'The bonus increases to +2.',
       },
